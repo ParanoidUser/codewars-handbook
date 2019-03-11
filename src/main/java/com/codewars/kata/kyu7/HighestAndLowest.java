a4324be9bd0d5273eba31a6327ac6d70a012a05f
Paranoid User <pilgrim.whynot@gmail.com>
2019-03-11 12:54:42 -0700

    https://www.codewars.com/kata/highest-and-lowest

--- src/main/java/com/codewars/kata/kyu7/HighestAndLowest.java
+++ src/main/java/com/codewars/kata/kyu7/HighestAndLowest.java
@@ -1,8 +1,6 @@
 package com.codewars.kata.kyu7;
 
 import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * @see <a href="https://www.codewars.com/kata/highest-and-lowest">Highest and Lowest</a>
@@ -11,11 +9,9 @@ public class HighestAndLowest
 {
     public static String kata(String numbers)
     {
-        List<Integer> sorted = Arrays.stream(numbers.split("\\s+"))
-                                     .map(Integer::parseInt)
-                                     .sorted(Integer::compare)
-                                     .collect(Collectors.toList());
-
-        return sorted.get(sorted.size() - 1) + " " + sorted.get(0);
+        var stats = Arrays.stream(numbers.split("\\s"))
+                          .mapToInt(Integer::parseInt)
+                          .summaryStatistics();
+        return stats.getMax() + " " + stats.getMin();
     }
 }
