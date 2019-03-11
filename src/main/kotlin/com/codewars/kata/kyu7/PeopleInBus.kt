eb2ba26e0f92ff90f6724fe0d5b3ad8ae181982d
Paranoid User <pilgrim.whynot@gmail.com>
2019-03-11 12:21:19 -0700

    https://www.codewars.com/kata/number-of-people-in-the-bus

--- src/main/kotlin/com/codewars/kata/kyu7/PeopleInBus.kt
+++ src/main/kotlin/com/codewars/kata/kyu7/PeopleInBus.kt
@@ -1,18 +1,6 @@
 package com.codewars.kata.kyu7
 
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Test
-
 /**
  * @see [Number of People in the Bus](https://www.codewars.com/kata/number-of-people-in-the-bus)
  */
 fun people(busStops: Array<Pair<Int, Int>>) = busStops.sumBy { it.first - it.second }
-
-class TestExample {
-    @Test
-    fun `Basic Tests`() {
-        assertEquals(17, people(arrayOf(3 to 0, 9 to 1, 4 to 10, 12 to 2, 6 to 1, 7 to 10)))
-        assertEquals(21, people(arrayOf(3 to 0, 9 to 1, 4 to 8, 12 to 2, 6 to 1, 7 to 8)))
-        assertEquals(5, people(arrayOf(10 to 0, 3 to 5, 5 to 8)))
-    }
-}
