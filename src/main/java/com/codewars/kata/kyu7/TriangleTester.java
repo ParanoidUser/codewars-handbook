80b029ae726cc867a2e3e58c98c76d705a3e8325
dmitfedo <dmitriy.fedoriv@edifecs.com>
2019-03-11 13:31:03 -0700

    https://www.codewars.com/kata/is-this-a-triangle

--- src/main/java/com/codewars/kata/kyu7/TriangleTester.java
+++ src/main/java/com/codewars/kata/kyu7/TriangleTester.java
@@ -7,6 +7,6 @@ public class TriangleTester
 {
     public static boolean kata(int a, int b, int c)
     {
-        return (a + b > c && b + c > a && a + c > b);
+        return a + b > c && b + c > a && a + c > b;
     }
 }
