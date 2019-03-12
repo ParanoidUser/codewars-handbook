684a9a62ec07f38a01c62bf366865c14abb3485c
Paranoid User <dmitriy.fedoriv@gmail.com>
2019-03-11 19:00:37 -0700

    https://www.codewars.com/kata/simple-fun-number-63-shape-area

---
+++ src/test/java/com/codewars/kata/kyu7/ShapeAreaTest.java
@@ -0,0 +1,18 @@
+package com.codewars.kata.kyu7;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ShapeAreaTest
+{
+    @Test
+    void tests()
+    {
+        assertEquals(1, ShapeArea.kata(1));
+        assertEquals(5, ShapeArea.kata(2));
+        assertEquals(13, ShapeArea.kata(3));
+        assertEquals(25, ShapeArea.kata(4));
+        assertEquals(41, ShapeArea.kata(5));
+    }
+}
