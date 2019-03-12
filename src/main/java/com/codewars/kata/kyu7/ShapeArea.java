b170201f06283e3af5e1d011f9e11d1449490c6f
Paranoid User <dmitriy.fedoriv@gmail.com>
2019-03-11 19:00:37 -0700

    https://www.codewars.com/kata/simple-fun-number-63-shape-area

---
+++ src/main/java/com/codewars/kata/kyu7/ShapeArea.java
@@ -0,0 +1,12 @@
+package com.codewars.kata.kyu7;
+
+/**
+ * @see <a href="https://www.codewars.com/kata/simple-fun-number-63-shape-area">Simple Fun #63: Shape Area</a>
+ */
+public class ShapeArea
+{
+    public static int kata(int n)
+    {
+      return 2 * n * (n - 1) + 1;
+    }
+}
