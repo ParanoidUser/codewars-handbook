e3ff58aef36815b868c97eb54dc31a1410615edb
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2019-03-13 00:54:11 -0700

    https://www.codewars.com/kata/sum-of-positive

---
+++ src/test/java/com/codewars/kata/kyu8/SumOfPositiveTest.java
@@ -0,0 +1,18 @@
+package com.codewars.kata.kyu8;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SumOfPositiveTest
+{
+    @Test
+    void testSomething()
+    {
+        assertEquals(15, SumOfPositive.kata(new int[]{ 1, 2, 3, 4, 5 }));
+        assertEquals(13, SumOfPositive.kata(new int[]{ 1, -2, 3, 4, 5 }));
+        assertEquals(0, SumOfPositive.kata(new int[]{}));
+        assertEquals(0, SumOfPositive.kata(new int[]{ -1, -2, -3, -4, -5 }));
+        assertEquals(9, SumOfPositive.kata(new int[]{ -1, 2, 3, 4, -5 }));
+    }
+}
