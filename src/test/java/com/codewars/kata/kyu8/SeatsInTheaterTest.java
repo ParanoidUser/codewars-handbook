e2ad702b39b735ad6f3003cbf162469aa8bb41b9
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2019-03-13 00:47:42 -0700

    https://www.codewars.com/kata/simple-fun-number-1-seats-in-theater

---
+++ src/test/java/com/codewars/kata/kyu8/SeatsInTheaterTest.java
@@ -0,0 +1,18 @@
+package com.codewars.kata.kyu8;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SeatsInTheaterTest
+{
+    @Test
+    void exampleTests()
+    {
+        assertEquals(96, SeatsInTheater.kata(16, 11, 5, 3));
+        assertEquals(0, SeatsInTheater.kata(1, 1, 1, 1));
+        assertEquals(18, SeatsInTheater.kata(13, 6, 8, 3));
+        assertEquals(99, SeatsInTheater.kata(60, 100, 60, 1));
+        assertEquals(0, SeatsInTheater.kata(1000, 1000, 1000, 1000));
+    }
+}
