707134b74dbff163da16153a522bc1aa70168a19
Paranoid User <dmitriy.fedoriv@gmail.com>
2019-03-13 09:14:53 -0700

    https://www.codewars.com/kata/opposite-number

---
+++ src/test/java/com/codewars/kata/kyu8/OppositeNumberTest.java
@@ -0,0 +1,14 @@
+package com.codewars.kata.kyu8;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class OppositeNumberTest
+{
+    @Test
+    void tests()
+    {
+        assertEquals(-1, OppositeNumber.kata(1));
+    }
+}
