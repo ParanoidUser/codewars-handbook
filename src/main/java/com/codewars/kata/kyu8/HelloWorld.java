6b78770f590c035d0f12a75b84b69bd27079fb93
Paranoid User <dmitriy.fedoriv@gmail.com>
2019-03-13 09:27:36 -0700

    https://www.codewars.com/kata/function-1-hello-world

---
+++ src/main/java/com/codewars/kata/kyu8/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.codewars.kata.kyu8;
+
+/**
+ * @see <a href="https://www.codewars.com/kata/function-1-hello-world">Function 1 - hello world</a>
+ */
+public class HelloWorld
+{
+    public static String kata()
+    {
+        return "hello world!";
+    }
+}
