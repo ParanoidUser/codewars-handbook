007ee18771e77c673b9f5c0d2830b5712fed4a67
Paranoid User <dmitriy.fedoriv@gmail.com>
2019-03-13 00:24:07 -0700

    https://www.codewars.com/kata/multiply

---
+++ src/test/java/com/codewars/kata/kyu8/MultiplyTest.java
@@ -0,0 +1,13 @@
+package com.codewars.kata.kyu8;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MultiplyTest
+{
+    @Test
+    void should()
+    {
+        Assertions.assertEquals(4, Multiply.kata(2.0, 2.0));
+    }
+}
