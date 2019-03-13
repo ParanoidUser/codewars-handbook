94d45a50612f96db42b7c466df15c54ad57aab67
Paranoid User <dmitriy.fedoriv@gmail.com>
2019-03-13 09:36:15 -0700

    https://www.codewars.com/kata/l1-set-alarm

---
+++ src/main/java/com/codewars/kata/kyu8/Alarm.java
@@ -0,0 +1,12 @@
+package com.codewars.kata.kyu8;
+
+/**
+ * @see <a href="https://www.codewars.com/kata/l1-set-alarm">L1: Set Alarm</a>
+ */
+public class Alarm
+{
+    public static boolean kata(boolean employed, boolean vacation)
+    {
+        return employed && !vacation;
+    }
+}
---
+++ src/test/java/com/codewars/kata/kyu8/AlarmTest.java
@@ -0,0 +1,18 @@
+package com.codewars.kata.kyu8;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AlarmTest
+{
+    @Test
+    void should()
+    {
+        assertFalse(Alarm.kata(true, true));
+        assertFalse(Alarm.kata(false, true));
+        assertFalse(Alarm.kata(false, false));
+        assertTrue(Alarm.kata(true, false));
+    }
+}
