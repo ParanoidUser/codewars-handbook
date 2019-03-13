f0381af787bbce2cf78671636c7e736198de251b
Paranoid User <dmitriy.fedoriv@gmail.com>
2019-03-13 09:14:53 -0700

    https://www.codewars.com/kata/opposite-number

---
+++ src/main/java/com/codewars/kata/kyu8/OppositeNumber.java
@@ -0,0 +1,12 @@
+package com.codewars.kata.kyu8;
+
+/**
+ * @see <a href="https://www.codewars.com/kata/opposite-number">Opposite number</a>
+ */
+public class OppositeNumber
+{
+    public static int kata(int number)
+    {
+        return -number;
+    }
+}
