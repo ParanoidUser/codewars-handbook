4388a885713da5a84420371d746d6dbdbc159a33
Paranoid User <dmitriy.fedoriv@gmail.com>
2019-03-13 00:24:07 -0700

    https://www.codewars.com/kata/multiply

---
+++ src/main/java/com/codewars/kata/kyu8/Multiply.java
@@ -0,0 +1,12 @@
+package com.codewars.kata.kyu8;
+
+/**
+ * @see <a href="https://www.codewars.com/kata/multiply">Multiply</a>
+ */
+public class Multiply
+{
+    public static Double kata(Double a, Double b)
+    {
+        return a * b;
+    }
+}
