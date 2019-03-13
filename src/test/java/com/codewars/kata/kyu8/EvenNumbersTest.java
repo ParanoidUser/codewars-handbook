fe1b33eed39e2c395e7bbe4e86f0c62c37094338
Paranoid User <dmitriy.fedoriv@gmail.com>
2019-03-13 00:27:01 -0700

    Sample tests for https://www.codewars.com/kata/find-numbers-which-are-divisible-by-given-number

---
+++ src/test/java/com/codewars/kata/kyu8/EvenNumbersTest.java
@@ -0,0 +1,16 @@
+package com.codewars.kata.kyu8;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class EvenNumbersTest
+{
+    @Test
+    void should()
+    {
+        assertArrayEquals(new int[]{ 2, 4, 6 }, EvenNumbers.kata(new int[]{ 1, 2, 3, 4, 5, 6 }, 2));
+        assertArrayEquals(new int[]{ 3, 6 }, EvenNumbers.kata(new int[]{ 1, 2, 3, 4, 5, 6 }, 3));
+        assertArrayEquals(new int[]{ 0, 4 }, EvenNumbers.kata(new int[]{ 0, 1, 2, 3, 4, 5, 6 }, 4));
+    }
+}
