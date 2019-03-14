8e538a2e3396ed96e1eb7fc6b3934b481a54f91b
Paranoid User <dmitriy.fedoriv@gmail.com>
2019-03-13 23:10:42 -0700

    https://www.codewars.com/kata/a-rule-of-divisibility-by-7

---
+++ src/test/java/com/codewars/kata/kyu7/DivSevenTest.java
@@ -0,0 +1,20 @@
+package com.codewars.kata.kyu7;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class DivSevenTest
+{
+    @Test
+    void should()
+    {
+        Assertions.assertArrayEquals(new long[]{ 37, 0 }, DivSeven.kata(37));
+        assertArrayEquals(new long[]{ 35, 1 }, DivSeven.kata(371));
+        assertArrayEquals(new long[]{ 33, 1 }, DivSeven.kata(372));
+        assertArrayEquals(new long[]{ 7, 2 }, DivSeven.kata(1603));
+        assertArrayEquals(new long[]{ 28, 7 }, DivSeven.kata(477557101));
+        assertArrayEquals(new long[]{ 42, 1 }, DivSeven.kata(483));
+    }
+}
