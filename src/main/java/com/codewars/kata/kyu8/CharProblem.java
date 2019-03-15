535ea782c72942795ab0ac739369e84643149b3e
Paranoid User <pilgrim.whynot@gmail.com>
2019-03-14 19:00:23 -0700

    https://www.codewars.com/kata/parse-nice-int-from-char-problem

---
+++ src/main/java/com/codewars/kata/kyu8/CharProblem.java
@@ -0,0 +1,12 @@
+package com.codewars.kata.kyu8;
+
+/**
+ * @see <a href="https://www.codewars.com/kata/parse-nice-int-from-char-problem">Parse nice int from char problem</a>
+ */
+public class CharProblem
+{
+    public static int kata(String herOld)
+    {
+        return herOld.charAt(0) - '0';
+    }
+}
