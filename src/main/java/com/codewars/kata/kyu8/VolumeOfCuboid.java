e81c2fd4a2b2d7eb0a331836cdb613aaeba1a54d
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2019-03-14 19:26:24 -0700

    https://www.codewars.com/kata/volume-of-a-cuboid

---
+++ src/main/java/com/codewars/kata/kyu8/VolumeOfCuboid.java
@@ -0,0 +1,12 @@
+package com.codewars.kata.kyu8;
+
+/**
+ * @see <a href="https://www.codewars.com/kata/volume-of-a-cuboid">Volume of a Cuboid</a>
+ */
+public class VolumeOfCuboid
+{
+    public static double kata(double length, double width, double height)
+    {
+        return length * width * height;
+    }
+}
