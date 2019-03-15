b01e3b711cfc8e674a5c1ca1bee13a46eaafa7bb
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2019-03-14 18:24:40 -0700

    https://www.codewars.com/kata/remove-exclamation-marks

---
+++ src/main/java/com/codewars/kata/kyu8/RemoveExclamationMarks.java
@@ -0,0 +1,12 @@
+package com.codewars.kata.kyu8;
+
+/**
+ * @see <a href="https://www.codewars.com/kata/remove-exclamation-marks">Remove exclamation marks</a>
+ */
+public class RemoveExclamationMarks
+{
+    public static String kata(String s)
+    {
+        return s.replace("!", "");
+    }
+}
