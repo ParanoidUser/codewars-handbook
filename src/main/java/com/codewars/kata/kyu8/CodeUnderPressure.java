b3beaf04b4c848640708936f70f164d88dd2da93
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2019-03-14 19:17:55 -0700

    https://www.codewars.com/kata/you-cant-code-under-pressure-number-1

---
+++ src/main/java/com/codewars/kata/kyu8/CodeUnderPressure.java
@@ -0,0 +1,12 @@
+package com.codewars.kata.kyu8;
+
+/**
+ * @see <a href="https://www.codewars.com/kata/you-cant-code-under-pressure-number-1">You Can't Code Under Pressure #1</a>
+ */
+public class CodeUnderPressure
+{
+    public static int kata(int i)
+    {
+        return 2 * i;
+    }
+}
---
+++ src/test/java/com/codewars/kata/kyu8/CodeUnderPressureTest.java
@@ -0,0 +1,14 @@
+package com.codewars.kata.kyu8;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CodeUnderPressureTest
+{
+    @Test
+    void sample()
+    {
+        assertEquals(4, CodeUnderPressure.kata(2));
+    }
+}
