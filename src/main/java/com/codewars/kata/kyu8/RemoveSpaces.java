14e1ed3111a2a1054d46fdc0d810a4af794ca829
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2019-03-14 18:27:38 -0700

    https://www.codewars.com/kata/remove-string-spaces

---
+++ src/main/java/com/codewars/kata/kyu8/RemoveSpaces.java
@@ -0,0 +1,12 @@
+package com.codewars.kata.kyu8;
+
+/**
+ * @see <a href="https://www.codewars.com/kata/remove-string-spaces">Remove String Spaces</a>
+ */
+public class RemoveSpaces
+{
+    public static String kata(String s)
+    {
+        return s.replace(" ", "");
+    }
+}
---
+++ src/test/java/com/codewars/kata/kyu8/RemoveSpacesTest.java
@@ -0,0 +1,18 @@
+package com.codewars.kata.kyu8;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RemoveSpacesTest
+{
+    @Test
+    void should()
+    {
+        assertEquals("8j8mBliB8gimjB8B8jlB", RemoveSpaces.kata("8 j 8   mBliB8g  imjB8B8  jl  B"));
+        assertEquals("88Bifk8hB8BB8BBBB888chl8BhBfd", RemoveSpaces.kata("8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd"));
+        assertEquals("8aaaaaddddr", RemoveSpaces.kata("8aaaaa dddd r     "));
+        assertEquals("jfBmgklf8hg88lbe8", RemoveSpaces.kata("jfBm  gk lf8hg  88lbe8 "));
+        assertEquals("8jaam", RemoveSpaces.kata("8j aam"));
+    }
+}
