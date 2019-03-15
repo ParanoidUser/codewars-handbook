f62afeeb7583aafb50cee840ad37f967a18b183b
Paranoid User <pilgrim.whynot@gmail.com>
2019-03-14 19:14:13 -0700

    https://www.codewars.com/kata/is-he-gonna-survive

---
+++ src/main/kotlin/com/codewars/kata/kyu8/GonnaSurvive.kt
@@ -0,0 +1,6 @@
+package com.codewars.kata.kyu8
+
+/**
+ * @see [Is he gonna survive?](https://www.codewars.com/kata/is-he-gonna-survive)
+ */
+fun hero(bullets: Int, dragons: Int) = bullets >= 2 * dragons
