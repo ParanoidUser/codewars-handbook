f41cca85ea34a2e255c6dc4347da3b6c45f4e5cc
Paranoid User <dmitriy.fedoriv@gmail.com>
2019-03-17 23:45:52 -0700

    https://www.codewars.com/kata/maximum-contiguous-sum

---
+++ src/main/java/com/codewars/kata/kyu5/MaxSum.java
@@ -0,0 +1,18 @@
+package com.codewars.kata.kyu5;
+
+/**
+ * @see <a href="https://www.codewars.com/kata/maximum-contiguous-sum">Maximum Contiguous Sum</a>
+ */
+public class MaxSum
+{
+    public static int kata(int[] arr)
+    {
+        int max = 0, sum = 0;
+        for (int num : arr)
+        {
+            sum = Math.max(sum + num, 0);
+            max = Math.max(sum, max);
+        }
+        return max;
+    }
+}
