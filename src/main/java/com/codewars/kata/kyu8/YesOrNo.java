1d7edcf293b8db861618789584c11caf596cebfe
Paranoid User <dmitriy.fedoriv@gmail.com>
2019-03-19 09:06:44 -0700

    https://www.codewars.com/kata/convert-boolean-values-to-strings-yes-or-no

---
+++ src/main/java/com/codewars/kata/kyu8/YesOrNo.java
@@ -0,0 +1,12 @@
+package com.codewars.kata.kyu8;
+
+/**
+ * @see <a href="https://www.codewars.com/kata/convert-boolean-values-to-strings-yes-or-no">Convert boolean values to strings 'Yes' or 'No'.</a>
+ */
+class YesOrNo
+{
+    public static String kata(boolean b)
+    {
+        return b ? "Yes" : "No";
+    }
+}
