af85609b9aef571ea46412639c794f0f082f66dd
Paranoid User <dmitriy.fedoriv@gmail.com>
2019-03-19 09:06:44 -0700

    https://www.codewars.com/kata/convert-boolean-values-to-strings-yes-or-no

--- src/test/java/com/codewars/kata/kyu8/YesOrNoTest.java
+++ src/test/java/com/codewars/kata/kyu8/YesOrNoTest.java
@@ -1,5 +1,15 @@
 package com.codewars.kata.kyu8;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
 public class YesOrNoTest
 {
+    @Test
+    void sample()
+    {
+        assertEquals("Yes", YesOrNo.kata(true));
+        assertEquals("No", YesOrNo.kata(false));
+    }
 }
