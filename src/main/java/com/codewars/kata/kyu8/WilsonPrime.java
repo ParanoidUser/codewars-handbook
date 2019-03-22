a7c7ebf1b5bb44e6881bfb111f933f9e2e5e5536
Paranoid User <pilgrim.whynot@gmail.com>
2019-03-21 18:30:12 -0700

    https://www.codewars.com/kata/wilson-primes

---
+++ src/main/java/com/codewars/kata/kyu8/WilsonPrime.java
@@ -0,0 +1,12 @@
+package com.codewars.kata.kyu8;
+
+/**
+ * @see <a href="https://www.codewars.com/kata/wilson-primes">Wilson primes</a>
+ */
+public class WilsonPrime
+{
+    public static boolean kata(double n)
+    {
+        return n == 5 || n == 13 || n == 563;
+    }
+}
