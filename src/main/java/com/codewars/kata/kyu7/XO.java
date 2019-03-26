def29eaf29b9d4ff1de5421ec297eab6cae1d49a
Paranoid User <dmitriy.fedoriv@gmail.com>
2019-03-26 00:01:09 -0700

    https://www.codewars.com/kata/exes-and-ohs

---
+++ src/main/java/com/codewars/kata/kyu7/XO.java
@@ -0,0 +1,12 @@
+package com.codewars.kata.kyu7;
+
+/**
+ *@see<a href="https://www.codewars.com/kata/exes-and-ohs">Exes and Ohs</a>
+ */
+public class XO
+{
+    public static boolean kata(String str)
+    {
+        return str.replaceAll("[xX]", "").length() == str.replaceAll("[oO]", "").length();
+    }
+}
