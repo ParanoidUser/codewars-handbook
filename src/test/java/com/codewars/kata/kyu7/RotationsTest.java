b2061158af57ec49f819179ee4fa451912085184
Paranoid User <dmitriy.fedoriv@gmail.com>
2019-03-27 10:23:39 -0700

    remove duplicate tests

--- src/test/java/com/codewars/kata/kyu7/RotationsTest.java
+++ src/test/java/com/codewars/kata/kyu7/RotationsTest.java
@@ -12,14 +12,11 @@ public class RotationsTest
     @Test
     void sample()
     {
-        assertFalse(Rotations.kata("bsjq", List.of()));
-        assertTrue(Rotations.kata("", List.of("bsjq", "qbsj", "sjqb", "twZNsslC", "jqbs")));
-        assertTrue(Rotations.kata("bsjq", List.of("bsjq", "qbsj", "sjqb", "twZNsslC", "jqbs")));
-        assertFalse(Rotations.kata("Ajylvpy", List.of("Ajylvpy", "ylvpyAj", "jylvpyA", "lvpyAjy", "pyAjylv", "vpyAjyl", "ipywee" )));
-
         assertTrue(Rotations.kata("", List.of()));
+        assertFalse(Rotations.kata("bsjq", List.of()));
         assertTrue(Rotations.kata("", List.of("bsjq", "qbsj")));
         assertTrue(Rotations.kata("bsjq", List.of("bsjq", "qbsj", "sjqb", "twZNsslC", "jqbs")));
+        assertFalse(Rotations.kata("Ajylvpy", List.of("Ajylvpy", "ylvpyAj", "jylvpyA", "lvpyAjy", "pyAjylv", "vpyAjyl", "ipywee" )));
         assertFalse(Rotations.kata("XjYABhR", List.of("TzYxlgfnhf", "yqVAuoLjMLy", "BhRXjYA", "YABhRXj", "hRXjYAB", "jYABhRX", "XjYABhR", "ABhRXjY")));
     }
 }
