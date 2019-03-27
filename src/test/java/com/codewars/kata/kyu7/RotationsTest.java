66b8d2a9975c97bd349503b3d63fcf03d29c2a3f
Paranoid User <dmitriy.fedoriv@gmail.com>
2019-03-27 10:07:05 -0700

    https://www.codewars.com/kata/all-inclusive

---
+++ src/test/java/com/codewars/kata/kyu7/RotationsTest.java
@@ -0,0 +1,25 @@
+package com.codewars.kata.kyu7;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+public class RotationsTest
+{
+    @Test
+    void sample()
+    {
+        assertFalse(Rotations.kata("bsjq", List.of()));
+        assertTrue(Rotations.kata("", List.of("bsjq", "qbsj", "sjqb", "twZNsslC", "jqbs")));
+        assertTrue(Rotations.kata("bsjq", List.of("bsjq", "qbsj", "sjqb", "twZNsslC", "jqbs")));
+        assertFalse(Rotations.kata("Ajylvpy", List.of("Ajylvpy", "ylvpyAj", "jylvpyA", "lvpyAjy", "pyAjylv", "vpyAjyl", "ipywee" )));
+
+        assertTrue(Rotations.kata("", List.of()));
+        assertTrue(Rotations.kata("", List.of("bsjq", "qbsj")));
+        assertTrue(Rotations.kata("bsjq", List.of("bsjq", "qbsj", "sjqb", "twZNsslC", "jqbs")));
+        assertFalse(Rotations.kata("XjYABhR", List.of("TzYxlgfnhf", "yqVAuoLjMLy", "BhRXjYA", "YABhRXj", "hRXjYAB", "jYABhRX", "XjYABhR", "ABhRXjY")));
+    }
+}
