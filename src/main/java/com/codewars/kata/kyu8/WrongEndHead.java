024e9c4b6bc7ec90ac1a8f505419e8e60f2a10c5
Paranoid User <dmitriy.fedoriv@gmail.com>
2019-03-28 19:02:47 -0700

    https://www.codewars.com/kata/my-head-is-at-the-wrong-end

---
+++ src/main/java/com/codewars/kata/kyu8/WrongEndHead.java
@@ -0,0 +1,12 @@
+package com.codewars.kata.kyu8;
+
+/**
+ * @see <a href="https://www.codewars.com/kata/my-head-is-at-the-wrong-end">My head is at the wrong end!</a>
+ */
+public class WrongEndHead
+{
+    public static String[] kata(String[] arr)
+    {
+        return new String[] { arr[2], arr[1], arr[0] };
+    }
+}
