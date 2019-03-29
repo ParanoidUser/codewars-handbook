dd2700d95f832af887810503ae1e1c2315a03287
Paranoid User <dmitriy.fedoriv@gmail.com>
2019-03-29 16:01:01 -0700

    https://www.codewars.com/kata/number-tostring

---
+++ src/main/java/com/codewars/kata/kyu8/ToString.java
@@ -0,0 +1,9 @@
+package com.codewars.kata.kyu8;
+
+/**
+ * @see <a href="https://www.codewars.com/kata/number-tostring">Number toString</a>
+ */
+public class ToString
+{
+    public static final String a = "" + 123;
+}
