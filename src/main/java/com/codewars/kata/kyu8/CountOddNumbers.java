b5dda943ce8ef4e9f72385d5808d034b53f3a8c2
Paranoid User <dmitriy.fedoriv@gmail.com>
2019-03-28 18:55:19 -0700

    https://www.codewars.com/kata/count-odd-numbers-below-n

---
+++ src/main/java/com/codewars/kata/kyu8/CountOddNumbers.java
@@ -0,0 +1,12 @@
+package com.codewars.kata.kyu8;
+
+/**
+ * @see <a href="https://www.codewars.com/kata/count-odd-numbers-below-n">Count Odd Numbers below n</a>
+ */
+public class CountOddNumbers
+{
+    public static int kata(int n)
+    {
+        return n / 2;
+    }
+}
