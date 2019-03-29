1614cdf10a2141dd2465317ba1c311112a7019b9
Paranoid User <dmitriy.fedoriv@gmail.com>
2019-03-28 18:10:42 -0700

    https://www.codewars.com/kata/easy-logs

---
+++ src/main/java/com/codewars/kata/kyu8/EasyLogs.java
@@ -0,0 +1,12 @@
+package com.codewars.kata.kyu8;
+
+/**
+ * @see <a href="https://www.codewars.com/kata/easy-logs">easy logs</a>
+ */
+public class EasyLogs
+{
+    public static double kata(double x, double a, double b)
+    {
+        return Math.log(a * b) / Math.log(x);
+    }
+}
