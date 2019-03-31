43f0026a9d4c8b08d32f2d1a9e8147f4c70cae4f
Paranoid User <dmitriy.fedoriv@gmail.com>
2019-03-31 00:54:47 -0700

    https://www.codewars.com/kata/circles-in-polygons

---
+++ src/test/java/com/codewars/kata/kyu8/PolygonTest.java
@@ -0,0 +1,16 @@
+package com.codewars.kata.kyu8;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class PolygonTest
+{
+    @Test
+    void sample()
+    {
+        assertEquals(5, new Polygon(4, 5).circleDiameter(), 0.001);
+        assertEquals(21.728, new Polygon(8, 9).circleDiameter(), 0.001);
+        assertEquals(2.309, new Polygon(3, 4).circleDiameter(), 0.001);
+    }
+}
