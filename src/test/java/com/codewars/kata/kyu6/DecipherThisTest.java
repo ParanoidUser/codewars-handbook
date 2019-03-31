e051340d84cf73cd53eff9d4fa27222582b633e6
Paranoid User <dmitriy.fedoriv@gmail.com>
2019-03-31 01:10:50 -0700

    tests clean-up

--- src/test/java/com/codewars/kata/kyu6/DecipherThisTest.java
+++ src/test/java/com/codewars/kata/kyu6/DecipherThisTest.java
@@ -12,11 +12,6 @@ public class DecipherThisTest
         assertEquals("Hello good day", DecipherThis.kata("72olle 103doo 100ya"));
         assertEquals("Ready set go", DecipherThis.kata("82yade 115te 103o"));
         assertEquals("Have a go at this and see how you do", DecipherThis.kata("72eva 97 103o 97t 116sih 97dn 115ee 104wo 121uo 100o"));
-    }
-
-    @Test
-    void exampleTests()
-    {
         assertEquals("A wise old owl lived in an oak", DecipherThis.kata("65 119esi 111dl 111lw 108dvei 105n 97n 111ka"));
         assertEquals("The more he saw the less he spoke", DecipherThis.kata("84eh 109ero 104e 115wa 116eh 108sse 104e 115eokp"));
         assertEquals("The less he spoke the more he heard", DecipherThis.kata("84eh 108sse 104e 115eokp 116eh 109ero 104e 104dare"));
