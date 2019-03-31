d2540cca3eaa89232707bbb0557f13b784020939
Paranoid User <dmitriy.fedoriv@gmail.com>
2019-03-31 00:41:16 -0700

    https://www.codewars.com/kata/simple-multiplication

---
+++ src/main/java/com/codewars/kata/kyu8/Sid.java
@@ -0,0 +1,12 @@
+package com.codewars.kata.kyu8;
+
+/**
+ * @see <a href="https://www.codewars.com/kata/simple-multiplication">Simple multiplication</a>
+ */
+public class Sid
+{
+    public static int kata(int n)
+    {
+        return n * (n % 2 + 8);
+    }
+}
