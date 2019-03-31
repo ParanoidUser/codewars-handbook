9e85a689c0cf2bf15a8a633e2a0f905089bd58ae
Paranoid User <dmitriy.fedoriv@gmail.com>
2019-03-31 00:36:27 -0700

    https://www.codewars.com/kata/sentence-smash

---
+++ src/main/java/com/codewars/kata/kyu8/SmashWords.java
@@ -0,0 +1,12 @@
+package com.codewars.kata.kyu8;
+
+/**
+ * @see <a href="https://www.codewars.com/kata/sentence-smash">Sentence Smash</a>
+ */
+public class SmashWords
+{
+    public static String kata(String... words)
+    {
+        return String.join(" ", words);
+    }
+}
