e46d720fd24ba6ca347dd4cda0bec9633656ecd3
Paranoid User <dmitriy.fedoriv@gmail.com>
2019-03-30 23:36:45 -0700

    https://www.codewars.com/kata/basic-variable-assignment

---
+++ src/main/java/com/codewars/kata/kyu8/BasicVariable.java
@@ -0,0 +1,9 @@
+package com.codewars.kata.kyu8;
+
+/**
+ * @see <a href="https://www.codewars.com/kata/basic-variable-assignment">Basic variable assignment</a>
+ */
+public class BasicVariable
+{
+    static String name = "codewa.rs";
+}
---
+++ src/test/java/com/codewars/kata/kyu8/BasicVariableTest.java
@@ -0,0 +1,14 @@
+package com.codewars.kata.kyu8;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class BasicVariableTest
+{
+    @Test
+    void sample()
+    {
+        assertEquals("codewa.rs", BasicVariable.name);
+    }
+}
