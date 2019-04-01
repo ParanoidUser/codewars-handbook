9e3b0636428ac35e4b4498be22a9119c8ed51797
Paranoid User <dmitriy.fedoriv@gmail.com>
2019-03-31 23:30:55 -0700

    https://www.codewars.com/kata/577ff15ad648a14b780000e7

---
+++ src/main/java/com/codewars/kata/kyu8/Welcome.java
@@ -0,0 +1,31 @@
+package com.codewars.kata.kyu8;
+
+/**
+ * @see <a href="https://www.codewars.com/kata/577ff15ad648a14b780000e7">Welcome!</a>
+ */
+public class Welcome
+{
+    public static String kata(String language)
+    {
+        switch(language)
+        {
+            case "czech": return  "Vitejte";
+            case "danish": return "Velkomst";
+            case "dutch": return "Welkom";
+            case "estonian": return "Tere tulemast";
+            case "finnish":  return "Tervetuloa";
+            case "flemish": return "Welgekomen";
+            case "french": return "Bienvenue";
+            case "german": return "Willkommen";
+            case "irish": return "Failte";
+            case "italian": return "Benvenuto";
+            case "latvian": return "Gaidits";
+            case "lithuanian": return  "Laukiamas";
+            case "polish": return "Witamy";
+            case "spanish": return  "Bienvenido";
+            case "swedish": return "Valkommen";
+            case "welsh": return "Croeso";
+            default: return "Welcome";
+        }
+    }
+}
