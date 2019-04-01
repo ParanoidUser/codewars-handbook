ebbcc77ad5ac58d10785c86552eb6d908eed0aec
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2019-04-01 09:19:10 -0700

    https://www.codewars.com/kata/5601c5f6ba804403c7000004

--- src/main/java/com/codewars/kata/kyu8/Barycenter.java
+++ src/main/java/com/codewars/kata/kyu8/Barycenter.java
@@ -1,14 +1,17 @@
 package com.codewars.kata.kyu8;
 
 /**
- * @see <a href="https://www.codewars.com/kata/localize-the-barycenter-of-a-triangle">Localize The Barycenter of a Triangle</a>
+ * @see <a href="https://www.codewars.com/kata/5601c5f6ba804403c7000004">Localize The Barycenter of a Triangle</a>
  */
 class Barycenter
 {
     public static double[] kata(double[] x, double[] y, double[] z)
     {
-        var x0 = Math.round((x[0] + y[0] + z[0]) * 3333.33) / 10.0e3;
-        var y0 = Math.round((x[1] + y[1] + z[1]) * 3333.33) / 10.0e3;
-        return new double[] { x0, y0 };
+        var coordinates = new double[2];
+        for (int i = 0; i < 2; i++)
+        {
+            coordinates[i] = Math.round((x[i] + y[i] + z[i]) * 3333.33) / 10.0e3;
+        }
+        return coordinates;
     }
 }
