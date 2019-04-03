495aab9e22f9228d1a42ae72643da85f9663a128
Paranoid User <dmitriy.fedoriv@gmail.com>
2019-04-02 18:08:38 -0700

    Move javadoc section on class level

--- src/main/java/com/codewars/kata/kyu7/IssuerChecker.java
+++ src/main/java/com/codewars/kata/kyu7/IssuerChecker.java
@@ -1,10 +1,10 @@
 package com.codewars.kata.kyu7;
 
+/**
+ * @see <a href="https://www.codewars.com/kata/5701e43f86306a615c001868">Credit card issuer checking</a>
+ */
 public class IssuerChecker
 {
-    /**
-     * @see <a href="https://www.codewars.com/kata/credit-card-issuer-checking">Credit card issuer checking</a>
-     */
     public static String kata(String cardNumber)
     {
         return cardNumber.matches("^3[4|7].{13}$") ? "AMEX"
