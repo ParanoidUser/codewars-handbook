5a2057df58ad4c844676f0c6f49387ba2383b3da
Paranoid User <dmitriy.fedoriv@gmail.com>
2019-04-02 18:07:59 -0700

    Fix for not closed javadoc </a> tag

--- src/main/java/com/codewars/kata/kyu7/GpsSpeed.java
+++ src/main/java/com/codewars/kata/kyu7/GpsSpeed.java
@@ -1,13 +1,13 @@
 package com.codewars.kata.kyu7;
 
 /**
- * @see <a href="https://www.codewars.com/kata/speed-control">Speed Control/a>
+ * @see <a href="https://www.codewars.com/kata/56484848ba95170a8000004d">Speed Control</a>
  */
 public class GpsSpeed
 {
     public static int kata(int s, double[] x)
     {
-        for(int i = 0; i < x.length - 1; i++)
+        for (int i = 0; i < x.length - 1; i++)
         {
             x[0] = Math.max(x[i + 1] - x[i], x[0]);
         }
