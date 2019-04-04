d0533031215541b5c3598932da3ae5fcff693cd5
Paranoid User <dmitriy.fedoriv@gmail.com>
2019-04-03 20:09:59 -0700

    https://www.codewars.com/kata/51e0007c1f9378fa810002a9

--- src/main/java/com/codewars/kata/kyu6/DeadFish.java
+++ src/main/java/com/codewars/kata/kyu6/DeadFish.java
@@ -1,28 +1,35 @@
 package com.codewars.kata.kyu6;
 
 import java.util.ArrayList;
-import java.util.List;
 
 /**
- * @see <a href="https://www.codewars.com/kata/make-the-deadfish-swim">Make the Deadfish swim</a>
+ * @see <a href="https://www.codewars.com/kata/51e0007c1f9378fa810002a9">Make the Deadfish swim</a>
  */
 public class DeadFish
 {
-    public static int[] parse(String data)
+    static int[] parse(String data)
     {
         int value = 0;
-        List<Integer> results = new ArrayList<>();
+        var results = new ArrayList<Integer>();
         for (char c : data.toCharArray())
         {
             if (c == 'i')
+            {
                 value++;
+            }
             else if (c == 'd')
+            {
                 value--;
+            }
             else if (c == 's')
+            {
                 value *= value;
+            }
             else if (c == 'o')
+            {
                 results.add(value);
+            }
         }
-        return results.stream().mapToInt(i -> i).toArray();
+        return results.stream().skip(1).mapToInt(i -> i).toArray();
     }
 }
