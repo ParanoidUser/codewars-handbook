2fe93d4828696ead73e9ea1f9fe27fda0516265b
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2019-07-02 00:15:46 -0700

    https://www.codewars.com/kata/55c0a79e20be94c91400014b

---
+++ src/main/java/com/codewars/kata/kyu8/Cube.java
@@ -0,0 +1,19 @@
+package com.codewars.kata.kyu8;
+
+/**
+ * @see <a href="https://www.codewars.com/kata/55c0a79e20be94c91400014b">Playing with cubes I</a>
+ */
+public class Cube
+{
+    private int side;
+
+    public int getSide()
+    {
+        return side;
+    }
+
+    public void setSide(int side)
+    {
+        this.side = side;
+    }
+}
