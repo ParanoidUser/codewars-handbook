9b4e8adb4ac669188dad3520444a6e6ad72d5cc7
Paranoid User <dmitriy.fedoriv@gmail.com>
2019-07-19 01:02:42 -0700

    https://www.codewars.com/kata/57cc975ed542d3148f00015b

--- src/main/java/com/codewars/kata/kyu8/ArrayContains.java
+++ src/main/java/com/codewars/kata/kyu8/ArrayContains.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 /**
  * @see <a href="https://www.codewars.com/kata/57cc975ed542d3148f00015b">You only need one - Beginner</a>
  */
-class Solution
+class ArrayContains
 {
     static boolean check(Object[] a, Object x)
     {
