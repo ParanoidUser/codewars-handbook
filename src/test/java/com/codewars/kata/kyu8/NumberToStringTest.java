4a68afa4e7c5cc5ff84f70e08aedf85e783a75fe
Paranoid User <dmitriy.fedoriv@gmail.com>
2019-07-28 23:47:47 -0700

    https://www.codewars.com/kata/5265326f5fda8eb1160004c8

--- src/test/java/com/codewars/kata/kyu8/NumberToStringTest.java
+++ src/test/java/com/codewars/kata/kyu8/NumberToStringTest.java
@@ -4,12 +4,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-public class NumberToStringTest
+class NumberToStringTest
 {
     @Test
     void sample()
     {
-        assertEquals("123", NumberToString.kata(123));
-        assertEquals("999", NumberToString.kata(999));
+        assertEquals("123", NumberToString.numberToString(123));
+        assertEquals("999", NumberToString.numberToString(999));
     }
 }
