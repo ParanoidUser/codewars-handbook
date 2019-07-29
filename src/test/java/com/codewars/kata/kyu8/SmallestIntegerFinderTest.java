bf729ab7b3fcdd62959409594034371575acd486
Paranoid User <dmitriy.fedoriv@gmail.com>
2019-07-29 00:26:36 -0700

    https://www.codewars.com/kata/55a2d7ebe362935a210000b2

--- src/test/java/com/codewars/kata/kyu8/SmallestIntegerFinderTest.java
+++ src/test/java/com/codewars/kata/kyu8/SmallestIntegerFinderTest.java
@@ -4,13 +4,13 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class SmallestIntegerFinderTest
+class SmallestIntegerFinderTest
 {
     @Test
     void sample()
     {
-        assertEquals(11, SmallestIntegerFinder.kata(new int[]{ 78, 56, 232, 12, 11, 43 }));
-        assertEquals(-33, SmallestIntegerFinder.kata(new int[]{ 78, 56, -2, 12, 8, -33 }));
-        assertEquals(Integer.MIN_VALUE, SmallestIntegerFinder.kata(new int[]{ 0, Integer.MIN_VALUE, Integer.MAX_VALUE }));
+        assertEquals(11, SmallestIntegerFinder.findSmallestInt(new int[]{ 78, 56, 232, 12, 11, 43 }));
+        assertEquals(-33, SmallestIntegerFinder.findSmallestInt(new int[]{ 78, 56, -2, 12, 8, -33 }));
+        assertEquals(Integer.MIN_VALUE, SmallestIntegerFinder.findSmallestInt(new int[]{ 0, Integer.MIN_VALUE, Integer.MAX_VALUE }));
     }
 }
