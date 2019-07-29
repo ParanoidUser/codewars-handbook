23be5740579bc6d989e2470144083ef8fafb5fbe
Paranoid User <dmitriy.fedoriv@gmail.com>
2019-07-28 22:20:49 -0700

    https://www.codewars.com/kata/5b68c7029756802aa2000176

--- src/test/java/com/codewars/kata/kyu8/EasyLogsTest.java
+++ src/test/java/com/codewars/kata/kyu8/EasyLogsTest.java
@@ -4,13 +4,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-public class EasyLogsTest
+class EasyLogsTest
 {
     @Test
     void sample()
     {
-        assertEquals(0.7781512503836435, EasyLogs.kata(10, 2, 3), 0.0000001);
-        assertEquals(1.1132827525593785, EasyLogs.kata(5, 2, 3), 0.0000001);
-        assertEquals(0.25938375012788123, EasyLogs.kata(1000, 2, 3), 0.0000001);
+        assertEquals(0.7781512503836435, EasyLogs.logs(10, 2, 3), 0.0000001);
+        assertEquals(1.1132827525593785, EasyLogs.logs(5, 2, 3), 0.0000001);
+        assertEquals(0.25938375012788123, EasyLogs.logs(1000, 2, 3), 0.0000001);
     }
 }
