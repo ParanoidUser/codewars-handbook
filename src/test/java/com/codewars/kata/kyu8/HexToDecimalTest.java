bd7d1ca9b59e643181e61850cb741943e331f3e1
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2019-07-28 23:01:21 -0700

    https://www.codewars.com/kata/57a4d500e298a7952100035d

--- src/test/java/com/codewars/kata/kyu8/HexToDecimalTest.java
+++ src/test/java/com/codewars/kata/kyu8/HexToDecimalTest.java
@@ -4,15 +4,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-public class HexToDecimalTest
+class HexToDecimalTest
 {
     @Test
     void sample()
     {
-        assertEquals(1, HexToDecimal.kata("1"));
-        assertEquals(10, HexToDecimal.kata("a"));
-        assertEquals(16, HexToDecimal.kata("10"));
-        assertEquals(255, HexToDecimal.kata("FF"));
-        assertEquals(-12, HexToDecimal.kata("-C"));
+        assertEquals(1, HexToDecimal.hexToDec("1"));
+        assertEquals(10, HexToDecimal.hexToDec("a"));
+        assertEquals(16, HexToDecimal.hexToDec("10"));
+        assertEquals(255, HexToDecimal.hexToDec("FF"));
+        assertEquals(-12, HexToDecimal.hexToDec("-C"));
     }
 }
