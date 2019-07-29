e4439f6f0eaedee2ca9aa25bb013102b38064a48
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2019-07-28 21:58:39 -0700

    https://www.codewars.com/kata/59342039eb450e39970000a6

--- src/test/java/com/codewars/kata/kyu8/CountOddNumbersTest.java
+++ src/test/java/com/codewars/kata/kyu8/CountOddNumbersTest.java
@@ -4,13 +4,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-public class CountOddNumbersTest
+class CountOddNumbersTest
 {
     @Test
     void sample()
     {
-        assertEquals(3, CountOddNumbers.kata(7));
-        assertEquals(7, CountOddNumbers.kata(15));
-        assertEquals(7511, CountOddNumbers.kata(15023));
+        assertEquals(3, CountOddNumbers.oddCount(7));
+        assertEquals(7, CountOddNumbers.oddCount(15));
+        assertEquals(7511, CountOddNumbers.oddCount(15023));
     }
 }
