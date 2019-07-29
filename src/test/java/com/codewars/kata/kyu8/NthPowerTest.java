127bd144f3bbae48b9c6fb85051f24cb7ec9da6c
Paranoid User <dmitriy.fedoriv@gmail.com>
2019-07-28 23:43:29 -0700

    https://www.codewars.com/kata/57d814e4950d8489720008db

--- src/test/java/com/codewars/kata/kyu8/NthPowerTest.java
+++ src/test/java/com/codewars/kata/kyu8/NthPowerTest.java
@@ -4,13 +4,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-public class NthPowerTest
+class NthPowerTest
 {
     @Test
     void sample()
     {
-        assertEquals(-1, NthPower.kata(new int[] { 1, 2 }, 2));
-        assertEquals(8, NthPower.kata(new int[] { 3, 1, 2, 2 }, 3));
-        assertEquals(4, NthPower.kata(new int[] { 3, 1, 2 }, 2));
+        assertEquals(-1, NthPower.nthPower(new int[] { 1, 2 }, 2));
+        assertEquals(8, NthPower.nthPower(new int[] { 3, 1, 2, 2 }, 3));
+        assertEquals(4, NthPower.nthPower(new int[] { 3, 1, 2 }, 2));
     }
 }
