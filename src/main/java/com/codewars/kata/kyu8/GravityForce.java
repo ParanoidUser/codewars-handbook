bc1d6bd23cd2223921eec6b495850542b40962fd
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2019-07-28 22:54:46 -0700

    https://www.codewars.com/kata/5b609ebc8f47bd595e000627

--- src/main/java/com/codewars/kata/kyu8/GravityForce.java
+++ src/main/java/com/codewars/kata/kyu8/GravityForce.java
@@ -3,14 +3,14 @@ package com.codewars.kata.kyu8;
 import java.util.Map;
 
 /**
- * @see <a href="https://www.codewars.com/kata/find-the-force-of-gravity-between-two-objects">Find the force of gravity between two objects</a>
+ * @see <a href="https://www.codewars.com/kata/5b609ebc8f47bd595e000627">Find the force of gravity between two objects</a>
  */
-public class Gravity
+class GravityForce
 {
-    public static double kata(double[] arrVal, String[] arrUnit)
+    static double solution(double[] arrVal, String[] arrUnit)
     {
         var units = Map.of("μg", 1e-9, "mg", 1e-6, "g", 0.001, "lb", 0.453592, "kg", 1.0,
-                           "μm", 1e-6, "mm", 0.001, "cm", 0.01, "ft", 0.3048, "m", 1.0);
+                "μm", 1e-6, "mm", 0.001, "cm", 0.01, "ft", 0.3048, "m", 1.0);
 
         double m1 = arrVal[0] * units.get(arrUnit[0]);
         double m2 = arrVal[1] * units.get(arrUnit[1]);
