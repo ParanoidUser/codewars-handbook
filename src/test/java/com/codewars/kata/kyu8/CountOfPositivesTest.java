2cfb702a51a6687ff4f31958d6829043fcbe265b
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2019-07-28 22:06:08 -0700

    https://www.codewars.com/kata/576bb71bbbcf0951d5000044

--- src/test/java/com/codewars/kata/kyu8/CountOfPositivesTest.java
+++ src/test/java/com/codewars/kata/kyu8/CountOfPositivesTest.java
@@ -4,12 +4,12 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
-public class CountOfPositivesTest
+class CountOfPositivesTest
 {
     @Test
     void sample()
     {
-        assertArrayEquals(new int[]{ 10, -65 }, CountOfPositives.kata(new int[]{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15 }));
-        assertArrayEquals(new int[]{ 8, -50 }, CountOfPositives.kata(new int[]{ 0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14 }));
+        assertArrayEquals(new int[]{ 10, -65 }, CountOfPositives.countPositivesSumNegatives(new int[]{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15 }));
+        assertArrayEquals(new int[]{ 8, -50 }, CountOfPositives.countPositivesSumNegatives(new int[]{ 0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14 }));
     }
 }
