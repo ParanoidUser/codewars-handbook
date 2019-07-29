5750b33a728661a1667632ee450b376c3f30006f
Paranoid User <dmitriy.fedoriv@gmail.com>
2019-07-28 22:13:32 -0700

    https://www.codewars.com/kata/5a6663e9fd56cb5ab800008b

--- src/main/java/com/codewars/kata/kyu8/Dinglemouse.java
+++ src/main/java/com/codewars/kata/kyu8/Dinglemouse.java
@@ -3,9 +3,9 @@ package com.codewars.kata.kyu8;
 /**
  * @see <a href="https://www.codewars.com/kata/5a6663e9fd56cb5ab800008b">Cat years, Dog years</a>
  */
-public class Dinglemouse
+class Dinglemouse
 {
-    public static int[] kata(final int y)
+    static int[] humanYearsCatYearsDogYears(int y)
     {
         return new int[] { y,
                            y == 1 ? 15 : 16 + 4 * y,
