9b08f3bf9f55458725d1f9ba9fa83b34f4a7481e
Paranoid User <dmitriy.fedoriv@gmail.com>
2019-07-29 00:29:28 -0700

    https://www.codewars.com/kata/57cfdf34902f6ba3d300001e

--- src/test/java/com/codewars/kata/kyu8/SortAndStarTest.java
+++ src/test/java/com/codewars/kata/kyu8/SortAndStarTest.java
@@ -4,12 +4,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-public class SortAndStarTest
+class SortAndStarTest
 {
     @Test
     void sample()
     {
-        assertEquals("b***i***t***c***o***i***n", SortAndStar.kata(new String[] { "bitcoin", "take", "over", "the", "world", "maybe", "who", "knows", "perhaps" }));
-        assertEquals("a***r***e", SortAndStar.kata(new String[] { "turns", "out", "random", "test", "cases", "are", "easier", "than", "writing", "out", "basic", "ones" }));
+        assertEquals("b***i***t***c***o***i***n", SortAndStar.twoSort(new String[] { "bitcoin", "take", "over", "the", "world", "maybe", "who", "knows", "perhaps" }));
+        assertEquals("a***r***e", SortAndStar.twoSort(new String[] { "turns", "out", "random", "test", "cases", "are", "easier", "than", "writing", "out", "basic", "ones" }));
     }
 }
