460a7b582bf56b4721ed230fbbb3872da4a8ef35
Paranoid User <dmitriy.fedoriv@gmail.com>
2019-07-28 23:18:42 -0700

    https://www.codewars.com/kata/57f780909f7e8e3183000078

--- src/test/java/com/codewars/kata/kyu8/IntReduceTest.java
+++ src/test/java/com/codewars/kata/kyu8/IntReduceTest.java
@@ -4,14 +4,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-public class IntReduceTest
+class IntReduceTest
 {
     @Test
     void sample()
     {
-        assertEquals(6, IntReduce.kata(new int[] { 1, 2, 3 }));
-        assertEquals(24, IntReduce.kata(new int[] { 1, 2, 3, 4 }));
-        assertEquals(16, IntReduce.kata(new int[] { 4, 1, 1, 1, 4 }));
-        assertEquals(64, IntReduce.kata(new int[] { 2, 2, 2, 2, 2, 2 }));
+        assertEquals(6, IntReduce.grow(new int[] { 1, 2, 3 }));
+        assertEquals(24, IntReduce.grow(new int[] { 1, 2, 3, 4 }));
+        assertEquals(16, IntReduce.grow(new int[] { 4, 1, 1, 1, 4 }));
+        assertEquals(64, IntReduce.grow(new int[] { 2, 2, 2, 2, 2, 2 }));
     }
 }
