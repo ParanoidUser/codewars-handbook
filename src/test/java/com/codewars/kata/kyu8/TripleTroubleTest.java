264a1ea0ddaeefb9f0e9eaa4f19545ef95ac4abe
Paranoid User <dmitriy.fedoriv@gmail.com>
2019-07-29 00:52:53 -0700

    https://www.codewars.com/kata/5704aea738428f4d30000914

--- src/test/java/com/codewars/kata/kyu8/TripleTroubleTest.java
+++ src/test/java/com/codewars/kata/kyu8/TripleTroubleTest.java
@@ -4,14 +4,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-public class TripleTroubleTest
+class TripleTroubleTest
 {
     @Test
     void sample()
     {
-        assertEquals("ttlheoiscstk", TripleTrouble.kata("this", "test", "lock"));
-        assertEquals("abcabc", TripleTrouble.kata("aa", "bb", "cc"));
-        assertEquals("Batman", TripleTrouble.kata("Bm", "aa", "tn"));
-        assertEquals("LexLuthor", TripleTrouble.kata("LLh", "euo", "xtr"));
+        assertEquals("ttlheoiscstk", TripleTrouble.tripleTrouble("this", "test", "lock"));
+        assertEquals("abcabc", TripleTrouble.tripleTrouble("aa", "bb", "cc"));
+        assertEquals("Batman", TripleTrouble.tripleTrouble("Bm", "aa", "tn"));
+        assertEquals("LexLuthor", TripleTrouble.tripleTrouble("LLh", "euo", "xtr"));
     }
 }
