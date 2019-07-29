a5ea6f7b34eeddd1c4f192a88cf1f6539f772183
Paranoid User <dmitriy.fedoriv@gmail.com>
2019-07-29 00:01:02 -0700

    https://www.codewars.com/kata/5a026a9cffe75fbace00007f

--- src/main/java/com/codewars/kata/kyu8/Polygon.java
+++ src/main/java/com/codewars/kata/kyu8/Polygon.java
@@ -1,15 +1,15 @@
 package com.codewars.kata.kyu8;
 
 /**
- * @see <a href="https://www.codewars.com/kata/circles-in-polygons">Circles in Polygons</a>
+ * @see <a href="https://www.codewars.com/kata/5a026a9cffe75fbace00007f">Circles in Polygons</a>
  */
-public class Polygon
+class Polygon
 {
     private final double diameter;
 
     Polygon(int sides, int sideLength)
     {
-        this.diameter = sideLength / Math.tan(Math.toRadians(180d / sides));
+        diameter = sideLength / Math.tan(Math.toRadians(180d / sides));
     }
 
     double circleDiameter()
