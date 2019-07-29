21870222e88f6fa28733c66f0c3249a28a0983a8
Paranoid User <dmitriy.fedoriv@gmail.com>
2019-07-28 23:52:29 -0700

    https://www.codewars.com/kata/555086d53eac039a2a000083

--- src/main/java/com/codewars/kata/kyu8/OppositesAttract.java
+++ src/main/java/com/codewars/kata/kyu8/OppositesAttract.java
@@ -1,11 +1,11 @@
 package com.codewars.kata.kyu8;
 
 /**
- * @see <a href="https://www.codewars.com/kata/opposites-attract">Opposites Attract</a>
+ * @see <a href="https://www.codewars.com/kata/555086d53eac039a2a000083">Opposites Attract</a>
  */
-public class OppositesAttract
+class OppositesAttract
 {
-    public static boolean kata(final int flower1, final int flower2)
+    static boolean isLove(int flower1, int flower2)
     {
         return flower1 % 2 != flower2 % 2;
     }
