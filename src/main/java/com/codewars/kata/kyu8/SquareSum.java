9f52ee21045dd72a45f8756bb18c3148bb8f6745
Paranoid User <dmitriy.fedoriv@gmail.com>
2019-07-29 00:30:39 -0700

    https://www.codewars.com/kata/515e271a311df0350d00000f

--- src/main/java/com/codewars/kata/kyu8/SquareSum.java
+++ src/main/java/com/codewars/kata/kyu8/SquareSum.java
@@ -5,9 +5,9 @@ import static java.util.Arrays.stream;
 /**
  * @see <a href="https://www.codewars.com/kata/515e271a311df0350d00000f">Square(n) Sum</a>
  */
-public class SquareSum
+class SquareSum
 {
-    public static int squareSum(int[] n)
+    static int squareSum(int[] n)
     {
         return stream(n).map(i -> i * i).sum();
     }
