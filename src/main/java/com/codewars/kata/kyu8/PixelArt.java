2c208f5fdba3bfdb600d6c4b45b4ad5732b681f3
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2019-07-28 23:59:29 -0700

    https://www.codewars.com/kata/58630e2ae88af44d2b0000ea

--- src/main/java/com/codewars/kata/kyu8/PixelArt.java
+++ src/main/java/com/codewars/kata/kyu8/PixelArt.java
@@ -1,11 +1,11 @@
 package com.codewars.kata.kyu8;
 
 /**
- * @see <a href="https://www.codewars.com/kata/thinkful-number-drills-pixelart-planning">Pixelart planning</a>
+ * @see <a href="https://www.codewars.com/kata/58630e2ae88af44d2b0000ea">Pixelart planning</a>
  */
-public class PixelArt
+class PixelArt
 {
-    public static boolean kata(int wallLength, int pixelSize)
+    static boolean isDivisible(int wallLength, int pixelSize)
     {
         return wallLength % pixelSize == 0;
     }
