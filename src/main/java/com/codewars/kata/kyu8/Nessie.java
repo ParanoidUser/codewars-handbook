bed3b68dc0f22f1b59ceddc268c3f26ac212b962
Paranoid User <dmitriy.fedoriv@gmail.com>
2019-07-28 23:40:24 -0700

    https://www.codewars.com/kata/55ccdf1512938ce3ac000056

--- src/main/java/com/codewars/kata/kyu8/Nessie.java
+++ src/main/java/com/codewars/kata/kyu8/Nessie.java
@@ -1,11 +1,11 @@
 package com.codewars.kata.kyu8;
 
 /**
- * @see <a href="https://www.codewars.com/kata/a-strange-trip-to-the-market">A Strange Trip to the Market</a>
+ * @see <a href="https://www.codewars.com/kata/55ccdf1512938ce3ac000056">A Strange Trip to the Market</a>
  */
-public class Nessie
+class Nessie
 {
-    public static boolean kata(String s)
+    static boolean isLockNessMonster(String s)
     {
         return s.contains("fid") || s.contains("3");
     }
