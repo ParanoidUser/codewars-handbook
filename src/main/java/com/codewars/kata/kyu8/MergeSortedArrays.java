e3228139db01a844b932c2ace5018ea506931b66
Paranoid User <dmitriy.fedoriv@gmail.com>
2019-07-28 23:30:40 -0700

    https://www.codewars.com/kata/573f5c61e7752709df0005d2

--- src/main/java/com/codewars/kata/kyu8/MergeSortedArrays.java
+++ src/main/java/com/codewars/kata/kyu8/MergeSortedArrays.java
@@ -3,11 +3,11 @@ package com.codewars.kata.kyu8;
 import java.util.stream.IntStream;
 
 /**
- * @see <a href="https://www.codewars.com/kata/merging-sorted-integer-arrays-without-duplicates">Merging sorted integer arrays (without duplicates)</a>
+ * @see <a href="https://www.codewars.com/kata/573f5c61e7752709df0005d2">Merging sorted integer arrays (without duplicates)</a>
  */
-public class MergeSortedArrays
+class MergeSortedArrays
 {
-    public static int[] kata(int[] first, int[] second)
+    static int[] mergeArrays(int[] first, int[] second)
     {
         return IntStream.concat(IntStream.of(first), IntStream.of(second))
                         .distinct()
