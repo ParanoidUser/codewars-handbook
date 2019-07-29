ca9afa136a39361c82dada3e0190d0c89d7867e0
Paranoid User <dmitriy.fedoriv@gmail.com>
2019-07-29 00:34:49 -0700

    https://www.codewars.com/kata/56fc55cd1f5a93d68a001d4e

--- src/test/java/com/codewars/kata/kyu8/StairsIn20Test.java
+++ src/test/java/com/codewars/kata/kyu8/StairsIn20Test.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-public class StairsIn20Test
+class StairsIn20Test
 {
     @Test
     void sample()
