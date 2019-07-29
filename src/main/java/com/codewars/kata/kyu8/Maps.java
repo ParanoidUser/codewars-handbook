04d8c1ebc4a4e6150d41dc3eb142a9d4c3e17a57
Paranoid User <dmitriy.fedoriv@gmail.com>
2019-07-28 23:28:02 -0700

    https://www.codewars.com/kata/57f781872e3d8ca2a000007e

--- src/main/java/com/codewars/kata/kyu8/Maps.java
+++ src/main/java/com/codewars/kata/kyu8/Maps.java
@@ -1,14 +1,14 @@
 package com.codewars.kata.kyu8;
 
-import java.util.Arrays;
+import static java.util.Arrays.stream;
 
 /**
  * @see <a href="https://www.codewars.com/kata/57f781872e3d8ca2a000007e">Lost Without a Map</a>
  */
-public class Maps
+class Maps
 {
-    public static int[] kata(int[] arr)
+    static int[] map(int[] arr)
     {
-        return Arrays.stream(arr).map(i -> 2 * i).toArray();
+        return stream(arr).map(i -> 2 * i).toArray();
     }
 }
