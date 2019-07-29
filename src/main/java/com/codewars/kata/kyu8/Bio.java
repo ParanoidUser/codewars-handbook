a589d396099233a3f18508dec7b3369d677e2676
Paranoid User <dmitriy.fedoriv@gmail.com>
2019-07-28 21:07:39 -0700

    https://www.codewars.com/kata/5556282156230d0e5e000089

--- src/main/java/com/codewars/kata/kyu8/Bio.java
+++ src/main/java/com/codewars/kata/kyu8/Bio.java
@@ -1,11 +1,11 @@
 package com.codewars.kata.kyu8;
 
 /**
- * @see <a href="https://www.codewars.com/kata/dna-to-rna-conversion">DNA to RNA Conversion</a>
+ * @see <a href="https://www.codewars.com/kata/5556282156230d0e5e000089">DNA to RNA Conversion</a>
  */
-public class Bio
+class Bio
 {
-    public static String kata(String dna)
+    static String dnaToRna(String dna)
     {
         return dna.replace("T", "U");
     }
