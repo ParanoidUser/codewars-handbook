70ba64f21b4fc3cd616266fa990b91e25b14dc75
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2019-07-28 23:30:40 -0700

    https://www.codewars.com/kata/573f5c61e7752709df0005d2

--- src/test/java/com/codewars/kata/kyu8/MergeSortedArraysTest.java
+++ src/test/java/com/codewars/kata/kyu8/MergeSortedArraysTest.java
@@ -4,12 +4,12 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 import org.junit.jupiter.api.Test;
 
-public class MergeSortedArraysTest
+class MergeSortedArraysTest
 {
     @Test
     void sample()
     {
-        assertArrayEquals(new int[] { 1, 2, 3, 4, 5, 6 }, MergeSortedArrays.kata(new int[] { 1, 3, 5 }, new int[] { 2, 4, 6 }));
-        assertArrayEquals(new int[] { 2, 4, 6, 8 }, MergeSortedArrays.kata(new int[] { 2, 4, 8 }, new int[] { 2, 4, 6 }));
+        assertArrayEquals(new int[] { 1, 2, 3, 4, 5, 6 }, MergeSortedArrays.mergeArrays(new int[] { 1, 3, 5 }, new int[] { 2, 4, 6 }));
+        assertArrayEquals(new int[] { 2, 4, 6, 8 }, MergeSortedArrays.mergeArrays(new int[] { 2, 4, 8 }, new int[] { 2, 4, 6 }));
     }
 }
