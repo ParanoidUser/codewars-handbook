17f97b5a0e572337c20fb774161fc47227b1680d
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2019-07-28 23:32:57 -0700

    https://www.codewars.com/kata/57eaeb9578748ff92a000009

--- src/main/java/com/codewars/kata/kyu8/MixedSum.java
+++ src/main/java/com/codewars/kata/kyu8/MixedSum.java
@@ -5,9 +5,9 @@ import java.util.List;
 /**
  * @see <a href="https://www.codewars.com/kata/57eaeb9578748ff92a000009">Sum Mixed Array</a>
  */
-public class MixedSum
+class MixedSum
 {
-    public int sum(List<?> mixed)
+    static int sum(List<?> mixed)
     {
         return mixed.stream().mapToInt(n -> Integer.parseInt(n.toString())).sum();
     }
