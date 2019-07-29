c684e1c2f18e492123c9c264ac8ed8237bba0265
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2019-07-28 21:52:39 -0700

    https://www.codewars.com/kata/5b077ebdaf15be5c7f000077

--- src/test/java/com/codewars/kata/kyu8/CountingSheepTest.java
+++ src/test/java/com/codewars/kata/kyu8/CountingSheepTest.java
@@ -4,13 +4,13 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class CountingSheepTest
+class CountingSheepTest
 {
     @Test
     void sample()
     {
-        assertEquals("1 sheep...", CountingSheep.kata(1));
-        assertEquals("1 sheep...2 sheep...", CountingSheep.kata(2));
-        assertEquals("1 sheep...2 sheep...3 sheep...", CountingSheep.kata(3));
+        assertEquals("1 sheep...", CountingSheep.countingSheep(1));
+        assertEquals("1 sheep...2 sheep...", CountingSheep.countingSheep(2));
+        assertEquals("1 sheep...2 sheep...3 sheep...", CountingSheep.countingSheep(3));
     }
 }
