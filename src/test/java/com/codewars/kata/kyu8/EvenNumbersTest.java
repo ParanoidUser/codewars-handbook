d57ea5f799092c87d8dec3570575bb5774218d18
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2019-07-28 22:25:45 -0700

    https://www.codewars.com/kata/55edaba99da3a9c84000003b

--- src/test/java/com/codewars/kata/kyu8/EvenNumbersTest.java
+++ src/test/java/com/codewars/kata/kyu8/EvenNumbersTest.java
@@ -4,13 +4,13 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
-public class EvenNumbersTest
+class EvenNumbersTest
 {
     @Test
     void sample()
     {
-        assertArrayEquals(new int[]{ 2, 4, 6 }, EvenNumbers.kata(new int[]{ 1, 2, 3, 4, 5, 6 }, 2));
-        assertArrayEquals(new int[]{ 3, 6 }, EvenNumbers.kata(new int[]{ 1, 2, 3, 4, 5, 6 }, 3));
-        assertArrayEquals(new int[]{ 0, 4 }, EvenNumbers.kata(new int[]{ 0, 1, 2, 3, 4, 5, 6 }, 4));
+        assertArrayEquals(new int[] { 2, 4, 6 }, EvenNumbers.divisibleBy(new int[] { 1, 2, 3, 4, 5, 6 }, 2));
+        assertArrayEquals(new int[] { 3, 6 }, EvenNumbers.divisibleBy(new int[] { 1, 2, 3, 4, 5, 6 }, 3));
+        assertArrayEquals(new int[] { 0, 4 }, EvenNumbers.divisibleBy(new int[] { 0, 1, 2, 3, 4, 5, 6 }, 4));
     }
 }
