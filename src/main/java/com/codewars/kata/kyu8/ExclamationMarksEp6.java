f03f710f22136fb287947901d1a92549ec6d5116
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2019-07-28 22:37:37 -0700

    https://www.codewars.com/kata/57faf7275c991027af000679

--- src/main/java/com/codewars/kata/kyu8/ExclamationMarksEp6.java
+++ src/main/java/com/codewars/kata/kyu8/ExclamationMarksEp6.java
@@ -1,13 +1,13 @@
 package com.codewars.kata.kyu8;
 
 /**
- * @see <a href="https://www.codewars.com/kata/exclamation-marks-series-number-6-remove-n-exclamation-marks-in-the-sentence-from-left-to-right">Exclamation marks series #6: Remove n exclamation marks in the sentence from left to right</a>
+ * @see <a href="https://www.codewars.com/kata/57faf7275c991027af000679">Exclamation marks series #6: Remove n exclamation marks in the sentence from left to right</a>
  */
-public class ExclamationMarksEp6
+class ExclamationMarksEp6
 {
-    public static String kata(String s, int n)
+    static String remove(String s, int n)
     {
-        while(n-- > 0)
+        while (n-- > 0)
         {
             s = s.replaceFirst("!", "");
         }
--- src/test/java/com/codewars/kata/kyu8/ExclamationMarksEp6Test.java
+++ src/test/java/com/codewars/kata/kyu8/ExclamationMarksEp6Test.java
@@ -4,22 +4,22 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class ExclamationMarksEp6Test
+class ExclamationMarksEp6Test
 {
     @Test
     void sample()
     {
-        assertEquals("Hi", ExclamationMarksEp6.kata("Hi!", 1));
-        assertEquals("Hi", ExclamationMarksEp6.kata("Hi!", 1));
-        assertEquals("Hi", ExclamationMarksEp6.kata("Hi!", 100));
-        assertEquals("Hi!!", ExclamationMarksEp6.kata("Hi!!!", 1));
-        assertEquals("Hi", ExclamationMarksEp6.kata("Hi!!!", 100));
-        assertEquals("Hi", ExclamationMarksEp6.kata("!Hi", 1));
-        assertEquals("Hi!", ExclamationMarksEp6.kata("!Hi!", 1));
-        assertEquals("Hi", ExclamationMarksEp6.kata("!Hi!", 100));
-        assertEquals("!!Hi !!hi!!! !hi", ExclamationMarksEp6.kata("!!!Hi !!hi!!! !hi", 1));
-        assertEquals("Hi !!hi!!! !hi", ExclamationMarksEp6.kata("!!!Hi !!hi!!! !hi", 3));
-        assertEquals("Hi hi!!! !hi", ExclamationMarksEp6.kata("!!!Hi !!hi!!! !hi", 5));
-        assertEquals("Hi hi hi", ExclamationMarksEp6.kata("!!!Hi !!hi!!! !hi", 100));
+        assertEquals("Hi", ExclamationMarksEp6.remove("Hi!", 1));
+        assertEquals("Hi", ExclamationMarksEp6.remove("Hi!", 1));
+        assertEquals("Hi", ExclamationMarksEp6.remove("Hi!", 100));
+        assertEquals("Hi!!", ExclamationMarksEp6.remove("Hi!!!", 1));
+        assertEquals("Hi", ExclamationMarksEp6.remove("Hi!!!", 100));
+        assertEquals("Hi", ExclamationMarksEp6.remove("!Hi", 1));
+        assertEquals("Hi!", ExclamationMarksEp6.remove("!Hi!", 1));
+        assertEquals("Hi", ExclamationMarksEp6.remove("!Hi!", 100));
+        assertEquals("!!Hi !!hi!!! !hi", ExclamationMarksEp6.remove("!!!Hi !!hi!!! !hi", 1));
+        assertEquals("Hi !!hi!!! !hi", ExclamationMarksEp6.remove("!!!Hi !!hi!!! !hi", 3));
+        assertEquals("Hi hi!!! !hi", ExclamationMarksEp6.remove("!!!Hi !!hi!!! !hi", 5));
+        assertEquals("Hi hi hi", ExclamationMarksEp6.remove("!!!Hi !!hi!!! !hi", 100));
     }
 }
