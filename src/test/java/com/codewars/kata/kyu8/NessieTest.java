71cffb6f9ca3c66e3091e7b68ac291bf38151048
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2019-07-28 23:40:24 -0700

    https://www.codewars.com/kata/55ccdf1512938ce3ac000056

--- src/test/java/com/codewars/kata/kyu8/NessieTest.java
+++ src/test/java/com/codewars/kata/kyu8/NessieTest.java
@@ -5,12 +5,12 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class NessieTest
+class NessieTest
 {
     @Test
     void sample()
     {
-        assertTrue(Nessie.kata("Your girlscout cookies are ready to ship. Your total comes to tree fiddy"));
-        assertFalse(Nessie.kata("Yo, I heard you were on the lookout for Nessie. Let me know if you need assistance."));
+        assertTrue(Nessie.isLockNessMonster("Your girlscout cookies are ready to ship. Your total comes to tree fiddy"));
+        assertFalse(Nessie.isLockNessMonster("Yo, I heard you were on the lookout for Nessie. Let me know if you need assistance."));
     }
 }
