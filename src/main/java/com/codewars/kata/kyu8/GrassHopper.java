ebf2119455c1b5fbce5cd0f5bb4608512d75a2c9
Paranoid User <dmitriy.fedoriv@gmail.com>
2019-07-28 22:51:45 -0700

    https://www.codewars.com/kata/55d24f55d7dd296eb9000030

--- src/main/java/com/codewars/kata/kyu8/GrassHopper.java
+++ src/main/java/com/codewars/kata/kyu8/GrassHopper.java
@@ -1,11 +1,11 @@
 package com.codewars.kata.kyu8;
 
 /**
- * @see <a href="https://www.codewars.com/kata/grasshopper-summation">Grasshopper - Summation</a>
+ * @see <a href="https://www.codewars.com/kata/55d24f55d7dd296eb9000030">Grasshopper - Summation</a>
  */
-public class GrassHopper
+class GrassHopper
 {
-    public static int kata(int n)
+    static int summation(int n)
     {
         return n * (n + 1) / 2;
     }
