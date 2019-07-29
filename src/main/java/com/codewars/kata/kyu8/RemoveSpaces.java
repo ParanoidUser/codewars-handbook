a109e63fab50c722d3d45f694e1dfade5f270a2f
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2019-07-29 00:08:43 -0700

    https://www.codewars.com/kata/57eae20f5500ad98e50002c5

--- src/main/java/com/codewars/kata/kyu8/RemoveSpaces.java
+++ src/main/java/com/codewars/kata/kyu8/RemoveSpaces.java
@@ -1,11 +1,11 @@
 package com.codewars.kata.kyu8;
 
 /**
- * @see <a href="https://www.codewars.com/kata/remove-string-spaces">Remove String Spaces</a>
+ * @see <a href="https://www.codewars.com/kata/57eae20f5500ad98e50002c5">Remove String Spaces</a>
  */
-public class RemoveSpaces
+class RemoveSpaces
 {
-    public static String kata(String s)
+    static String noSpace(String s)
     {
         return s.replace(" ", "");
     }
