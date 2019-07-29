0f3312c6db75a1378f8d3e62fc55e7a9b47c167f
Paranoid User <dmitriy.fedoriv@gmail.com>
2019-07-29 01:00:01 -0700

    https://www.codewars.com/kata/58261acb22be6e2ed800003a

--- src/test/java/com/codewars/kata/kyu8/VolumeOfCuboidTest.java
+++ src/test/java/com/codewars/kata/kyu8/VolumeOfCuboidTest.java
@@ -4,13 +4,13 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class VolumeOfCuboidTest
+class VolumeOfCuboidTest
 {
     @Test
     void sample()
     {
         double delta = 0.0001;
-        assertEquals(4, VolumeOfCuboid.kata(1, 2, 2), delta);
-        assertEquals(63, VolumeOfCuboid.kata(6.3, 2, 5), delta);
+        assertEquals(4, VolumeOfCuboid.getVolumeOfCuboid(1, 2, 2), delta);
+        assertEquals(63, VolumeOfCuboid.getVolumeOfCuboid(6.3, 2, 5), delta);
     }
 }
