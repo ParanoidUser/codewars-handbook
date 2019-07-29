2f461dc5a2804e18f55ec8533e605bdb1863b8fb
Paranoid User <dmitriy.fedoriv@gmail.com>
2019-07-28 20:59:14 -0700

    https://www.codewars.com/kata/57356c55867b9b7a60000bd7

--- src/test/java/com/codewars/kata/kyu8/BasicOperationsTest.java
+++ src/test/java/com/codewars/kata/kyu8/BasicOperationsTest.java
@@ -4,14 +4,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-public class BasicOperationsTest
+class BasicOperationsTest
 {
     @Test
     void sample()
     {
-        assertEquals(11, BasicOperations.kata("+", 4, 7));
-        assertEquals(-3, BasicOperations.kata("-", 15, 18));
-        assertEquals(25, BasicOperations.kata("*", 5, 5));
-        assertEquals(7, BasicOperations.kata("/", 49, 7));
+        assertEquals(11, BasicOperations.basicMath("+", 4, 7));
+        assertEquals(-3, BasicOperations.basicMath("-", 15, 18));
+        assertEquals(25, BasicOperations.basicMath("*", 5, 5));
+        assertEquals(7, BasicOperations.basicMath("/", 49, 7));
     }
 }
