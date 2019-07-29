b50fb3824d009adf65c4e21fd749c83bdb2a955f
Paranoid User <dmitriy.fedoriv@gmail.com>
2019-07-28 22:19:39 -0700

    https://www.codewars.com/kata/57e92e91b63b6cbac20001e5

--- src/test/java/com/codewars/kata/kyu8/DutyFreeTest.java
+++ src/test/java/com/codewars/kata/kyu8/DutyFreeTest.java
@@ -4,15 +4,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-public class DutyFreeTest
+class DutyFreeTest
 {
     @Test
     void sample()
     {
-        assertEquals(166, DutyFree.kata(12, 50, 1000));
-        assertEquals(294, DutyFree.kata(17, 10, 500));
-        assertEquals(357, DutyFree.kata(24, 35, 3000));
-        assertEquals(20, DutyFree.kata(1400, 35, 10000));
-        assertEquals(38, DutyFree.kata(700, 26, 7000));
+        assertEquals(166, DutyFree.dutyFree(12, 50, 1000));
+        assertEquals(294, DutyFree.dutyFree(17, 10, 500));
+        assertEquals(357, DutyFree.dutyFree(24, 35, 3000));
+        assertEquals(20, DutyFree.dutyFree(1400, 35, 10000));
+        assertEquals(38, DutyFree.dutyFree(700, 26, 7000));
     }
 }
