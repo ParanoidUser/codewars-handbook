93c620cc9402d5441b6559077aa91b9cc94ff5d2
Paranoid User <dmitriy.fedoriv@gmail.com>
2019-07-28 22:28:11 -0700

    https://www.codewars.com/kata/53da3dbb4a5168369a0000fe

--- src/main/java/com/codewars/kata/kyu8/EvenOrOdd.java
+++ src/main/java/com/codewars/kata/kyu8/EvenOrOdd.java
@@ -1,11 +1,11 @@
 package com.codewars.kata.kyu8;
 
 /**
- * @see <a href="https://www.codewars.com/kata/even-or-odd">Even or Odd</a>
+ * @see <a href="https://www.codewars.com/kata/53da3dbb4a5168369a0000fe">Even or Odd</a>
  */
-public class EvenOrOdd
+class EvenOrOdd
 {
-    public static String kata(int number)
+    static String even_or_odd(int number)
     {
         return number % 2 == 0 ? "Even" : "Odd";
     }
