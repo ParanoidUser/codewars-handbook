f7c2811f00d9dcbf13da45f131f32630cc6ed516
Paranoid User <dmitriy.fedoriv@gmail.com>
2019-07-28 23:32:57 -0700

    https://www.codewars.com/kata/57eaeb9578748ff92a000009

--- src/test/java/com/codewars/kata/kyu8/MixedSumTest.java
+++ src/test/java/com/codewars/kata/kyu8/MixedSumTest.java
@@ -6,18 +6,16 @@ import java.util.Arrays;
 
 import org.junit.jupiter.api.Test;
 
-public class MixedSumTest
+class MixedSumTest
 {
-    MixedSum mixedSum = new MixedSum();
-
     @Test
     void sample()
     {
-        assertEquals(10, mixedSum.sum(Arrays.asList(5, "5")));
-        assertEquals(22, mixedSum.sum(Arrays.asList(9, 3, "7", "3")));
-        assertEquals(41, mixedSum.sum(Arrays.asList("3", 6, 6, 0, "5", 8, 5, "6", 2, "0")));
-        assertEquals(42, mixedSum.sum(Arrays.asList("5", "0", 9, 3, 2, 1, "9", 6, 7)));
-        assertEquals(45, mixedSum.sum(Arrays.asList("1", "5", "8", 8, 9, 9, 2, "3")));
-        assertEquals(61, mixedSum.sum(Arrays.asList(8, 0, 0, 8, 5, 7, 2, 3, 7, 8, 6, 7)));
+        assertEquals(10, MixedSum.sum(Arrays.asList(5, "5")));
+        assertEquals(22, MixedSum.sum(Arrays.asList(9, 3, "7", "3")));
+        assertEquals(41, MixedSum.sum(Arrays.asList("3", 6, 6, 0, "5", 8, 5, "6", 2, "0")));
+        assertEquals(42, MixedSum.sum(Arrays.asList("5", "0", 9, 3, 2, 1, "9", 6, 7)));
+        assertEquals(45, MixedSum.sum(Arrays.asList("1", "5", "8", 8, 9, 9, 2, "3")));
+        assertEquals(61, MixedSum.sum(Arrays.asList(8, 0, 0, 8, 5, 7, 2, 3, 7, 8, 6, 7)));
     }
 }
