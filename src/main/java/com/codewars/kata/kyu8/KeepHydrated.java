a28a7934a5dfa86a760e9277e1319d2e8df3885f
Paranoid User <dmitriy.fedoriv@gmail.com>
2019-07-28 23:26:33 -0700

    https://www.codewars.com/kata/582cb0224e56e068d800003c

--- src/main/java/com/codewars/kata/kyu8/KeepHydrated.java
+++ src/main/java/com/codewars/kata/kyu8/KeepHydrated.java
@@ -1,11 +1,11 @@
 package com.codewars.kata.kyu8;
 
 /**
- * @see <a href="https://www.codewars.com/kata/keep-hydrated-1">Keep Hydrated!</a>
+ * @see <a href="https://www.codewars.com/kata/582cb0224e56e068d800003c">Keep Hydrated!</a>
  */
-public class KeepHydrated
+class KeepHydrated
 {
-    public static int kata(double time)
+    static int Liters(double time)
     {
         return (int) (time / 2);
     }
