45581ee6a696254e3cee29ea4cd4844c1eb092ef
Paranoid User <dmitriy.fedoriv@gmail.com>
2019-07-29 01:04:53 -0700

    https://www.codewars.com/kata/55dc4520094bbaf50e0000cb

--- src/main/java/com/codewars/kata/kyu8/WilsonPrime.java
+++ src/main/java/com/codewars/kata/kyu8/WilsonPrime.java
@@ -1,11 +1,11 @@
 package com.codewars.kata.kyu8;
 
 /**
- * @see <a href="https://www.codewars.com/kata/wilson-primes">Wilson primes</a>
+ * @see <a href="https://www.codewars.com/kata/55dc4520094bbaf50e0000cb">Wilson primes</a>
  */
-public class WilsonPrime
+class WilsonPrime
 {
-    public static boolean kata(double n)
+    static boolean am_i_wilson(double n)
     {
         return n == 5 || n == 13 || n == 563;
     }
