c636f8c19b2adfac4cfbb39a48e142720fbcc5df
Paranoid User <dmitriy.fedoriv@gmail.com>
2019-07-29 00:11:54 -0700

    https://www.codewars.com/kata/57202aefe8d6c514300001fd

--- src/main/java/com/codewars/kata/kyu8/SaleHotdogs.java
+++ src/main/java/com/codewars/kata/kyu8/SaleHotdogs.java
@@ -3,9 +3,9 @@ package com.codewars.kata.kyu8;
 /**
  * @see <a href="https://www.codewars.com/kata/57202aefe8d6c514300001fd">if..else and ternary operator</a>
  */
-public class SaleHotdogs
+class SaleHotdogs
 {
-    public static int kata(int n)
+    static int saleHotdogs(int n)
     {
         return n * (n < 5 ? 100 : n < 10 ? 95 : 90);
     }
