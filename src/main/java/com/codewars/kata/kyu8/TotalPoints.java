ccdff00806f708014a85297d339cf38daceb0f5e
Paranoid User <dmitriy.fedoriv@gmail.com>
2019-07-29 00:50:56 -0700

    https://www.codewars.com/kata/5bb904724c47249b10000131

--- src/main/java/com/codewars/kata/kyu8/TotalPoints.java
+++ src/main/java/com/codewars/kata/kyu8/TotalPoints.java
@@ -1,15 +1,15 @@
 package com.codewars.kata.kyu8;
 
-import java.util.Arrays;
+import static java.util.Arrays.stream;
 
 /**
- * @see <a href="https://www.codewars.com/kata/total-amount-of-points">Total amount of points</a>
+ * @see <a href="https://www.codewars.com/kata/5bb904724c47249b10000131">Total amount of points</a>
  */
-public class TotalPoints
+class TotalPoints
 {
-    public static int kata(String[] games)
+    static int points(String[] games)
     {
-        return Arrays.stream(games)
+        return stream(games)
                      .mapToInt(score -> score.charAt(0) - score.charAt(2))
                      .map(match -> match > 0 ? 3 : match == 0 ? 1 : 0)
                      .sum();
--- src/test/java/com/codewars/kata/kyu8/TotalPointsTest.java
+++ src/test/java/com/codewars/kata/kyu8/TotalPointsTest.java
@@ -4,15 +4,15 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class TotalPointsTest
+class TotalPointsTest
 {
     @Test
     void sample()
     {
-        assertEquals(30, TotalPoints.kata(new String[]{ "1:0", "2:0", "3:0", "4:0", "2:1", "3:1", "4:1", "3:2", "4:2", "4:3" }));
-        assertEquals(10, TotalPoints.kata(new String[]{ "1:1", "2:2", "3:3", "4:4", "2:2", "3:3", "4:4", "3:3", "4:4", "4:4" }));
-        assertEquals(0, TotalPoints.kata(new String[]{ "0:1", "0:2", "0:3", "0:4", "1:2", "1:3", "1:4", "2:3", "2:4", "3:4" }));
-        assertEquals(15, TotalPoints.kata(new String[]{ "1:0", "2:0", "3:0", "4:0", "2:1", "1:3", "1:4", "2:3", "2:4", "3:4" }));
-        assertEquals(12, TotalPoints.kata(new String[]{ "1:0", "2:0", "3:0", "4:4", "2:2", "3:3", "1:4", "2:3", "2:4", "3:4" }));
+        assertEquals(30, TotalPoints.points(new String[]{ "1:0", "2:0", "3:0", "4:0", "2:1", "3:1", "4:1", "3:2", "4:2", "4:3" }));
+        assertEquals(10, TotalPoints.points(new String[]{ "1:1", "2:2", "3:3", "4:4", "2:2", "3:3", "4:4", "3:3", "4:4", "4:4" }));
+        assertEquals(0, TotalPoints.points(new String[]{ "0:1", "0:2", "0:3", "0:4", "1:2", "1:3", "1:4", "2:3", "2:4", "3:4" }));
+        assertEquals(15, TotalPoints.points(new String[]{ "1:0", "2:0", "3:0", "4:0", "2:1", "1:3", "1:4", "2:3", "2:4", "3:4" }));
+        assertEquals(12, TotalPoints.points(new String[]{ "1:0", "2:0", "3:0", "4:4", "2:2", "3:3", "1:4", "2:3", "2:4", "3:4" }));
     }
 }
