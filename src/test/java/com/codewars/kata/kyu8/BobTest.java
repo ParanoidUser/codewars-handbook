7854677670c336faa143e02b82218c998895c871
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2019-07-28 21:09:50 -0700

    https://www.codewars.com/kata/5875b200d520904a04000003

--- src/test/java/com/codewars/kata/kyu8/BobTest.java
+++ src/test/java/com/codewars/kata/kyu8/BobTest.java
@@ -4,12 +4,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-public class BobTest
+class BobTest
 {
     @Test
     void sample()
     {
-        assertEquals(0, Bob.kata(10, 5, 5));
-        assertEquals(10, Bob.kata(100, 60, 50));
+        assertEquals(0, Bob.enough(10, 5, 5));
+        assertEquals(10, Bob.enough(100, 60, 50));
+        assertEquals(0, Bob.enough(20, 5, 5));
     }
 }
