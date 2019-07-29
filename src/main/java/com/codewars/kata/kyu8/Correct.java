4e64829f20b7142fe0330c8249ae9bf8a564fd8c
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2019-07-28 21:47:58 -0700

    https://www.codewars.com/kata/577bd026df78c19bca0002c0

--- src/main/java/com/codewars/kata/kyu8/Correct.java
+++ src/main/java/com/codewars/kata/kyu8/Correct.java
@@ -1,11 +1,11 @@
 package com.codewars.kata.kyu8;
 
 /**
- * @see <a href="https://www.codewars.com/kata/correct-the-mistakes-of-the-character-recognition-software">Correct the mistakes of the character recognition software</a>
+ * @see <a href="https://www.codewars.com/kata/577bd026df78c19bca0002c0">Correct the mistakes of the character recognition software</a>
  */
-public class Correct
+class Correct
 {
-    public static String kata(String string)
+    static String correct(String string)
     {
         return string.replace("5", "S")
                      .replace("0", "O")
