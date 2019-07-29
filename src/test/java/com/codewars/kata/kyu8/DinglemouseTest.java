e7338bdd5eee505c2d9be2792c68ed83db2de7bb
Paranoid User <dmitriy.fedoriv@gmail.com>
2019-07-28 22:13:32 -0700

    https://www.codewars.com/kata/5a6663e9fd56cb5ab800008b

--- src/test/java/com/codewars/kata/kyu8/DinglemouseTest.java
+++ src/test/java/com/codewars/kata/kyu8/DinglemouseTest.java
@@ -4,13 +4,13 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 import org.junit.jupiter.api.Test;
 
-public class DinglemouseTest
+class DinglemouseTest
 {
     @Test
     void sample()
     {
-        assertArrayEquals(new int[] { 1, 15, 15 }, Dinglemouse.kata(1));
-        assertArrayEquals(new int[] { 2, 24, 24 }, Dinglemouse.kata(2));
-        assertArrayEquals(new int[] { 10, 56, 64 }, Dinglemouse.kata(10));
+        assertArrayEquals(new int[] { 1, 15, 15 }, Dinglemouse.humanYearsCatYearsDogYears(1));
+        assertArrayEquals(new int[] { 2, 24, 24 }, Dinglemouse.humanYearsCatYearsDogYears(2));
+        assertArrayEquals(new int[] { 10, 56, 64 }, Dinglemouse.humanYearsCatYearsDogYears(10));
     }
 }
