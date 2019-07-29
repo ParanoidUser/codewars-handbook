dc01c654ed203f578cbca956e5e8157cc138af6c
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2019-07-28 23:49:37 -0700

    https://www.codewars.com/kata/56dec885c54a926dcd001095

--- src/main/java/com/codewars/kata/kyu8/OppositeNumber.java
+++ src/main/java/com/codewars/kata/kyu8/OppositeNumber.java
@@ -1,11 +1,11 @@
 package com.codewars.kata.kyu8;
 
 /**
- * @see <a href="https://www.codewars.com/kata/opposite-number">Opposite number</a>
+ * @see <a href="https://www.codewars.com/kata/56dec885c54a926dcd001095">Opposite number</a>
  */
-public class OppositeNumber
+class OppositeNumber
 {
-    public static int kata(int number)
+    static int opposite(int number)
     {
         return -number;
     }
