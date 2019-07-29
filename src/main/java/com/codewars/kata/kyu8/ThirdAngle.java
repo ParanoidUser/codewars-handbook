f0d860dea9bf05f98a37b841ff1819944ceaada0
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2019-07-29 00:48:21 -0700

    https://www.codewars.com/kata/5a023c426975981341000014

--- src/main/java/com/codewars/kata/kyu8/ThirdAngle.java
+++ src/main/java/com/codewars/kata/kyu8/ThirdAngle.java
@@ -3,9 +3,9 @@ package com.codewars.kata.kyu8;
 /**
  * @see <a href="https://www.codewars.com/kata/5a023c426975981341000014">Third Angle of a Triangle</a>
  */
-public class ThirdAngle
+class ThirdAngle
 {
-    public static int otherAngle(int angle1, int angle2)
+    static int otherAngle(int angle1, int angle2)
     {
         return 180 - angle1 - angle2;
     }
