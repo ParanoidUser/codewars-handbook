74fd02cb76eee2e0cb2f3c3959ae15209243049b
Paranoid User <dmitriy.fedoriv@gmail.com>
2019-07-28 21:38:44 -0700

    https://www.codewars.com/kata/557cd6882bfa3c8a9f0000c1

--- src/test/java/com/codewars/kata/kyu8/CharProblemTest.java
+++ src/test/java/com/codewars/kata/kyu8/CharProblemTest.java
@@ -4,13 +4,13 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class CharProblemTest
+class CharProblemTest
 {
     @Test
     void sample()
     {
-        assertEquals(5, CharProblem.kata("5 years old"));
-        assertEquals(9, CharProblem.kata("9 years old"));
-        assertEquals(1, CharProblem.kata("1 year old"));
+        assertEquals(5, CharProblem.howOld("5 years old"));
+        assertEquals(9, CharProblem.howOld("9 years old"));
+        assertEquals(1, CharProblem.howOld("1 year old"));
     }
 }
