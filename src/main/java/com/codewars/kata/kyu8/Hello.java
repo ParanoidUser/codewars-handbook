fc8d35756a116e1aa2cf873176c11265a1152abb
Paranoid User <dmitriy.fedoriv@gmail.com>
2019-07-28 22:56:16 -0700

    https://www.codewars.com/kata/5302d846be2a9189af0001e4

--- src/main/java/com/codewars/kata/kyu8/Hello.java
+++ src/main/java/com/codewars/kata/kyu8/Hello.java
@@ -1,11 +1,11 @@
 package com.codewars.kata.kyu8;
 
 /**
- * @see <a href="https://www.codewars.com/kata/welcome-to-the-city">Welcome to the City</a>
+ * @see <a href="https://www.codewars.com/kata/5302d846be2a9189af0001e4">Welcome to the City</a>
  */
-public class Hello
+class Hello
 {
-    public static String kata(String[] name, String city, String state)
+    static String sayHello(String[] name, String city, String state)
     {
         return String.format("Hello, %s! Welcome to %s, %s!", String.join(" ", name), city, state);
     }
--- src/test/java/com/codewars/kata/kyu8/HelloTest.java
+++ src/test/java/com/codewars/kata/kyu8/HelloTest.java
@@ -4,12 +4,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-public class HelloTest
+class HelloTest
 {
     @Test
     void sample()
     {
         String[] name = { "John", "Smith" };
-        assertEquals("Hello, John Smith! Welcome to Phoenix, Arizona!", Hello.kata(name, "Phoenix", "Arizona"));
+        assertEquals("Hello, John Smith! Welcome to Phoenix, Arizona!", Hello.sayHello(name, "Phoenix", "Arizona"));
     }
 }
