bfea8250c791394a25673da31a9b72039d0aff25
Paranoid User <dmitriy.fedoriv@gmail.com>
2019-07-28 22:39:07 -0700

    https://www.codewars.com/kata/57eae65a4321032ce000002d

--- src/test/java/com/codewars/kata/kyu8/FakeBinaryTest.java
+++ src/test/java/com/codewars/kata/kyu8/FakeBinaryTest.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-public class FakeBinaryTest
+class FakeBinaryTest
 {
     @Test
     void sample()
