4314450d55ed9dd4434466766dffbff67cf7b89a
Paranoid User <dmitriy.fedoriv@gmail.com>
2019-07-29 00:24:35 -0700

    https://www.codewars.com/kata/55a75e2d0803fea18f00009d

--- src/main/java/com/codewars/kata/kyu8/Slope.java
+++ src/main/java/com/codewars/kata/kyu8/Slope.java
@@ -1,11 +1,11 @@
 package com.codewars.kata.kyu8;
 
 /**
- * @see <a href="https://www.codewars.com/kata/find-the-slope">Find the Slope</a>
+ * @see <a href="https://www.codewars.com/kata/55a75e2d0803fea18f00009d">Find the Slope</a>
  */
-public class Slope
+class Slope
 {
-    public static String kata(int[] points)
+    static String slope(int[] points)
     {
         return points[0] == points[2] ? "undefined" : "" + (points[3] - points[1]) / (points[2] - points[0]);
     }
--- src/test/java/com/codewars/kata/kyu8/SlopeTest.java
+++ src/test/java/com/codewars/kata/kyu8/SlopeTest.java
@@ -4,16 +4,16 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-public class SlopeTest
+class SlopeTest
 {
     @Test
     void sample()
     {
-        assertEquals("0", Slope.kata(new int[] { 19, 3, 20, 3 }));
-        assertEquals("undefined", Slope.kata(new int[] { -7, 2, -7, 4 }));
-        assertEquals("5", Slope.kata(new int[] { 10, 50, 30, 150 }));
-        assertEquals("-5", Slope.kata(new int[] { 15, 45, 12, 60 }));
-        assertEquals("6", Slope.kata(new int[] { 10, 20, 20, 80 }));
-        assertEquals("undefined", Slope.kata(new int[] { -10, 6, -10, 3 }));
+        assertEquals("0", Slope.slope(new int[] { 19, 3, 20, 3 }));
+        assertEquals("undefined", Slope.slope(new int[] { -7, 2, -7, 4 }));
+        assertEquals("5", Slope.slope(new int[] { 10, 50, 30, 150 }));
+        assertEquals("-5", Slope.slope(new int[] { 15, 45, 12, 60 }));
+        assertEquals("6", Slope.slope(new int[] { 10, 20, 20, 80 }));
+        assertEquals("undefined", Slope.slope(new int[] { -10, 6, -10, 3 }));
     }
 }
