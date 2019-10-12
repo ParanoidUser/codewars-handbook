19252b7833e5d8ce523cb6b96f66e0718a2aa669
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2019-10-11 18:41:28 -0700

    https://www.codewars.com/kata/5ae62fcf252e66d44d00008e

--- src/main/java/com/codewars/kata/kyu8/ExpressionsMatter.java
+++ src/main/java/com/codewars/kata/kyu8/ExpressionsMatter.java
@@ -3,7 +3,6 @@ package com.codewars.kata.kyu8;
 /**
  * @see <a href="https://www.codewars.com/kata/5ae62fcf252e66d44d00008e">Expressions Matter</a>
  */
-
 class ExpressionsMatter
 {
     static class Kata
