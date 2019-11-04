2e36db250658bc45be8ac51f7168f7584ebd2c41
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2019-11-03 23:26:38 -0800

    Kata based project structure

--- src/main/kotlin/com/codewars/kata/kyu8/Elevator.kt
+++
@@ -1,6 +0,0 @@
-package com.codewars.kata.kyu8
-
-/**
- * @see [Closest elevator](https://www.codewars.com/kata/closest-elevator)
- */
-fun elevator(left: Int, right: Int, call: Int) = if (Math.abs(left - call) < Math.abs(right - call)) "left" else "right"
