216d8a0e97b2d60459a1b7793766ba558b77932f
Paranoid User <pilgrim.whynot@gmail.com>
2019-11-03 23:26:38 -0800

    Kata based project structure

--- src/main/java/com/codewars/kata/kyu8/ToString.java
+++
@@ -1,9 +0,0 @@
-package com.codewars.kata.kyu8;
-
-/**
- * @see <a href="https://www.codewars.com/kata/53934feec44762736c00044b">Number toString</a>
- */
-class ToString
-{
-    static String a = "123";
-}
--- src/main/kotlin/com/codewars/kata/kyu8/GonnaSurvive.kt
+++
@@ -1,6 +0,0 @@
-package com.codewars.kata.kyu8
-
-/**
- * @see [Is he gonna survive?](https://www.codewars.com/kata/is-he-gonna-survive)
- */
-fun hero(bullets: Int, dragons: Int) = bullets >= 2 * dragons
