a79672830cfa300fa27949bff67166bdc6d0db1d
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2019-11-04 10:45:57 -0800

    https://www.codewars.com/kata/101-dalmatians-squash-the-bugs-not-the-dogs

---
+++ kata/kyu8/101-dalmatians-squash-the-bugs-not-the-dogs/main/ZywOo.java
@@ -0,0 +1,7 @@
+class ZywOo {
+  static String howManyDalmatians(int number) {
+    return number <= 10 ? "Hardly any" :
+           number <= 50 ? "More than a handful!" :
+           number == 101 ? "101 DALMATIANS!!!" : "Woah that's a lot of dogs!";
+  }
+}
