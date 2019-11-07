6828bbccf744dd2b19a26e22c42a67f0a3ddf1ef
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2019-11-06 18:18:55 -0800

    https://www.codewars.com/kata/count-odd-numbers-below-n

--- kata/8 kyu/count-odd-numbers-below-n/main/Kata.java
+++ kata/8 kyu/count-odd-numbers-below-n/main/Kata.java
@@ -1,7 +1,5 @@
-import java.util.stream.IntStream;
-
 class Kata {
   static int oddCount(int n) {
-    return (int) IntStream.range(0, n).filter(i -> i % 2 != 0).count();
+    return n / 2;
   }
 }
