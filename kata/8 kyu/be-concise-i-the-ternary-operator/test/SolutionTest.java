fb20c0e22d5467c16d9e585b61ef38307c83832a
Paranoid User <pilgrim.whynot@gmail.com>
2019-11-06 16:54:01 -0800

    https://www.codewars.com/kata/be-concise-i-the-ternary-operator

---
+++ kata/8 kyu/be-concise-i-the-ternary-operator/test/SolutionTest.java
@@ -0,0 +1,25 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class SolutionTest {
+  @Test
+  public void basicTests() {
+    assertEquals("You're a(n) kid", C.describeAge(9));
+    assertEquals("You're a(n) kid", C.describeAge(10));
+    assertEquals("You're a(n) kid", C.describeAge(11));
+    assertEquals("You're a(n) kid", C.describeAge(12));
+    assertEquals("You're a(n) teenager", C.describeAge(13));
+    assertEquals("You're a(n) teenager", C.describeAge(14));
+    assertEquals("You're a(n) teenager", C.describeAge(15));
+    assertEquals("You're a(n) teenager", C.describeAge(16));
+    assertEquals("You're a(n) teenager", C.describeAge(17));
+    assertEquals("You're a(n) adult", C.describeAge(18));
+    assertEquals("You're a(n) adult", C.describeAge(19));
+    assertEquals("You're a(n) adult", C.describeAge(63));
+    assertEquals("You're a(n) adult", C.describeAge(64));
+    assertEquals("You're a(n) elderly", C.describeAge(65));
+    assertEquals("You're a(n) elderly", C.describeAge(66));
+    assertEquals("You're a(n) elderly", C.describeAge(100));
+  }
+}
