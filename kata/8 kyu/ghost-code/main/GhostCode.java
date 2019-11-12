5ad913717b862e98d483de201fcd36d4d0d2aa4b
Paranoid User <pilgrim.whynot@gmail.com>
2019-11-11 18:20:47 -0800

    https://www.codewars.com/kata/ghost-code

---
+++ kata/8 kyu/ghost-code/main/GhostCode.java
@@ -0,0 +1,5 @@
+class GhostCode {
+  static String helloName(String name) {
+    return "Hello " + (name == null || name.isEmpty() ? "world!" : "my name is " + name);
+  }
+}
---
+++ kata/8 kyu/ghost-code/test/GhostCodeTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class GhostCodeTest {
+  @Test
+  public void exampleTests() {
+    assertEquals("Hello my name is Javatlacati", GhostCode.helloName("Javatlacati"));
+  }
+}
