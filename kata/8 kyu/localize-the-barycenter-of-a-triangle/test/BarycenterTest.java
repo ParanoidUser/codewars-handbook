60763252c7b6336ff45ed26e0f8ea545d162369c
Paranoid User <pilgrim.whynot@gmail.com>
2019-11-13 00:35:03 -0800

    https://www.codewars.com/kata/localize-the-barycenter-of-a-triangle

--- kata/8 kyu/localize-the-barycenter-of-a-triangle/test/BarycenterTest.java
+++ kata/8 kyu/localize-the-barycenter-of-a-triangle/test/BarycenterTest.java
@@ -1,11 +1,11 @@
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 import org.junit.Test;
 
 public class BarycenterTest {
   @Test
   public void test() {
-    assertEquals(new double[] {8.6667, 6.6667}, Barycenter.barTriang(new double[] {4, 6}, new double[] {12, 4}, new double[] {10, 10}));
-    assertEquals(new double[] {7.3333, 4.6667}, Barycenter.barTriang(new double[] {4, 2}, new double[] {12, 2}, new double[] {6, 10}));
+    assertArrayEquals(new double[] {8.6667, 6.6667}, Barycenter.barTriang(new double[] {4, 6}, new double[] {12, 4}, new double[] {10, 10}));
+    assertArrayEquals(new double[] {7.3333, 4.6667}, Barycenter.barTriang(new double[] {4, 2}, new double[] {12, 2}, new double[] {6, 10}));
   }
 }
