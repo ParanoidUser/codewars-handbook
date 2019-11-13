8b7d0b86525281b41e6fb3b7e04ea1fec21bb40b
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2019-11-13 00:26:38 -0800

    https://www.codewars.com/kata/multiply

---
+++ kata/8 kyu/multiply/test/SolutionTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class SolutionTest {
+  @Test
+  public void testSomething() {
+    assertEquals(4.0, Multiply.multiply(2.0, 2.0), 0.1);
+  }
+}
