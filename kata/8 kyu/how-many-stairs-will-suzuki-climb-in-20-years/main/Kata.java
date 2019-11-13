2430d47062a5cde418a584a2ac5784b4bc32cb60
Paranoid User <pilgrim.whynot@gmail.com>
2019-11-13 01:00:07 -0800

    Alert: Potential overflow in int multiplication before it is converted to long by use in a return context

--- kata/8 kyu/how-many-stairs-will-suzuki-climb-in-20-years/main/Kata.java
+++ kata/8 kyu/how-many-stairs-will-suzuki-climb-in-20-years/main/Kata.java
@@ -2,6 +2,6 @@ import java.util.Arrays;
 
 class Kata {
   static long stairsIn20(int[][] stairs) {
-    return 20 * Arrays.stream(stairs).flatMapToInt(Arrays::stream).sum();
+    return 20L * Arrays.stream(stairs).flatMapToInt(Arrays::stream).sum();
   }
 }
