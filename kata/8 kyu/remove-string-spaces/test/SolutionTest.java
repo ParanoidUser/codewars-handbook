e5c212d3bbdc340d518d17fc951a18f9b11f4c0d
Paranoid User <pilgrim.whynot@gmail.com>
2019-11-13 18:36:31 -0800

    https://www.codewars.com/kata/remove-string-spaces

---
+++ kata/8 kyu/remove-string-spaces/test/SolutionTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class SolutionTest {
+  @Test
+  public void testSomething() {
+    assertEquals("8j8mBliB8gimjB8B8jlB", Kata.noSpace("8 j 8   mBliB8g  imjB8B8  jl  B"));
+    assertEquals("88Bifk8hB8BB8BBBB888chl8BhBfd", Kata.noSpace("8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd"));
+    assertEquals("8aaaaaddddr", Kata.noSpace("8aaaaa dddd r     "));
+    assertEquals("jfBmgklf8hg88lbe8", Kata.noSpace("jfBm  gk lf8hg  88lbe8 "));
+    assertEquals("8jaam", Kata.noSpace("8j aam"));
+  }
+}
