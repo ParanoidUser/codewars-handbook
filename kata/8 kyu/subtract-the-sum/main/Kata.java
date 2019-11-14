a46c08f7632be323666e9549ac801f353d07a473
Paranoid User <pilgrim.whynot@gmail.com>
2019-11-14 14:17:30 -0800

    https://www.codewars.com/kata/subtract-the-sum

---
+++ kata/8 kyu/subtract-the-sum/main/Kata.java
@@ -0,0 +1,5 @@
+class Kata {
+  static String subtractSum(int n) {
+    return "apple";
+  }
+}
---
+++ kata/8 kyu/subtract-the-sum/test/SolutionTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SolutionTest {
+  @Test
+  public void basicTest() {
+    assertEquals("apple", Kata.subtractSum(10));
+  }
+}
