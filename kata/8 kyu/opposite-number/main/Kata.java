7039691f577f793f5dff75e1827072b40c3287ce
Paranoid User <pilgrim.whynot@gmail.com>
2019-11-13 16:02:05 -0800

    https://www.codewars.com/kata/opposite-number

---
+++ kata/8 kyu/opposite-number/main/Kata.java
@@ -0,0 +1,5 @@
+class Kata {
+  static int opposite(int number) {
+    return -number;
+  }
+}
---
+++ kata/8 kyu/opposite-number/test/OppositeExampleTests.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class OppositeExampleTests {
+  @Test
+  public void tests() {
+    assertEquals(-1, Kata.opposite(1));
+  }
+}
