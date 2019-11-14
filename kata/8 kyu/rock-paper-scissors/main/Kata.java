e8df4e18c5b2c2dd502f7d4b2718d1c5e3420a7e
Paranoid User <pilgrim.whynot@gmail.com>
2019-11-13 19:00:00 -0800

    https://www.codewars.com/kata/rock-paper-scissors

---
+++ kata/8 kyu/rock-paper-scissors/main/Kata.java
@@ -0,0 +1,5 @@
+class Kata {
+  static String rps(String p1, String p2) {
+    return p1.equals(p2) ? "Draw!" : "Player " + ("rockscissors paperrock scissorspaper".contains(p1 + p2) ? 1 : 2) + " won!";
+  }
+}
