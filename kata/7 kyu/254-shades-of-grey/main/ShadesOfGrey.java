358548172dac96c2f3da3126947d6fdf2c78604a
Paranoid User <pilgrim.whynot@gmail.com>
2019-11-14 21:26:14 -0800

    https://www.codewars.com/kata/254-shades-of-grey

---
+++ kata/7 kyu/254-shades-of-grey/main/ShadesOfGrey.java
@@ -0,0 +1,7 @@
+import static java.util.stream.IntStream.range;
+
+class ShadesOfGrey {
+  static String[] shadesOfGrey(int num) {
+    return range(1, Math.min(++num, 255)).mapToObj(i -> String.format("#%1$02x%1$02x%1$02x", i)).toArray(String[]::new);
+  }
+}
