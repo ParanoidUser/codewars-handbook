2d733bda639bd32d8bdd382b96aa6f385889bfed
Paranoid User <pilgrim.whynot@gmail.com>
2019-11-14 20:51:19 -0800

    https://www.codewars.com/kata/21-sticks

---
+++ kata/7 kyu/21-sticks/test/SolutionTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class SolutionTest {
+  @Test
+  public void sampleTest() {
+    assertEquals(3, Kata.makeMove(3));
+  }
+}
