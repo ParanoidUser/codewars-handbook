4b4dd4fb0ce1aa5146758694ffefdb066dda597f
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2019-11-14 17:47:08 -0800

    https://www.codewars.com/kata/the-feast-of-many-beasts

---
+++ kata/8 kyu/the-feast-of-many-beasts/main/Kata.java
@@ -0,0 +1,5 @@
+class Kata {
+  static boolean feast(String beast, String dish) {
+    return beast.charAt(0) == dish.charAt(0) && beast.charAt(beast.length() - 1) == dish.charAt(dish.length() - 1);
+  }
+}
