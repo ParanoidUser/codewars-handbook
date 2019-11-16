c4b2e1d97ebe10f896a1e2e797bc4dcea7c53d72
Paranoid User <pilgrim.whynot@gmail.com>
2019-11-15 20:36:56 -0800

    https://www.codewars.com/kata/antistring

---
+++ kata/7 kyu/antistring/main/JomoPipi.java
@@ -0,0 +1,8 @@
+class JomoPipi {
+  static String antiString(String str) {
+    return str.chars()
+        .map(c -> (char) ((c < 'A' ? 105 : 187) - c))
+        .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+        .reverse().toString();
+  }
+}
