637ad740d01d20328ada6ff527eb921833f3e386
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2019-11-16 23:44:11 -0800

    https://www.codewars.com/kata/balanced-number-special-numbers-series-number-1

---
+++ kata/7 kyu/balanced-number-special-numbers-series-number-1/main/Solution.java
@@ -0,0 +1,8 @@
+class Solution {
+  static String balancedNum(long number) {
+    var s = String.valueOf(number);
+    int l = s.substring(0, s.length() / 2 - (s.length() % 2 == 0 ? 1 : 0)).chars().sum();
+    int r = s.substring(s.length() / 2 + 1).chars().sum();
+    return (l == r ? "" : "Not ") + "Balanced";
+  }
+}
