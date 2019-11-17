2bc67c7bef85bfa5230ee18529eab739828dd7d8
Paranoid User <pilgrim.whynot@gmail.com>
2019-11-16 22:46:09 -0800

    https://www.codewars.com/kata/alphabetical-sequence

--- kata/7 kyu/alphabetical-sequence/test/SolutionTests.java
+++ kata/7 kyu/alphabetical-sequence/test/SolutionTests.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 
 public class SolutionTests {
   @Test
-  public void BasicTests() {
+  public void basicTests() {
     assertThat(Solution.alphaSeq("ZpglnRxqenU"), is("Eeeee,Ggggggg,Llllllllllll,Nnnnnnnnnnnnnn,Nnnnnnnnnnnnnn,Pppppppppppppppp,Qqqqqqqqqqqqqqqqq,Rrrrrrrrrrrrrrrrrr,Uuuuuuuuuuuuuuuuuuuuu,Xxxxxxxxxxxxxxxxxxxxxxxx,Zzzzzzzzzzzzzzzzzzzzzzzzzz"));
     assertThat(Solution.alphaSeq("NyffsGeyylB"), is("Bb,Eeeee,Ffffff,Ffffff,Ggggggg,Llllllllllll,Nnnnnnnnnnnnnn,Sssssssssssssssssss,Yyyyyyyyyyyyyyyyyyyyyyyyy,Yyyyyyyyyyyyyyyyyyyyyyyyy,Yyyyyyyyyyyyyyyyyyyyyyyyy"));
     assertThat(Solution.alphaSeq("MjtkuBovqrU"), is("Bb,Jjjjjjjjjj,Kkkkkkkkkkk,Mmmmmmmmmmmmm,Ooooooooooooooo,Qqqqqqqqqqqqqqqqq,Rrrrrrrrrrrrrrrrrr,Tttttttttttttttttttt,Uuuuuuuuuuuuuuuuuuuuu,Uuuuuuuuuuuuuuuuuuuuu,Vvvvvvvvvvvvvvvvvvvvvv"));
