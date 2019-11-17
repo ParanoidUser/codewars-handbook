1a1f1da33c4d32c28e8c28db3126a738d7553570
Paranoid User <pilgrim.whynot@gmail.com>
2019-11-16 23:00:43 -0800

    https://www.codewars.com/kata/alternate-square-sum

---
+++ kata/7 kyu/alternate-square-sum/main/Kata.java
@@ -0,0 +1,7 @@
+import static java.util.stream.IntStream.range;
+
+class Kata {
+  static int alternateSqSum(int[] arr) {
+    return range(0, arr.length).map(i -> arr[i] * (i % 2 != 0 ? arr[i] : 1)).sum();
+  }
+}
