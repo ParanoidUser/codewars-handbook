719a362b5c2b110827658fca8ddcff2f14fb3899
Paranoid User <pilgrim.whynot@gmail.com>
2019-11-17 11:14:59 -0800

    https://www.codewars.com/kata/composing-squared-strings

---
+++ kata/7 kyu/composing-squared-strings/main/Composing.java
@@ -0,0 +1,12 @@
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.IntStream.range;
+
+class Composing {
+  static String compose(String s1, String s2) {
+    var l1 = s1.split("\n");
+    var l2 = s2.split("\n");
+    return range(0, l1.length)
+        .mapToObj(i -> l1[i].substring(0, i + 1) + l2[l1.length - 1 - i].substring(0, l1.length - i))
+        .collect(joining("\n"));
+  }
+}
