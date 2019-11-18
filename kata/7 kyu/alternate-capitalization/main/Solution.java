1a44a3a29c3bf5fdf01cdb3a0711fc1a4cadff40
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2019-11-17 23:30:10 -0800

    https://www.codewars.com/kata/alternate-capitalization

---
+++ kata/7 kyu/alternate-capitalization/main/Solution.java
@@ -0,0 +1,10 @@
+class Solution {
+  static String[] capitalize(String s) {
+    var r = new String[] {"", ""};
+    for (int i = 0; i < s.length(); i++) {
+      r[i & 1] += (char) (s.charAt(i) - 32);
+      r[i & 1 ^ 1] += s.charAt(i);
+    }
+    return r;
+  }
+}
