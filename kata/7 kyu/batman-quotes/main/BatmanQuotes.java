6789f8f2753881a6690e078970553b7e77d8dd44
Paranoid User <pilgrim.whynot@gmail.com>
2019-11-18 21:54:01 -0800

    https://www.codewars.com/kata/batman-quotes

---
+++ kata/7 kyu/batman-quotes/main/BatmanQuotes.java
@@ -0,0 +1,5 @@
+class BatmanQuotes {
+  static String getQuote(String[] quotes, String hero) {
+    return (hero.startsWith("B") ? "Batman" : hero.startsWith("R") ? "Robin" : "Joker") + ": " + quotes[hero.replaceAll("[^\\d]", "").charAt(0) - 48];
+  }
+}
