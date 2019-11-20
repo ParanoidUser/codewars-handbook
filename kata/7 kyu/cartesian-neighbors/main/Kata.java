eb26e257f723097b389f54c16aae7850f42fc6fc
Paranoid User <pilgrim.whynot@gmail.com>
2019-11-20 13:54:32 -0800

    https://www.codewars.com/kata/cartesian-neighbors

---
+++ kata/7 kyu/cartesian-neighbors/main/Kata.java
@@ -0,0 +1,14 @@
+class Kata {
+  static int[][] cartesianNeighbor(int x, int y) {
+    return new int[][] {
+      {x - 1, y - 1},
+      {x - 1, y},
+      {x - 1, y + 1},
+      {x, y - 1},
+      {x, y + 1},
+      {x + 1, y - 1},
+      {x + 1, y},
+      {x + 1, y + 1},
+    };
+  }
+}
