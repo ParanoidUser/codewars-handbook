3fd97785ca35d78f29cd4621c92b44e5f7a40ece
Paranoid User <pilgrim.whynot@gmail.com>
2019-11-20 13:43:47 -0800

    https://www.codewars.com/kata/cat-years-dog-years-2

---
+++ kata/7 kyu/cat-years-dog-years-2/main/Dinglemouse.java
@@ -0,0 +1,8 @@
+class Dinglemouse {
+  static int[] ownedCatAndDog(int catYears, int dogYears) {
+    return new int[] {
+      catYears > 14 ? catYears > 23 ? catYears / 4 - 4 : 1 : 0,
+      dogYears > 14 ? dogYears > 23 ? 2 + (dogYears - 24) / 5 : 1 : 0
+    };
+  }
+}
