edb41c28e65b59b5fba4f461d365dd409dc92c1c
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2019-11-21 00:29:34 -0800

    https://www.codewars.com/kata/character-counter

--- kata/7 kyu/character-counter/main/CharacterCounter.java
+++ kata/7 kyu/character-counter/main/CharacterCounter.java
@@ -1,10 +1,12 @@
-import static java.util.Set.of;
 import static java.util.function.Function.identity;
 import static java.util.stream.Collectors.counting;
 import static java.util.stream.Collectors.groupingBy;
 
+import java.util.HashSet;
+
 class CharacterCounter {
   static boolean validateWord(String word) {
-    return of(word.toLowerCase().chars().boxed().collect(groupingBy(identity(), counting())).values()).size() == 1;
+    return new HashSet<>(word.toLowerCase().chars().boxed()
+                           .collect(groupingBy(identity(), counting())).values()).size() == 1;
   }
 }
