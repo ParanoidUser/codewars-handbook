7f0d81cc622947d802e4917069383b4b8bd49e42
Paranoid User <pilgrim.whynot@gmail.com>
2019-11-20 19:55:12 -0800

    https://www.codewars.com/kata/check-three-and-two

---
+++ kata/7 kyu/check-three-and-two/main/Solution.java
@@ -0,0 +1,14 @@
+import static java.util.function.Function.identity;
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
+
+import java.util.Set;
+
+class Solution {
+
+  static boolean checkThreeAndTwo(char[] chars) {
+    return new String(chars).chars().boxed()
+        .collect(groupingBy(identity(), counting()))
+        .values().containsAll(Set.of(2L, 3L));
+  }
+}
