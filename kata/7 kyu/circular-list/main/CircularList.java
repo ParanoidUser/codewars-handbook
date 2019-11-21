b848e8d8df636318ad3b1e28ba7a1b2cc8312e5f
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2019-11-21 11:28:11 -0800

    https://www.codewars.com/kata/circular-list

---
+++ kata/7 kyu/circular-list/main/CircularList.java
@@ -0,0 +1,17 @@
+class CircularList<T> {
+  private T[] elements;
+  private int pointer = -1;
+
+  CircularList(T... elements) {
+    if (elements.length == 0) throw new IllegalArgumentException();
+    this.elements = elements;
+  }
+
+  T next() {
+    return elements[(pointer = ++pointer > elements.length - 1 ? 0 : pointer)];
+  }
+
+  T prev() {
+    return elements[(pointer = --pointer < 0 ? elements.length - 1 : pointer)];
+  }
+}
