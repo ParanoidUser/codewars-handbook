cbf02604be2a2c21e0c71b60e4a8c553555bb971
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2019-11-21 21:58:41 -0800

    https://www.codewars.com/kata/collatz-conjecture-length

---
+++ kata/7 kyu/collatz-conjecture-length/main/Collatz.java
@@ -0,0 +1,9 @@
+class Collatz {
+  static long conjecture(long x) {
+    long count = 2;
+    while ((x = x % 2 == 0 ? x / 2 : x * 3 + 1) != 1) {
+      count++;
+    }
+    return count;
+  }
+}
---
+++ kata/7 kyu/collatz-conjecture-length/test/SolutionTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class SolutionTest {
+  @Test
+  public void testSomething() {
+    assertEquals(8, Collatz.conjecture(20));
+  }
+}
