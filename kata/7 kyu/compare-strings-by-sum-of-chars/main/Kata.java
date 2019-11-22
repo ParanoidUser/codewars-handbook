b3040409ae086c214185052ca00a3cdef075c074
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2019-11-21 23:23:39 -0800

    https://www.codewars.com/kata/compare-strings-by-sum-of-chars

---
+++ kata/7 kyu/compare-strings-by-sum-of-chars/main/Kata.java
@@ -0,0 +1,6 @@
+class Kata {
+  static boolean compare(String s1, String s2) {
+    return (s1 != null && s1.matches("[a-zA-Z]+") ? s1.toUpperCase().chars().sum() : 0)
+        == (s2 != null && s2.matches("[a-zA-Z]+") ? s2.toUpperCase().chars().sum() : 0);
+  }
+}
