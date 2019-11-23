77f7b51b0b8034aa2f92f6e34d2152341d69953c
Paranoid User <pilgrim.whynot@gmail.com>
2019-11-22 18:09:12 -0800

    https://www.codewars.com/kata/converting-from-base-10-to-16-dare-you

---
+++ kata/7 kyu/converting-from-base-10-to-16-dare-you/main/Hexadecimal.java
@@ -0,0 +1,10 @@
+class Hexadecimal {
+  static String convertToHex(int a) {
+    var sb = new StringBuilder();
+    while (a != 0) {
+      sb.append("0123456789ABCDEF".charAt(a % 16));
+      a /= 16;
+    }
+    return "0x" + sb.reverse().toString();
+  }
+}
