6689a161c83b30a84c998ac7d00f3ea5233465cc
Paranoid User <pilgrim.whynot@gmail.com>
2019-11-24 21:58:10 -0800

    https://www.codewars.com/kata/excel-sheet-column-numbers

--- kata/7 kyu/excel-sheet-column-numbers/main/TitleToNumber.java
+++ kata/7 kyu/excel-sheet-column-numbers/main/TitleToNumber.java
@@ -2,7 +2,7 @@ class TitleToNumber {
   static long titleToNumber(String title) {
     long sum = 0;
     for (char c : title.toCharArray()) {
-      sum = sum * 26 + c - '@';
+      sum = sum * 26 + c - 64;
     }
     return sum;
   }
