bb39459c8e3cd84be7147e54dc5eb16785452164
Paranoid User <pilgrim.whynot@gmail.com>
2019-11-27 02:10:28 -0800

    https://www.codewars.com/kata/execute-me-ntimes

--- kata/7 kyu/execute-me-ntimes/test/SolutionTest.java
+++ kata/7 kyu/execute-me-ntimes/test/SolutionTest.java
@@ -1,31 +1,23 @@
+import static org.junit.Assert.assertEquals;
+
 import java.util.concurrent.atomic.AtomicInteger;
 
-import org.junit.Assert;
 import org.junit.Test;
 
 public class SolutionTest {
   @Test
   public void sampleTests() {
-
-    Worker w = new Worker();
     int counter = 20;
     AtomicInteger executionCounter = new AtomicInteger();
-
     Runnable action =
         () -> {
-          System.out.print(".");
           executionCounter.incrementAndGet();
           try {
-            Thread.sleep(1000);
+            Thread.sleep(100);
           } catch (InterruptedException ex) {
           }
         };
-    w.execute(action, counter);
-
-    int actualCounter = executionCounter.get();
-    if (counter != actualCounter)
-      System.out.println(
-          "Aciton was executed " + actualCounter + " times instead of " + counter + " times");
-    Assert.assertEquals(counter, actualCounter);
+    Worker.execute(action, counter);
+    assertEquals(counter, executionCounter.get());
   }
 }
