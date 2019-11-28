93d92cc7b849ab5fba4b52c1d06e64ee92897726
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2019-11-27 17:20:34 -0800

    https://www.codewars.com/kata/fizz-buzz-cuckoo-clock

---
+++ kata/7 kyu/fizz-buzz-cuckoo-clock/main/FizzBuzzCuckooClock.java
@@ -0,0 +1,14 @@
+import java.time.LocalTime;
+
+class FizzBuzzCuckooClock {
+  static String fizzBuzzCuckooClock(String time) {
+    var clock = LocalTime.parse(time);
+    if (clock.getMinute() == 0) {
+      return "Cuckoo" + " Cuckoo".repeat(clock.getHour() % 12 == 0 ? 11 : clock.getHour() % 12 - 1);
+    }
+    return clock.getMinute() % 30 == 0 ? "Cuckoo" :
+           clock.getMinute() % 15 == 0 ? "Fizz Buzz" :
+           clock.getMinute() % 3 == 0 ? "Fizz" :
+           clock.getMinute() % 5 == 0 ? "Buzz" : "tick";
+  }
+}
