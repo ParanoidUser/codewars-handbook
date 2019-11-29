ce2a98b63872eaa8bf5101bf223c20d9cef50876
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2019-11-28 23:14:50 -0800

    https://www.codewars.com/kata/ghostbusters-whitespace-removal

---
+++ kata/7 kyu/ghostbusters-whitespace-removal/main/Ghostbusters.java
@@ -0,0 +1,5 @@
+class Ghostbusters {
+  static String ghostBusters(String building) {
+    return building.contains(" ") ? building.replace(" ", "") : "You just wanted my autograph didn't you?";
+  }
+}
