2e2e2ed0870f436cb6cde2b32fa6a09d6d5cb38b
Paranoid User <pilgrim.whynot@gmail.com>
2019-12-01 09:50:22 -0800

    https://www.codewars.com/kata/odd-or-even

---
+++ kata/7 kyu/odd-or-even/test/SolutionTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class SolutionTest {
+  @Test
+  public void exampleTest() {
+    assertEquals("odd", Codewars.oddOrEven(new int[] {2, 5, 34, 6}));
+  }
+}
