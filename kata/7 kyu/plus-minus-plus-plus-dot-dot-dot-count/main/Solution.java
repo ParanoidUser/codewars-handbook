c3558d75ae69f7d5f16a88245fd1287ed5d4689c
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2019-12-01 10:21:24 -0800

    https://www.codewars.com/kata/plus-minus-plus-plus-dot-dot-dot-count

---
+++ kata/7 kyu/plus-minus-plus-plus-dot-dot-dot-count/main/Solution.java
@@ -0,0 +1,7 @@
+import static java.util.stream.IntStream.range;
+
+class Solution {
+  static int signChange(int[] arr) {
+    return range(1, arr.length).map(i -> (arr[i - 1] ^ arr[i]) < 0 ? 1 : 0).sum();
+  }
+}
