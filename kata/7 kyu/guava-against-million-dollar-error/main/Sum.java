33d2b219758ae39c3aaa99c39792b120bca4fb20
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2019-12-03 17:38:43 -0800

    https://www.codewars.com/kata/guava-against-million-dollar-error

---
+++ kata/7 kyu/guava-against-million-dollar-error/main/Sum.java
@@ -0,0 +1,12 @@
+import java.util.Objects;
+
+class Sum {
+  static double sum(Double a, Double b) {
+    Objects.requireNonNull(a, "Illegal Argument passed: First parameter is Null.");
+    Objects.requireNonNull(b, "Illegal Argument passed: Second parameter is Null.");
+    if (a > 0 && b > 0) {
+      return a + b;
+    }
+    throw new IllegalArgumentException("Illegal Argument passed: Non-positive value " + (a <= 0 ? a : b) + ".");
+  }
+}
