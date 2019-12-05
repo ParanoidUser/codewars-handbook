f88a9b21e805675b413b1e8b5796ac80afea02aa
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2019-12-05 12:13:17 -0800

    https://www.codewars.com/kata/target-date

---
+++ kata/7 kyu/target-date/test/DateDaysTest.java
@@ -0,0 +1,19 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class DateDaysTest {
+  @Test
+  public void test() {
+    assertEquals("2024-07-03", DateDays.dateNbDays(4281, 5087, 2));
+    assertEquals("2021-09-19", DateDays.dateNbDays(4620, 5188, 2));
+    assertEquals("2018-03-13", DateDays.dateNbDays(9999, 11427, 6));
+    assertEquals("2026-04-18", DateDays.dateNbDays(3525, 4822, 3));
+    assertEquals("2017-06-10", DateDays.dateNbDays(5923, 6465, 6));
+    assertEquals("2017-05-22", DateDays.dateNbDays(4254, 4761, 8));
+    assertEquals("2033-11-04", DateDays.dateNbDays(1244, 2566, 4));
+    assertEquals("2019-05-25", DateDays.dateNbDays(6328, 7517, 5));
+    assertEquals("2029-06-03", DateDays.dateNbDays(2920, 3834, 2));
+    assertEquals("2019-07-09", DateDays.dateNbDays(7792, 8987, 4));
+  }
+}
