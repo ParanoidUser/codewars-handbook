618bf19d2bb7b66c4d018586af290e98a75235aa
Paranoid User <pilgrim.whynot@gmail.com>
2019-12-07 15:45:32 -0800

    https://www.codewars.com/kata/up-and-down

---
+++ kata/6 kyu/up-and-down/main/Updown.java
@@ -0,0 +1,16 @@
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.IntStream.range;
+
+class Updown {
+  static String arrange(String strng) {
+    var words = strng.split(" ");
+    for (int i = 1; i < words.length; i++) {
+      if (i % 2 == 0 && words[i].length() > words[i - 1].length() || i % 2 != 0 && words[i].length() < words[i - 1].length()) {
+        var tmp = words[i];
+        words[i] = words[i - 1];
+        words[i - 1] = tmp;
+      }
+    }
+    return range(0, words.length).mapToObj(i -> i % 2 != 0 ? words[i].toUpperCase() : words[i].toLowerCase()).collect(joining(" "));
+  }
+}
