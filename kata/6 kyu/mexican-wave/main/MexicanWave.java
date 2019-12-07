dca1c7a850d4ce7fa6515ba35c0344ca6b935871
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2019-12-07 15:25:24 -0800

    https://www.codewars.com/kata/mexican-wave

---
+++ kata/6 kyu/mexican-wave/main/MexicanWave.java
@@ -0,0 +1,10 @@
+import static java.util.stream.IntStream.range;
+
+class MexicanWave {
+  static String[] wave(String str) {
+    return range(0, str.length())
+        .filter(n -> str.charAt(n) != ' ')
+        .mapToObj(n -> str.substring(0, n) + Character.toUpperCase(str.charAt(n)) + str.substring(n + 1))
+        .toArray(String[]::new);
+  }
+}
