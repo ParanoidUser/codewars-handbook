413107b64a171bef5e6747e271c700da73949d87
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2019-12-09 00:07:27 -0800

    https://www.codewars.com/kata/stacked-balls-2d

--- kata/7 kyu/stacked-balls-2d/main/Dinglemouse.java
+++ kata/7 kyu/stacked-balls-2d/main/Dinglemouse.java
@@ -1,5 +1,5 @@
 class Dinglemouse {
   static double stackHeight2d(int layers) {
-    return layers == 0 ? 0 : 1 + Math.sqrt(Math.pow(layers - 1., 2) - Math.pow(layers / 2. - .5, 2));
+    return layers > 0 ? .86602 * --layers + 1 : 0;
   }
 }
