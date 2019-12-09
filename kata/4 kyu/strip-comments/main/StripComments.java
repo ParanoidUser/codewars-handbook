3a7c12c1d7a9621fee6634446ae12d63174900ae
Paranoid User <pilgrim.whynot@gmail.com>
2019-12-08 17:35:49 -0800

    https://www.codewars.com/kata/strip-comments

---
+++ kata/4 kyu/strip-comments/main/StripComments.java
@@ -0,0 +1,7 @@
+class StripComments {
+  static String stripComments(String text, String[] commentSymbols) {
+    return text.replaceAll("\\h*[" + String.join("", commentSymbols) + "].*(\\n?)", "$1")
+               .replaceAll("\\h(\\n)", "$1")
+               .replaceAll("\\h+\\z", "");
+  }
+}
