f0080b4a1d338c0172882541aaf2c7c338d8fb5f
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2019-12-08 23:58:41 -0800

    https://www.codewars.com/kata/last-digits-of-n-2-equals-equals-n

--- kata/4 kyu/index.md
+++ kata/4 kyu/index.md
@@ -1,5 +1,7 @@
 # C
 - [Connect Four](connect-four-1)
+# L
+- [Last digits of N^2 == N](last-digits-of-n-2-equals-equals-n)
 # S
 - [Snail](snail)
 - [Strip Comments](strip-comments)
---
+++ kata/4 kyu/last-digits-of-n-2-equals-equals-n/test/SolutionTest.java
@@ -0,0 +1,27 @@
+import static org.junit.Assert.assertEquals;
+
+import java.math.BigInteger;
+
+import org.junit.Test;
+
+public class SolutionTest {
+  @Test
+  public void basicTests() {
+    assertEquals(new BigInteger("1"), GreenNumbers.get(1));
+    assertEquals(new BigInteger("5"), GreenNumbers.get(2));
+    assertEquals(new BigInteger("6"), GreenNumbers.get(3));
+    assertEquals(new BigInteger("25"), GreenNumbers.get(4));
+  }
+
+  @Test
+  public void biggerTests() {
+    assertEquals(new BigInteger("2890625"), GreenNumbers.get(12));
+    assertEquals(new BigInteger("7109376"), GreenNumbers.get(13));
+  }
+
+  @Test
+  public void advancedTests() {
+    assertEquals(new BigInteger("6188999442576576769103890995893380022607743740081787109376"), GreenNumbers.get(100));
+    assertEquals(new BigInteger("9580863811000557423423230896109004106619977392256259918212890625"), GreenNumbers.get(110));
+  }
+}
