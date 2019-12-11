5c85adf84e890ca2660672a1a8bd19bfd4665291
Paranoid User <pilgrim.whynot@gmail.com>
2019-12-11 11:38:11 -0800

    https://www.codewars.com/kata/jumping-number-special-numbers-series-number-4

---
+++ kata/7 kyu/jumping-number-special-numbers-series-number-4/main/Solution.java
@@ -0,0 +1,8 @@
+import static java.util.stream.IntStream.range;
+
+class Solution {
+  static String jumpingNumber(int number) {
+    String s = "" + number;
+    return number > 9 && range(0, s.length() - 1).anyMatch(i -> Math.abs(s.charAt(i) - s.charAt(i + 1)) != 1) ? "Not!!" : "Jumping!!";
+  }
+}
