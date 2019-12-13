84e3044590b0d7c51238f12667e97395c070425d
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2019-12-13 00:37:30 -0800

    https://sonarcloud.io/project/issues?id=codewars&issues=AW7-YV3AI_ltG7gakBfN&open=AW7-YV3AI_ltG7gakBfN

--- kata/7 kyu/easy-wallpaper/main/EasyWallpaper.java
+++ kata/7 kyu/easy-wallpaper/main/EasyWallpaper.java
@@ -4,6 +4,6 @@ class EasyWallpaper {
             "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",
             "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "twenty"
     };
-    return l > 0 & w > 0 & h > 0 ? numbers[(int) (.4423 * (l + w) * h)] : "zero";
+    return l > 0 && w > 0 && h > 0 ? numbers[(int) (.4423 * (l + w) * h)] : "zero";
   }
 }
