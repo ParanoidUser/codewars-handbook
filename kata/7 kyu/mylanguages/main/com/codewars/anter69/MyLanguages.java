45b937b7e12779e7f883551559809cdc0cc6b29e
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2019-12-13 13:52:03 -0800

    https://www.codewars.com/kata/mylanguages

---
+++ kata/7 kyu/mylanguages/main/com/codewars/anter69/MyLanguages.java
@@ -0,0 +1,15 @@
+package com.codewars.anter69;
+
+import static java.util.stream.Collectors.toList;
+
+import java.util.List;
+import java.util.Map;
+
+class MyLanguages {
+  static List<String> myLanguages(Map<String, Integer> results) {
+    return results.keySet().stream()
+        .filter(l -> results.get(l) > 59)
+        .sorted((a, b) -> results.get(b) - results.get(a))
+        .collect(toList());
+  }
+}
