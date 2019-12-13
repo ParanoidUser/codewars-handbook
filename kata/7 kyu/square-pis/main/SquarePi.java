0f3c4d9da496b990cca4823fcf6d347bb1e768fd
Paranoid User <pilgrim.whynot@gmail.com>
2019-12-13 01:47:05 -0800

    https://sonarcloud.io/project/issues?id=codewars&issues=AW7-YV83I_ltG7gakBgV&open=AW7-YV83I_ltG7gakBgV

--- kata/7 kyu/square-pis/main/SquarePi.java
+++ kata/7 kyu/square-pis/main/SquarePi.java
@@ -5,7 +5,7 @@ class SquarePi {
 
   SquarePi(int digits) {
     var pi = "31415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679";
-    square = (int) Math.ceil(Math.sqrt(range(0, digits).mapToDouble(i -> Math.pow(pi.charAt(i) - 48, 2)).sum()));
+    square = (int) Math.ceil(Math.sqrt(range(0, digits).mapToDouble(i -> Math.pow(pi.charAt(i) - 48., 2)).sum()));
   }
 
   int calculate() {
