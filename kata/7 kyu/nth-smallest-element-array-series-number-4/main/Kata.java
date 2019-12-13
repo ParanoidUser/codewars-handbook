e7659dd2894b38fadc1ee1714576f4840b2d232d
Paranoid User <pilgrim.whynot@gmail.com>
2019-12-13 12:04:10 -0800

    https://sonarcloud.io/project/issues?id=codewars&issues=AW8Axzc-4zioEgUSgdqS&open=AW8Axzc-4zioEgUSgdqS

--- kata/7 kyu/nth-smallest-element-array-series-number-4/main/Kata.java
+++ kata/7 kyu/nth-smallest-element-array-series-number-4/main/Kata.java
@@ -2,6 +2,6 @@ import static java.util.stream.IntStream.of;
 
 class Kata {
   static int nthSmallest(int[] arr, int n) {
-    return of(arr).sorted().limit(n).skip(n - 1).findFirst().orElse(0);
+    return of(arr).sorted().limit(n).skip(n - 1L).findFirst().orElse(0);
   }
 }
