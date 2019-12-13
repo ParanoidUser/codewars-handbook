90a728f7e1597b5f87c2419e40d91ea392bed054
Paranoid User <pilgrim.whynot@gmail.com>
2019-12-13 12:50:53 -0800

    https://sonarcloud.io/project/issues?id=codewars&issues=AW8Ax0Gy4zioEgUSgdyJ&open=AW8Ax0Gy4zioEgUSgdyJ

--- kata/4 kyu/connect-four-1/main/ConnectFour.java
+++ kata/4 kyu/connect-four-1/main/ConnectFour.java
@@ -8,16 +8,26 @@ class ConnectFour {
       for (row = 0; board[column][row] != null; row++) ;
 
       board[column][row] = piece.substring(2);
-      for (var move : new int[][] {{0, 1}, {1, 0}, {1, 1}, {1, -1}}) {
-        for (int n = 0, f = 1; f >= -1; f -= 2) {
-          for (int c = column, r = row; c >= 0 && c < 7 && r >= 0 && r < 6 && piece.substring(2).equals(board[c][r]); c += move[0] * f, r += move[1] * f) {
-            if (++n > 4) {
-              return piece.substring(2);
-            }
-          }
+      for (var step : new int[][] {{0, 1}, {1, 0}, {1, 1}, {1, -1}}) {
+        var result = move(column, row, step, piece.substring(2), board);
+        if (!result.isEmpty()) {
+          return result;
         }
       }
     }
     return "Draw";
+  }
+
+  private static String move(int column, int row, int[] step, String piece, String[][] board) {
+    for (int n = 0, f = 1; f >= -1; f -= 2) {
+      for (int c = column, r = row;
+          c >= 0 && c < 7 && r >= 0 && r < 6 && piece.equals(board[c][r]);
+          c += step[0] * f, r += step[1] * f) {
+        if (++n > 4) {
+          return piece;
+        }
+      }
+    }
+    return "";
   }
 }
