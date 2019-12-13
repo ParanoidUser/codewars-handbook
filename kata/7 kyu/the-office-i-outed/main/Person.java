6276da4bf4797ccf71dab270dc6359fe44f141a6
Paranoid User <pilgrim.whynot@gmail.com>
2019-12-12 19:48:18 -0800

    https://www.codewars.com/kata/the-office-i-outed

--- kata/7 kyu/the-office-i-outed/main/Person.java
+++ kata/7 kyu/the-office-i-outed/main/Person.java
@@ -2,7 +2,7 @@ class Person {
   final String name;
   final int happiness;
 
-  public Person(String name, int happiness) {
+  Person(String name, int happiness) {
     this.name = name;
     this.happiness = happiness;
   }
