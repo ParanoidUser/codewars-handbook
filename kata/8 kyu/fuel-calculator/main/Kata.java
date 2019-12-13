6536fa1d07e88ffb91446aa8f6e29f44b7a03c04
Paranoid User <pilgrim.whynot@gmail.com>
2019-12-13 01:56:51 -0800

    https://sonarcloud.io/project/issues?id=codewars&issues=AW7-YVOZI_ltG7gakBWo&open=AW7-YVOZI_ltG7gakBWo

--- kata/8 kyu/fuel-calculator/main/Kata.java
+++ kata/8 kyu/fuel-calculator/main/Kata.java
@@ -1,5 +1,5 @@
 class Kata {
   static double fuelPrice(int litres, double price) {
-    return litres * (price - Math.min(litres / 2 * 0.05, 0.25));
+    return litres * (price - Math.min(Math.floorDiv(litres, 2) * .05, .25));
   }
 }
