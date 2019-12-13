397aa1e861be16797581a68974ce1931c4b68d25
Paranoid User <pilgrim.whynot@gmail.com>
2019-12-13 00:59:21 -0800

    https://sonarcloud.io/project/issues?id=codewars&issues=AW7-YVa3I_ltG7gakBZK&open=AW7-YVa3I_ltG7gakBZK

--- kata/8 kyu/find-the-force-of-gravity-between-two-objects/main/Solution.java
+++ kata/8 kyu/find-the-force-of-gravity-between-two-objects/main/Solution.java
@@ -2,8 +2,8 @@ class Solution {
   static double solution(double[] arrVal, String[] arrUnit) {
     double newtons = 6.67e-11 * arrVal[0] * arrVal[1] / (arrVal[2] * arrVal[2]);
     for (String el : arrUnit) {
-      newtons /= el == "g" ? 1e3 : el == "mg" ? 1e6 : el == "μg" ? 1e9 : el == "ft" ? 0.3048 * 0.3048 : 1;
-      newtons *= el == "lb" ? 0.45359 : el == "cm" ? 1e4 : el == "mm" ? 1e6 : el == "μm" ? 1e12 : 1;
+      newtons /= el.equals("g") ? 1e3 : el.equals("mg") ? 1e6 : el.equals("μg") ? 1e9 : el.equals("ft") ? .092903 : 1;
+      newtons *= el.equals("lb") ? .45359 : el.equals("cm") ? 1e4 : el.equals("mm") ? 1e6 : el.equals("μm") ? 1e12 : 1;
     }
     return newtons;
   }
