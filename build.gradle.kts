032229f4b034ebe46efbaf8e9c0113ee89070dc5
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2019-12-12 16:32:08 -0800

    Migrating Gradle build scripts to Kotlin DSL

---
+++ build.gradle.kts
@@ -0,0 +1,47 @@
+plugins {
+    java
+}
+
+subprojects {
+    apply(plugin = "java")
+
+    java {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    sourceSets {
+        main {
+            java {
+                srcDir("main")
+            }
+        }
+        test {
+            java {
+                srcDir("test")
+            }
+        }
+    }
+
+    repositories {
+        mavenCentral()
+    }
+
+    dependencies {
+        testImplementation("org.junit.jupiter:junit-jupiter:5.5.2")
+        testRuntimeOnly("org.junit.vintage:junit-vintage-engine:5.5.2")
+        testImplementation("junit:junit:4.12")
+    }
+
+    tasks {
+        compileJava {
+            options.encoding = "UTF-8"
+        }
+        compileTestJava {
+            options.encoding = "UTF-8"
+        }
+        test {
+            useJUnitPlatform()
+        }
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,40 @@
+rootProject.name = "codewars"
+
+val ignore = setOf("build", "8 kyu", "7 kyu", "6 kyu", "5 kyu", "4 kyu", "3 kyu", "2 kyu", "1 kyu")
+File("kata").walk().maxDepth(2)
+        .filter { it.isDirectory && !ignore.contains(it.name) }
+        .forEach {
+            include(it.name)
+            project(":${it.name}").projectDir = it
+        }
+
+plugins {
+    id("com.gradle.enterprise") version "3.1"
+}
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+
+        background {
+            value("Git Commit", exec("git rev-parse --short=8 --verify HEAD"))
+            value("Git Branch", exec("git rev-parse --abbrev-ref HEAD"))
+        }
+
+        obfuscation {
+            username { "${it.first()}${it.last()}".toUpperCase() }
+            hostname { "localhost" }
+            ipAddresses { it.map { "0.0.0.0" } }
+        }
+    }
+}
+
+fun exec(command: String): String {
+    val process = Runtime.getRuntime().exec(command)
+    if (process.waitFor() != 0) {
+        println("WARNING: " + String(process.errorStream.readBytes()))
+        return "unknown"
+    }
+    return String(process.inputStream.readBytes()).trim()
+}
