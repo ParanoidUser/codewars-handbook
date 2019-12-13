1221125ae888dd7b3046a1e0690dea28edc13fd5
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2019-12-13 13:26:33 -0800

    https://www.codewars.com/kata/linked-lists-append

---
+++ kata/7 kyu/linked-lists-append/main/Node.java
@@ -0,0 +1,27 @@
+class Node {
+  int data;
+  Node next;
+
+  Node(int data) {
+    this(data, null);
+  }
+
+  Node(int data, Node next) {
+    this.data = data;
+    this.next = next;
+  }
+
+  static Node append(Node listA, Node listB) {
+    if (listA == null && listB == null) {
+      return null;
+    } else if (listA != null && listB != null) {
+      Node last = listA;
+      while (last.next != null) {
+        last = last.next;
+      }
+      last.next = listB;
+      return listA;
+    }
+    return listA != null ? listA : listB;
+  }
+}
