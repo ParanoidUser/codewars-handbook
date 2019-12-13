d1888ac0ca01f539472c44bbffb204a01632e023
Paranoid User <pilgrim.whynot@gmail.com>
2019-12-13 01:02:17 -0800

    https://sonarcloud.io/project/issues?id=codewars&issues=AW7-YWA6I_ltG7gakBhI&open=AW7-YWA6I_ltG7gakBhI

--- kata/7 kyu/binary-calculator/main/BinaryCalculator.java
+++ kata/7 kyu/binary-calculator/main/BinaryCalculator.java
@@ -1,6 +1,6 @@
 class BinaryCalculator {
   static String calculate(String n1, String n2, String o) {
     int a = Integer.parseInt(n1, 2), b = Integer.parseInt(n2, 2);
-    return Integer.toBinaryString(o == "add" ? a + b : o == "multiply" ? a * b : a - b);
+    return Integer.toBinaryString(o.equals("add") ? a + b : o.equals("multiply") ? a * b : a - b);
   }
 }
