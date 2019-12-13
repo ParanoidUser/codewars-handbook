99bbcebc956d4f9eb2b20cae38b226795d51b7af
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2019-12-12 22:51:48 -0800

    SonarCloud define java libraries path

--- build.gradle.kts
+++ build.gradle.kts
@@ -56,6 +56,6 @@ tasks {
 
 sonarqube {
     properties {
-        property("sonar.java.libraries", "**/*.jar")
+        property("sonar.java.libraries", "${project.projectDir}/build/libs")
     }
 }
