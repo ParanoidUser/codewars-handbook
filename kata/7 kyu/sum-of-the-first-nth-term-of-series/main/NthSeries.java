0d7511bc3bca3747aaca23645c3bcdf4c47f83b0
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2019-12-16 01:47:48 -0800

    https://www.codewars.com/kata/sum-of-the-first-nth-term-of-series

---
+++ kata/7 kyu/sum-of-the-first-nth-term-of-series/main/NthSeries.java
@@ -0,0 +1,7 @@
+import static java.util.stream.IntStream.range;
+
+class NthSeries {
+  static String seriesSum(int n) {
+    return String.format("%.2f", range(0, n).mapToDouble(num -> 1. / (num * 3 + 1)).sum());
+  }
+}
