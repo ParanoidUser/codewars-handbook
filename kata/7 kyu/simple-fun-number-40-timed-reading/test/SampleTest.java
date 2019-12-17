8feccac1adc8f0d3848b724eb344f7550fcffc2a
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2019-12-16 18:14:03 -0800

    5 new kata solutions!  - https://www.codewars.com/kata/resistor-color-codes  - https://www.codewars.com/kata/simple-fun-number-40-timed-reading  - https://www.codewars.com/kata/simple-fun-number-37-house-numbers-sum  - https://www.codewars.com/kata/isograms  - https://www.codewars.com/kata/inttunes

---
+++ kata/7 kyu/simple-fun-number-40-timed-reading/test/SampleTest.java
@@ -0,0 +1,17 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class SampleTest {
+  @Test
+  public void basicTests() {
+    assertEquals(7, Kata.timedReading(4, "The Fox asked the stork, 'How is the soup?'"));
+    assertEquals(0, Kata.timedReading(1, "..."));
+    assertEquals(3, Kata.timedReading(3, "This play was good for us."));
+    assertEquals(5, Kata.timedReading(3, "Suddenly he stopped, and glanced up at the houses"));
+    assertEquals(11, Kata.timedReading(6, "Zebras evolved among the Old World horses within the last four million years."));
+    assertEquals(6, Kata.timedReading(5, "Although zebra species may have overlapping ranges, they do not interbreed."));
+    assertEquals(0, Kata.timedReading(1, "Oh!"));
+    assertEquals(14, Kata.timedReading(5,"Now and then, however, he is horribly thoughtless, and seems to take a real delight in giving me pain."));
+  }
+}
