b59202db84100f5cb8df12a3580be2672429ee45
Paranoid User <pilgrim.whynot@gmail.com>
2019-12-21 12:27:45 -0800

    https://sonarcloud.io/project/issues?id=codewars-solutions&issues=AW8aMazJah6ko95KFxnc&open=AW8aMazJah6ko95KFxnc

--- kata/7 kyu/send-in-the-clones/main/Dinglemouse.java
+++ kata/7 kyu/send-in-the-clones/main/Dinglemouse.java
@@ -1,8 +1,8 @@
 class Dinglemouse {
   static long[] clonewars(int kataPerDay) {
     return new long[] {
-      (long) Math.ceil(Math.pow(2, kataPerDay - 1)),
-      (long) Math.pow(2, kataPerDay + 1) - kataPerDay - 2
+      (long) Math.ceil(Math.pow(2, kataPerDay - 1.)),
+      (long) Math.pow(2, kataPerDay + 1.) - kataPerDay - 2
     };
   }
 }
