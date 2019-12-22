bc61a714c5d1a40e69e20f17a5522261c1179c6a
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2019-12-22 01:56:43 -0800

    https://www.codewars.com/kata/os-and-xs-adjudicator

--- kata/7 kyu/os-and-xs-adjudicator/test/OXAdjudicatorExampleTest.java
+++ kata/7 kyu/os-and-xs-adjudicator/test/OXAdjudicatorExampleTest.java
@@ -1,6 +1,5 @@
 import static org.junit.Assert.assertEquals;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Test;
@@ -8,40 +7,8 @@ import org.junit.Test;
 public class OXAdjudicatorExampleTest {
   @Test
   public void testJudgeEx() {
-    OXAdjudicator judge = new OXAdjudicator();
-    List<Integer> game1 = new ArrayList<>();
-    game1.add(0);
-    game1.add(1);
-    game1.add(4);
-    game1.add(8);
-    game1.add(3);
-    game1.add(6);
-    game1.add(5); // X_WIN
-
-    List<Integer> game2 = new ArrayList<>();
-    game2.add(4);
-    game2.add(2);
-    game2.add(8);
-    game2.add(0);
-    game2.add(1);
-    game2.add(7);
-    game2.add(5);
-    game2.add(3);
-    game2.add(6); // NO_WINNER
-
-    List<Integer> game3 = new ArrayList<>();
-    game3.add(0);
-    game3.add(2);
-    game3.add(4);
-    game3.add(5);
-    game3.add(6);
-    game3.add(8); // O_WIN
-
-    String result = judge.judge(game1);
-    assertEquals("Game1 expected result X was " + result, "X_WIN", result);
-    result = judge.judge(game2);
-    assertEquals("Game2 expected result Draw was " + result, "NO_WINNER", result);
-    result = judge.judge(game3);
-    assertEquals("Game3 expected result O", "O_WIN", result);
+    assertEquals("X_WIN", OXAdjudicator.judge(List.of(0, 1, 4, 8, 3, 6, 5)));
+    assertEquals("NO_WINNER", OXAdjudicator.judge(List.of(4, 2, 8, 0, 1, 7, 5, 3, 6)));
+    assertEquals("O_WIN", OXAdjudicator.judge(List.of(0, 2, 4, 5, 6, 8)));
   }
 }
