862bfe8917a4871ccdb40b9c90306b85563f811e
Paranoid User <pilgrim.whynot@gmail.com>
2019-12-22 11:15:27 -0800

    https://www.codewars.com/kata/where-is-thb

---
+++ kata/7 kyu/where-is-thb/test/THBtest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class THBtest {
+  @Test
+  public void basicTest() {
+    assertEquals("thhttttbbttt", THB.testing("For those of you who would like to test your solution before submitting it."));
+  }
+}
