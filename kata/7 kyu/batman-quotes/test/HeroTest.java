d710b4542f04a806c88858c3e537f14d2f3c92a1
Paranoid User <pilgrim.whynot@gmail.com>
2019-12-24 01:21:26 -0800

    https://www.codewars.com/kata/batman-quotes

--- kata/7 kyu/batman-quotes/test/HeroTest.java
+++ kata/7 kyu/batman-quotes/test/HeroTest.java
@@ -4,11 +4,25 @@ import org.junit.Test;
 
 public class HeroTest {
   static String[] quotes = {
-    "Quote Hidden in example test", "Holy haberdashery, Batman!", "Quote Hidden in example test"
+    "WHERE IS SHE?!", "Holy haberdashery, Batman!", "Let's put a smile on that faaaceee!"
   };
 
   @Test
   public void test1() {
-    assertEquals("Fail", "Robin: Holy haberdashery, Batman!", BatmanQuotes.getQuote(quotes, "Rob1n"));
+    assertEquals(
+        "Fail", "Robin: Holy haberdashery, Batman!", BatmanQuotes.getQuote(quotes, "Rob1n"));
+  }
+
+  @Test
+  public void test2() {
+    assertEquals("Fail", "Batman: WHERE IS SHE?!", BatmanQuotes.getQuote(quotes, "Batm0n"));
+  }
+
+  @Test
+  public void test3() {
+    assertEquals(
+        "Fail",
+        "Joker: Let's put a smile on that faaaceee!",
+        BatmanQuotes.getQuote(quotes, "Jok2r"));
   }
 }
