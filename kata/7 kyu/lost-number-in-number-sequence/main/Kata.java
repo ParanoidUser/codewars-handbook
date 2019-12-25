f086447d2bf8a875a7e6e023f6735a985269271f
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2019-12-24 21:51:17 -0800

    https://www.codewars.com/kata/lost-number-in-number-sequence

--- kata/7 kyu/lost-number-in-number-sequence/main/Kata.java
+++ kata/7 kyu/lost-number-in-number-sequence/main/Kata.java
@@ -2,6 +2,6 @@ import static java.util.stream.IntStream.of;
 
 class Kata {
   static int findDeletedNumber(int[] arr, int[] mixedArr) {
-    return arr.length == mixedArr.length ? 0 : (arr.length + 1) * (arr.length + 2) / 2 - of(mixedArr).sum();
+    return arr.length == mixedArr.length ? 0 : arr.length * (arr.length + 1) / 2 - of(mixedArr).sum();
   }
 }
