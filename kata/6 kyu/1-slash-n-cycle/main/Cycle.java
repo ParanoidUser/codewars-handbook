a55c54379610d341f1d233b089b86d6ab54c517a
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2020-01-10 05:23:23 -0800

    https://www.codewars.com/kata/1-slash-n-cycle

--- kata/6 kyu/1-slash-n-cycle/main/Cycle.java
+++ kata/6 kyu/1-slash-n-cycle/main/Cycle.java
@@ -1,6 +1,6 @@
 class Cycle {
   static int cycle(int n) {
-    if (n % 2 == 1 && n % 5 != 0) return -1;
+    if (n % 2 == 0 && n % 5 == 0) return -1;
 
     int c = 1, m = 1;
     while ((m = 10 * m % n) != 1) c++;
