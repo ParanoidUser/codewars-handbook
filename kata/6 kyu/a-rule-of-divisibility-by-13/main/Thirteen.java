78b779f0ed60935b956a2e047af0525581cd0e83
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2020-01-14 23:21:28 -0800

    https://www.codewars.com/kata/a-rule-of-divisibility-by-13

---
+++ kata/6 kyu/a-rule-of-divisibility-by-13/main/Thirteen.java
@@ -0,0 +1,13 @@
+import static java.util.stream.IntStream.range;
+
+import java.util.stream.IntStream;
+
+class Thirteen {
+  static int[] arr = new int[] {1, 10, 9, 12, 3, 4};
+
+  static long thirt(long n) {
+      var r = new StringBuilder("" + n).reverse();
+      long sum = range(0, r.length()).map(i -> arr[i % 6] * (r.charAt(i) - 48)).sum();
+      return sum == n ? sum : thirt(sum);
+  }
+}
