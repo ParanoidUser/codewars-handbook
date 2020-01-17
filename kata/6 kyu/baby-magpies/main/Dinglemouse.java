5b1cd82f356a9e3655143edbf3274279ac393569
Paranoid User <pilgrim.whynot@gmail.com>
2020-01-17 05:53:00 -0800

    https://www.codewars.com/kata/baby-magpies

---
+++ kata/6 kyu/baby-magpies/main/Dinglemouse.java
@@ -0,0 +1,13 @@
+import static java.util.stream.IntStream.range;
+
+public class Dinglemouse {
+  static boolean child(String bird1, String bird2) {
+    long diff = range(0, bird1.length()).filter(i -> bird1.charAt(i) != bird2.charAt(i)).count();
+    return diff > 0 && diff < 3;
+  }
+
+  static boolean grandchild(String bird1, String bird2) {
+    long diff = range(0, bird1.length()).filter(i -> bird1.charAt(i) != bird2.charAt(i)).count();
+    return bird1.length() > 1 && diff > 0 && diff < 5 || diff == 0;
+  }
+}
