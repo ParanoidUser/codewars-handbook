f0ce12801c6b168b2e6f7d7323b24283d57204d9
Paranoid User <pilgrim.whynot@gmail.com>
2020-01-21 01:59:30 -0800

    https://www.codewars.com/kata/bouncing-balls

---
+++ kata/6 kyu/bouncing-balls/main/BouncingBall.java
@@ -0,0 +1,5 @@
+class BouncingBall {
+  static int bouncingBall(double h, double bounce, double window) {
+    return window < h && bounce > 0 && bounce < 1 ? bouncingBall(h * bounce, bounce, window) + 2 : -1;
+  }
+}
