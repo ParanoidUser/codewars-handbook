7af18c2e4305518c7f4f6953e9c456e6fa526083
Paranoid User <pilgrim.whynot@gmail.com>
2020-01-23 05:11:19 -0800

    https://www.codewars.com/kata/button-sequences

---
+++ kata/6 kyu/button-sequences/main/Circuit.java
@@ -0,0 +1,18 @@
+class Circuit {
+  static String ledSeq(String redSeq, String blueSeq) {
+    var blinks = new StringBuilder();
+    for (int i = 0, prevRed = '0', red, prevBlue = '0', blue; i < redSeq.length(); i++, prevRed = red, prevBlue = blue) {
+      red = redSeq.charAt(i);
+      blue = blueSeq.charAt(i);
+
+      boolean buttonPressed = prevRed + prevBlue == '0' + '0' && red + blue > '0' + '0';
+      boolean buttonReleased = prevRed + prevBlue == '1' + '1' && red + blue == '1' + '0' && blinks.charAt(blinks.length() - 1) != (red == '1' ? 'R' : 'B');
+      boolean buttonSwitched = red + blue == '0' + '1' && prevRed == blue && prevBlue == red;
+
+      if (buttonReleased || buttonPressed || buttonSwitched) {
+        blinks.append(red == '1' ? 'R' : 'B');
+      }
+    }
+    return blinks.toString();
+  }
+}
