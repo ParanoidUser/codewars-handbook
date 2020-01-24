fe8a054d20f17a076485d35bcd018e74298897b8
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2020-01-23 16:25:50 -0800

    https://www.codewars.com/kata/camelcase-to-underscore

---
+++ kata/6 kyu/camelcase-to-underscore/main/CamelCaseTranslator.java
@@ -0,0 +1,5 @@
+class CamelCaseTranslator {
+  static String toUnderScore(String name) {
+    return name.replaceAll("(?<=[^_-])_?(?=[A-Z])|(?<=[^\\d_])_?(?=\\d)", "_");
+  }
+}
---
+++ kata/6 kyu/camelcase-to-underscore/test/SolutionTest.java
@@ -0,0 +1,26 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class SolutionTest {
+  @Test
+  public void simpleUnitNameTests() {
+    assertEquals("This_Is_A_Unit_Test", CamelCaseTranslator.toUnderScore("ThisIsAUnitTest"));
+    assertEquals("This_Should_Be_Split_Correct_Into_Underscore", CamelCaseTranslator.toUnderScore("ThisShouldBeSplitCorrectIntoUnderscore"));
+  }
+
+  @Test
+  public void calculationUnitNameTests() {
+    assertEquals("Calculating_1_Plus_1_Equals_2", CamelCaseTranslator.toUnderScore("Calculating1Plus1Equals2"));
+    assertEquals("Calculating_5_Plus_5_Equals_10", CamelCaseTranslator.toUnderScore("Calculating5Plus5Equals10"));
+    assertEquals("Calculate_500_Divided_By_5_Equals_100", CamelCaseTranslator.toUnderScore("Calculate500DividedBy5Equals100"));
+  }
+
+  @Test
+  public void underscoreContainingUnitNameTests() {
+    assertEquals("This_Is_Already_Split_Correct", CamelCaseTranslator.toUnderScore("This_Is_Already_Split_Correct"));
+    assertEquals("This_Is_Not_Split_Correct", CamelCaseTranslator.toUnderScore("ThisIs_Not_SplitCorrect"));
+    assertEquals("Adding_3_To_3_Should_Be_6", CamelCaseTranslator.toUnderScore("Adding_3To_3ShouldBe_6"));
+    assertEquals("_If_A_Test_Start_And_Ends_With_Underscore_It_Should_Be_The_Same_", CamelCaseTranslator.toUnderScore("_IfATestStartAndEndsWithUnderscore_ItShouldBeTheSame_"));
+  }
+}
