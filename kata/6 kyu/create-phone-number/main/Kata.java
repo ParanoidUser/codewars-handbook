8e2f994586cfaae10283069fc0e50296808b7f73
Paranoid User <pilgrim.whynot@gmail.com>
2020-01-28 15:45:48 -0800

    https://www.codewars.com/kata/create-phone-number

--- kata/6 kyu/create-phone-number/main/Kata.java
+++ kata/6 kyu/create-phone-number/main/Kata.java
@@ -1,5 +1,9 @@
 class Kata {
-  static String createPhoneNumber(int[] n) {
-    return "(" + n[0] + n[1] + n[2] + ") " + n[3] + n[4] + n[5] + "-" + n[6] + n[7] + n[8] + n[9];
+  static String createPhoneNumber(int[] numbers) {
+    var phone = "(xxx) xxx-xxxx";
+    for (int n : numbers) {
+      phone = phone.replaceFirst("x", n + "");
+    }
+    return phone;
   }
 }
