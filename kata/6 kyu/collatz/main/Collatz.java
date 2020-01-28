71b89f9d9061038584e94ef3d45c42a0dee626ac
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2020-01-27 18:14:42 -0800

    https://www.codewars.com/kata/collatz

--- kata/6 kyu/collatz/main/Collatz.java
+++ kata/6 kyu/collatz/main/Collatz.java
@@ -1,5 +1,5 @@
 class Collatz {
   static String collatz(int n) {
-    return n > 0 ? n + "->" + collatz(n % 2 == 0 ? n / 2 : 3 * n + 1) : "0";
+    return n > 1 ? n + "->" + collatz(n % 2 == 0 ? n / 2 : 3 * n + 1) : "1";
   }
 }
