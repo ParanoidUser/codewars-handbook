b676929d87a09e953d99716466f44a4805611d0d
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2020-01-28 18:01:51 -0800

    https://www.codewars.com/kata/stop-gninnips-my-sdrow

--- kata/6 kyu/stop-gninnips-my-sdrow/test/SpinWordsTest.java
+++ kata/6 kyu/stop-gninnips-my-sdrow/test/SpinWordsTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 public class SpinWordsTest {
   @Test
   public void test() {
-    assertEquals("emocleW", new SpinWords().spinWords("Welcome"));
-    assertEquals("Hey wollef sroirraw", new SpinWords().spinWords("Hey fellow warriors"));
+    assertEquals("emocleW", SpinWords.spinWords("Welcome"));
+    assertEquals("Hey wollef sroirraw", SpinWords.spinWords("Hey fellow warriors"));
   }
 }
