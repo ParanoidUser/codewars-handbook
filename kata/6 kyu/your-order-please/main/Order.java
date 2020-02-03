0b63af03d11ffbac709c0e64bdde15da6547b590
Paranoid User <pilgrim.whynot@gmail.com>
2020-02-03 11:52:38 -0800

    https://www.codewars.com/kata/your-order-please

---
+++ kata/6 kyu/your-order-please/main/Order.java
@@ -0,0 +1,9 @@
+import static java.util.Comparator.comparing;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Stream.of;
+
+class Order {
+  static String order(String words) {
+    return of(words.split(" ")).sorted(comparing(a -> Integer.parseInt(a.replaceAll("\\D", "")))).collect(joining(" "));
+  }
+}
