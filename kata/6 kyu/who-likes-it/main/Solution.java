b6b43ca088740708d972c020d44da88a913958d1
Paranoid User <pilgrim.whynot@gmail.com>
2020-02-04 09:28:52 -0800

    https://www.codewars.com/kata/who-likes-it

---
+++ kata/6 kyu/who-likes-it/main/Solution.java
@@ -0,0 +1,11 @@
+class Solution {
+  static String whoLikesIt(String... names) {
+    if (names.length < 2) {
+      return (names.length > 0 ? names[0] : "no one") + " likes this";
+    }
+    else if (names.length == 2) {
+      return names[0] + " and " + names[1] + " like this";
+    }
+    return names[0] + ", " + names[1]  + " and " + (names.length > 3 ? names.length - 2 + " others" : names[2]) + " like this";
+  }
+}
