692fc6e22e89df197e8a9897f9b03fe08a6eaf91
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2020-02-04 21:09:35 -0800

    https://www.codewars.com/kata/the-supermarket-queue

---
+++ kata/6 kyu/the-supermarket-queue/main/Solution.java
@@ -0,0 +1,13 @@
+import static java.util.Collections.nCopies;
+
+import java.util.PriorityQueue;
+
+class Solution {
+  static int solveSuperMarketQueue(int[] customers, int n) {
+    var queue = new PriorityQueue<>(nCopies(n, 0));
+    for (int customer : customers) {
+      queue.add(queue.remove() + customer);
+    }
+    return queue.stream().max(Integer::compareTo).orElse(0);
+  }
+}
