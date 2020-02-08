57a463e6910b1bfe4999bf7b9c4389bb3d9d568c
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2020-02-07 17:39:14 -0800

    https://www.codewars.com/kata/roman-numerals-encoder

---
+++ kata/6 kyu/roman-numerals-encoder/main/Conversion.java
@@ -0,0 +1,11 @@
+class Conversion {
+  static String solution(int n) {
+    return "I".repeat(n)
+        .replace("IIIII", "V").replace("IIII", "IV")
+        .replace("VV", "X").replace("VIV", "IX")
+        .replace("XXXXX", "L").replace("XXXX", "XL")
+        .replace("LL", "C").replace("LXL", "XC")
+        .replace("CCCCC", "D").replace("CCCC", "CD")
+        .replace("DD", "M").replace("DCD", "CM");
+  }
+}
