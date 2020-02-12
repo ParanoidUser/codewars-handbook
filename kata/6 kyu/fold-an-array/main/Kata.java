e0c7ac8adce84dcc6e774f796791c7dcc4c1ef36
Paranoid User <pilgrim.whynot@gmail.com>
2020-02-12 12:09:17 -0800

    https://www.codewars.com/kata/fold-an-array

---
+++ kata/6 kyu/fold-an-array/main/Kata.java
@@ -0,0 +1,11 @@
+import static java.util.Arrays.copyOfRange;
+
+class Kata {
+  static int[] foldArray(int[] array, int runs) {
+    int[] folded = copyOfRange(array, 0, Math.round(array.length / 2.0f));
+    for (int i = 0; i < array.length / 2; i++) {
+      folded[i] = array[i] + array[array.length - i - 1];
+    }
+    return runs > 1 ? foldArray(folded, --runs) : folded;
+  }
+}
