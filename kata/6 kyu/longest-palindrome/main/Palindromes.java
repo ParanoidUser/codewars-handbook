c8a1f5175f7b0ab07c0689b1580b377364c6ada0
Paranoid User <pilgrim.whynot@gmail.com>
2020-02-11 20:26:51 -0800

    https://www.codewars.com/kata/longest-palindrome

---
+++ kata/6 kyu/longest-palindrome/main/Palindromes.java
@@ -0,0 +1,15 @@
+class Palindromes {
+  static int longestPalindrome(String s) {
+    int longest = 0;
+    for (int len = s.length(); len > longest; len--) {
+      for (int i = 0; i <= s.length() - len; i++) {
+        var sub = s.substring(i, i + len);
+        if (new StringBuilder(sub).reverse().toString().equals(sub)) {
+          longest = len;
+          break;
+        }
+      }
+    }
+    return longest;
+  }
+}
