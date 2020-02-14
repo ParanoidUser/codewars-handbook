29fd80a9ff987dfa648709c04b8cbd5cc728d2e0
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2020-02-14 12:18:31 -0800

    https://www.codewars.com/kata/two-sum

--- kata/6 kyu/two-sum/test/TwoSumTest.java
+++ kata/6 kyu/two-sum/test/TwoSumTest.java
@@ -5,8 +5,8 @@ import org.junit.Test;
 public class TwoSumTest {
   @Test
   public void basicTests() {
-    //assertArrayEquals(new int[] {0, 2}, Solution.twoSum(new int[] {1, 2, 3}, 4));
-    //assertArrayEquals(new int[] {1, 2}, Solution.twoSum(new int[] {1234, 5678, 9012}, 14690));
+    assertArrayEquals(new int[] {0, 2}, Solution.twoSum(new int[] {1, 2, 3}, 4));
+    assertArrayEquals(new int[] {1, 2}, Solution.twoSum(new int[] {1234, 5678, 9012}, 14690));
     assertArrayEquals(new int[] {0, 1}, Solution.twoSum(new int[] {2, 2, 3}, 4));
   }
 }
