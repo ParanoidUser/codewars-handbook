ac75b482b52eabea2f7479b6f7c9dc150fdd262c
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2020-02-14 10:27:16 -0800

    https://www.codewars.com/kata/meeting

---
+++ kata/6 kyu/meeting/main/Meeting.java
@@ -0,0 +1,8 @@
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Stream.of;
+
+class Meeting {
+  static String meeting(String s) {
+    return of(s.toUpperCase().split(";")).map(g -> g.replaceAll("(\\w+):(\\w+)", "($2, $1)")).sorted().collect(joining());
+  }
+}
