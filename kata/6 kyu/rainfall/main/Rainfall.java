00f1ada406e4981904ad391926215f3d3fa40ed2
Paranoid User <pilgrim.whynot@gmail.com>
2020-02-14 16:56:33 -0800

    https://www.codewars.com/kata/rainfall

---
+++ kata/6 kyu/rainfall/main/Rainfall.java
@@ -0,0 +1,18 @@
+import static java.util.stream.Stream.of;
+
+import java.util.stream.DoubleStream;
+
+class Rainfall {
+  static double mean(String town, String strng) {
+    return stream(town, strng).average().orElse(-1);
+  }
+
+  static double variance(String town, String strng) {
+    return stream(town, strng).map(m -> Math.pow(m - mean(town, strng), 2)).average().orElse(-1);
+  }
+
+  private static DoubleStream stream(String town, String data) {
+    return of(data.split("\n")).filter(s -> s.startsWith(town + ":"))
+        .flatMapToDouble(s -> of(s.replaceAll("[^\\d.]", " ").trim().split("\\s+")).mapToDouble(Double::parseDouble));
+  }
+}
