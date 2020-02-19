c1f60b501f7401a2a29ea9b805635903bdfd85d4
Paranoid User <pilgrim.whynot@gmail.com>
2020-02-18 21:20:14 -0800

    https://www.codewars.com/kata/age-range-compatibility-equation

--- kata/8 kyu/age-range-compatibility-equation/main/Kata.java
+++ kata/8 kyu/age-range-compatibility-equation/main/Kata.java
@@ -1,7 +1,5 @@
 class Kata {
   static String datingRange(int age) {
-    return age > 14
-        ? age / 2 + 7 + "-" + (age - 7) * 2
-        : (int) (0.9 * age) + "-" + (int) (1.1 * age);
+    return age > 14 ? age / 2 + 7 + "-" + (age - 7) * 2 : (int) (.9 * age) + "-" + (int) (1.1 * age);
   }
 }
