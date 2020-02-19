4f2703ba21116877711a08639d847e97e81335b1
Paranoid User <pilgrim.whynot@gmail.com>
2020-02-18 21:50:21 -0800

    https://www.codewars.com/kata/classic-hello-world

--- kata/8 kyu/classic-hello-world/main/Solution.java
+++ kata/8 kyu/classic-hello-world/main/Solution.java
@@ -1,5 +1,5 @@
 class Solution {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+  static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
 }
