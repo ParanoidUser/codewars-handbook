9f5b5af1ded6d23864403829b79f7427ee787fe5
Paranoid User <pilgrim.whynot@gmail.com>
2020-02-18 21:17:01 -0800

    https://www.codewars.com/kata/a-wolf-in-sheeps-clothing

--- kata/8 kyu/a-wolf-in-sheeps-clothing/main/ZywOo.java
+++ kata/8 kyu/a-wolf-in-sheeps-clothing/main/ZywOo.java
@@ -1,8 +1,8 @@
-class ZywOo
-{
-  static String warnTheSheep(String[] array)
-  {
-    int wolfN = java.util.Arrays.asList(array).indexOf("wolf") + 1;
+import static java.util.List.of;
+
+class ZywOo {
+  static String warnTheSheep(String[] array) {
+    int wolfN = of(array).indexOf("wolf") + 1;
     return wolfN == array.length ? "Pls go away and stop eating my sheep" : "Oi! Sheep number " + (array.length - wolfN) + "! You are about to be eaten by a wolf!";
   }
 }
