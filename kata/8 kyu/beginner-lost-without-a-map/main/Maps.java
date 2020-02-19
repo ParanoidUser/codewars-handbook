8e0bc51addd4debf3523d2ade4ce6d2ad42b9d9d
Paranoid User <pilgrim.whynot@gmail.com>
2020-02-18 21:36:33 -0800

    https://www.codewars.com/kata/beginner-lost-without-a-map

--- kata/8 kyu/beginner-lost-without-a-map/main/Maps.java
+++ kata/8 kyu/beginner-lost-without-a-map/main/Maps.java
@@ -1,5 +1,7 @@
+import static java.util.stream.IntStream.of;
+
 class Maps {
   static int[] map(int[] arr) {
-    return java.util.Arrays.stream(arr).map(i -> 2 * i).toArray();
+    return of(arr).map(i -> 2 * i).toArray();
   }
 }
