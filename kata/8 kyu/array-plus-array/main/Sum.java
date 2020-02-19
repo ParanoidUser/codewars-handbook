9b70ae9960342b5e73a2f44e916b659636de6ee8
Paranoid User <pilgrim.whynot@gmail.com>
2020-02-18 21:27:58 -0800

    https://www.codewars.com/kata/array-plus-array

--- kata/8 kyu/array-plus-array/main/Sum.java
+++ kata/8 kyu/array-plus-array/main/Sum.java
@@ -1,7 +1,7 @@
-import java.util.Arrays;
+import static java.util.stream.IntStream.of;
 
 class Sum {
   static int arrayPlusArray(int[] arr1, int[] arr2) {
-    return Arrays.stream(arr1).sum() + Arrays.stream(arr2).sum();
+    return of(arr1).sum() + of(arr2).sum();
   }
 }
