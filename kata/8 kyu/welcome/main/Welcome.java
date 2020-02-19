ab4dfa1d35cbbeb1d930a84db51a6ae43a68a64b
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2020-02-18 22:46:41 -0800

    https://www.codewars.com/kata/welcome

--- kata/8 kyu/welcome/main/Welcome.java
+++ kata/8 kyu/welcome/main/Welcome.java
@@ -1,23 +1,25 @@
+import static java.util.Map.entry;
+import static java.util.Map.ofEntries;
+
 class Welcome {
- static String greet(String language) {
-    switch (language) {
-      case "czech":  return "Vitejte";
-      case "danish": return "Velkomst";
-      case "dutch": return "Welkom";
-      case "estonian": return "Tere tulemast";
-      case "finnish": return "Tervetuloa";
-      case "flemish": return "Welgekomen";
-      case "french": return "Bienvenue";
-      case "german": return "Willkommen";
-      case "irish": return "Failte";
-      case "italian": return "Benvenuto";
-      case "latvian": return "Gaidits";
-      case "lithuanian": return "Laukiamas";
-      case "polish": return "Witamy";
-      case "spanish": return "Bienvenido";
-      case "swedish": return "Valkommen";
-      case "welsh": return "Croeso";
-      default: return "Welcome";
-    }
+  static String greet(String language) {
+    return ofEntries(
+            entry("czech", "Vitejte"),
+            entry("danish", "Velkomst"),
+            entry("dutch", "Welkom"),
+            entry("estonian", "Tere tulemast"),
+            entry("finnish", "Tervetuloa"),
+            entry("flemish", "Welgekomen"),
+            entry("french", "Bienvenue"),
+            entry("german", "Willkommen"),
+            entry("irish", "Failte"),
+            entry("italian", "Benvenuto"),
+            entry("latvian", "Gaidits"),
+            entry("lithuanian", "Laukiamas"),
+            entry("polish", "Witamy"),
+            entry("spanish", "Bienvenido"),
+            entry("swedish", "Valkommen"),
+            entry("welsh", "Croeso"))
+        .getOrDefault(language, "Welcome");
   }
 }
