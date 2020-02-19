f5453443db7c1ffb5c8fb3928624637ddf3772fe
Paranoid User <pilgrim.whynot@gmail.com>
2020-02-18 21:52:16 -0800

    https://www.codewars.com/kata/color-ghost

--- kata/8 kyu/color-ghost/main/Ghost.java
+++ kata/8 kyu/color-ghost/main/Ghost.java
@@ -1,5 +1,9 @@
+import static java.util.List.of;
+
+import java.util.Random;
+
 class Ghost {
   static String getColor() {
-    return java.util.List.of("white", "yellow", "purple", "red").get(new java.util.Random().nextInt(4));
+    return of("white", "yellow", "purple", "red").get(new Random().nextInt(4));
   }
 }
