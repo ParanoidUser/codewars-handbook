6d2dc1ff5bfce3727ddd6a055fe2ecf7cc04ccd0
Paranoid User <pilgrim.whynot@gmail.com>
2020-02-18 21:06:20 -0800

    https://www.codewars.com/kata/a-needle-in-the-haystack

--- kata/8 kyu/a-needle-in-the-haystack/main/Kata.java
+++ kata/8 kyu/a-needle-in-the-haystack/main/Kata.java
@@ -1,5 +1,7 @@
+import static java.util.Arrays.asList;
+
 class Kata {
-    static String findNeedle(Object[] haystack) {
-        return String.format("found the needle at position %d", java.util.Arrays.asList(haystack).indexOf("needle"));
-    }
+  static String findNeedle(Object[] haystack) {
+    return "found the needle at position " + asList(haystack).indexOf("needle");
+  }
 }
