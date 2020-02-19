1f86a055352aaaa0d033893b3c0387e548467adf
Paranoid User <pilgrim.whynot@gmail.com>
2020-02-18 19:38:57 -0800

    https://www.codewars.com/kata/srot-the-inner-ctonnet-in-dsnnieedcg-oredr

---
+++ kata/6 kyu/srot-the-inner-ctonnet-in-dsnnieedcg-oredr/main/Kata.java
@@ -0,0 +1,18 @@
+import static java.util.Comparator.reverseOrder;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Stream.of;
+
+import java.util.ArrayList;
+
+class Kata {
+  static String sortTheInnerContent(String words) {
+    var reversed = new ArrayList<String>();
+    for (var word : words.split(" ")) {
+      if (word.length() > 3) {
+        word = word.charAt(0) + of(word.substring(1, word.length() - 1).split("")).sorted(reverseOrder()).collect(joining())+ word.charAt(word.length() - 1);
+      }
+      reversed.add(word);
+    }
+    return String.join(" ", reversed);
+  }
+}
