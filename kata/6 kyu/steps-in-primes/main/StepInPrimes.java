a926938859741ce84fc5c08f8e781a49516784cf
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2020-02-19 11:19:21 -0800

    https://www.codewars.com/kata/steps-in-primes

--- kata/6 kyu/steps-in-primes/main/StepInPrimes.java
+++ kata/6 kyu/steps-in-primes/main/StepInPrimes.java
@@ -4,7 +4,7 @@ import static java.util.stream.LongStream.range;
 class StepInPrimes {
   static long[] step(int g, long m, long n) {
     return range(m, n - g)
-        .filter(l -> valueOf(l).isProbablePrime(2) && valueOf(l + g).isProbablePrime(2))
+        .filter(l -> valueOf(l).isProbablePrime(10) && valueOf(l + g).isProbablePrime(10))
         .mapToObj(l -> new long[] {l, l + g})
         .findFirst()
         .orElse(null);
