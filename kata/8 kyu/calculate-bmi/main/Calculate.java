e3db92526ee61a622f623ad577d3f0b45915ea25
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2020-02-18 21:41:40 -0800

    https://www.codewars.com/kata/calculate-bmi

--- kata/8 kyu/calculate-bmi/main/Calculate.java
+++ kata/8 kyu/calculate-bmi/main/Calculate.java
@@ -1,6 +1,5 @@
 class Calculate {
   static String bmi(double weight, double height) {
-    weight /= Math.pow(height, 2);
-    return weight > 30 ? "Obese" : weight > 25 ? "Overweight" : weight > 18.5 ? "Normal" : "Underweight";
+    return (weight /= Math.pow(height, 2)) > 30 ? "Obese" : weight > 25 ? "Overweight" : weight > 18.5 ? "Normal" : "Underweight";
   }
 }
