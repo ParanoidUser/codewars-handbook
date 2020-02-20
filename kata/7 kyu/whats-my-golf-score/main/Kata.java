dfcab35ae1cb5eea094bb46550bbbc1164b97fa3
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2020-02-20 11:37:03 -0800

    https://www.codewars.com/kata/whats-my-golf-score

---
+++ kata/7 kyu/whats-my-golf-score/main/Kata.java
@@ -0,0 +1,5 @@
+class Kata {
+  static int golfScoreCalculator(String parList, String scoreList) {
+    return scoreList.chars().sum() - parList.chars().sum();
+  }
+}
---
+++ kata/7 kyu/whats-my-golf-score/test/SolutionTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class SolutionTest {
+  @Test
+  public void exampleTest() {
+    assertEquals(-1, Kata.golfScoreCalculator("443454444344544443", "353445334534445344"));
+  }
+}
