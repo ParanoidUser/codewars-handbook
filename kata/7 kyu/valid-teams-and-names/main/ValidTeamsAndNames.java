88b306b1554071167d2feb165f4d1b055b8d235b
Paranoid User <pilgrim.whynot@gmail.com>
2020-02-21 12:43:15 -0800

    https://www.codewars.com/kata/valid-teams-and-names

---
+++ kata/7 kyu/valid-teams-and-names/main/ValidTeamsAndNames.java
@@ -0,0 +1,10 @@
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Stream.of;
+import static org.apache.commons.lang3.StringUtils.countMatches;
+
+class ValidTeamsAndNames {
+  static String validate(String s) {
+    s = of(s.split("[A-Z]:")).filter(t -> t.contains(",")).collect(joining(":"));
+    return 1 + countMatches(s, ":") + ":" + s.replace(':', ',');
+  }
+}
