485c160baabe2e5868761d1c55ca84bf889ea6c6
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2020-02-21 20:01:26 -0800

    https://www.codewars.com/kata/roboscript-number-1-implement-syntax-highlighting

---
+++ kata/6 kyu/roboscript-number-1-implement-syntax-highlighting/main/RoboScript.java
@@ -0,0 +1,11 @@
+import java.util.function.UnaryOperator;
+
+class RoboScript {
+  static String highlight(String code) {
+    UnaryOperator<String> span = c -> String.format("<span style=\"color: %s\">$1</span>", c);
+    return code.replaceAll("(F+)", span.apply("pink"))
+        .replaceAll("(L+)", span.apply("red"))
+        .replaceAll("(R+)", span.apply("green"))
+        .replaceAll("(\\d+)", span.apply("orange"));
+  }
+}
