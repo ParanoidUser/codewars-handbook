c7630835c08b5a68315390fee35327e09b0ff7f5
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2020-02-26 13:11:22 -0800

    https://www.codewars.com/kata/sorting-on-planet-twisted-3-7

---
+++ kata/6 kyu/sorting-on-planet-twisted-3-7/main/Kata.java
@@ -0,0 +1,10 @@
+import static java.util.Arrays.stream;
+
+import java.util.function.IntUnaryOperator;
+
+class Kata {
+ static Integer[] sortTwisted37(Integer[] array) {
+     IntUnaryOperator twist = i -> Integer.parseInt((i + "").replace('3', '_').replace('7', '3').replace('_', '7'));
+    return stream(array).map(twist::applyAsInt).sorted().map(twist::applyAsInt).toArray(Integer[]::new);
+  }
+}
