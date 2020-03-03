b31f171c77f063c0c0d450341c88599adcd0f6bf
Paranoid User <pilgrim.whynot@gmail.com>
2020-03-02 23:03:49 -0800

    https://sonarcloud.io/project/issues?id=codewars-solutions&issues=AW8aMavIah6ko95KFxk6&open=AW8aMavIah6ko95KFxk6

--- kata/7 kyu/bumps-in-the-road/test/SolutionTest.java
+++ kata/7 kyu/bumps-in-the-road/test/SolutionTest.java
@@ -5,9 +5,9 @@ import org.junit.Test;
 public class SolutionTest {
   @Test
   public void basicTest() {
-    assertEquals(BumpsTheRoad.bumps("n"), "Woohoo!");
-    assertEquals(BumpsTheRoad.bumps("_nnnnnnn_n__n______nn__nn_nnn"), "Car Dead");
-    assertEquals(BumpsTheRoad.bumps("______n___n_"), "Woohoo!");
-    assertEquals(BumpsTheRoad.bumps("nnnnnnnnnnnnnnnnnnnnn"), "Car Dead");
+    assertEquals("Woohoo!", BumpsTheRoad.bumps("n"));
+    assertEquals("Car Dead", BumpsTheRoad.bumps("_nnnnnnn_n__n______nn__nn_nnn"));
+    assertEquals("Woohoo!", BumpsTheRoad.bumps("______n___n_"));
+    assertEquals("Car Dead", BumpsTheRoad.bumps("nnnnnnnnnnnnnnnnnnnnn"));
   }
 }
