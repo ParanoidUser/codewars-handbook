d2e3267f5c4272f359b8c4f8323c6d4bd9c851ff
Paranoid User <pilgrim.whynot@gmail.com>
2020-03-02 23:09:02 -0800

    https://sonarcloud.io/project/issues?id=codewars-solutions&issues=AW8aMa1Rah6ko95KFxov&open=AW8aMa1Rah6ko95KFxov

--- kata/7 kyu/find-the-gcf-of-two-numbers/test/TestGCF.java
+++ kata/7 kyu/find-the-gcf-of-two-numbers/test/TestGCF.java
@@ -5,8 +5,8 @@ import org.junit.Test;
 public class TestGCF {
   @Test
   public void basicTests() {
-    assertEquals(Kata.findGCF(8, 20), 4);
-    assertEquals(Kata.findGCF(5, 13), 1);
-    assertEquals(Kata.findGCF(100, 100), 100);
+    assertEquals(4, Kata.findGCF(8, 20));
+    assertEquals(1, Kata.findGCF(5, 13));
+    assertEquals(100, Kata.findGCF(100, 100));
   }
 }
