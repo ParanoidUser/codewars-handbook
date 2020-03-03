190b97968b79b5d95a6f1e7c10991bdb16e1f927
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2020-03-02 21:07:43 -0800

    https://sonarcloud.io/project/issues?id=codewars-solutions&issues=AXBAJKJaNBtOAKHgEr44&open=AXBAJKJaNBtOAKHgEr44

--- kata/6 kyu/street-fighter-2-character-selection/main/Solution.java
+++ kata/6 kyu/street-fighter-2-character-selection/main/Solution.java
@@ -2,10 +2,9 @@ class Solution {
   static String[] streetFighterSelection(String[][] fighters, int[] position, String[] moves) {
     for (int i = 0; i < moves.length; i++) {
       switch (moves[i]) {
-        case "up": position[0] = 0; break;
         case "right": position[1] = (position[1] + 1) % 6; break;
-        case "down": position[0] = 1; break;
         case "left": position[1] = (position[1] + 5) % 6; break;
+        default: position[0] = moves[i].equals("up") ? 0 : 1;
       }
       moves[i] = fighters[position[0]][position[1]];
     }
