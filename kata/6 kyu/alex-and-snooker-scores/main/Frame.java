d83ce6481a33fe39c58d7108394e9c278cd2ce54
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2020-03-02 22:45:26 -0800

    https://sonarcloud.io/project/issues?id=codewars-solutions&issues=AW-tedb6TGVyrxzpKO3g&open=AW-tedb6TGVyrxzpKO3g

--- kata/6 kyu/alex-and-snooker-scores/main/Frame.java
+++ kata/6 kyu/alex-and-snooker-scores/main/Frame.java
@@ -2,8 +2,8 @@ class Frame {
   static int[] frame(String score) {
     int[] result = new int[2];
     for (var match : score.replaceAll("\\(.*?\\)", "").split(";")) {
-      var one = Integer.parseInt(match.substring(0, match.indexOf("-")).trim());
-      var two = Integer.parseInt(match.substring(match.indexOf("-") + 1).trim());
+      var one = Integer.parseInt(match.substring(0, match.indexOf('-')).trim());
+      var two = Integer.parseInt(match.substring(match.indexOf('-') + 1).trim());
       if (one > two) {
         result[0]++;
       } else {
