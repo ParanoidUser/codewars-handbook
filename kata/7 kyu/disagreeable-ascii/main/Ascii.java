c60468fedaff37fe42346d375e06436b73598244
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2020-03-02 22:50:09 -0800

    https://sonarcloud.io/project/issues?id=codewars-solutions&issues=AW8aMar_ah6ko95KFxi4&open=AW8aMar_ah6ko95KFxi4

--- kata/7 kyu/disagreeable-ascii/main/Ascii.java
+++ kata/7 kyu/disagreeable-ascii/main/Ascii.java
@@ -1,5 +1,5 @@
 class Ascii {
   static int getWeight(String name) {
-    return name.chars().map(с -> с > '`' && с < '{' ? с - 32 : с > '@' && с < '[' ? с + 32 : 0).sum();
+    return name.chars().map(c -> c > '`' && c < '{' ? c - 32 : c > '@' && c < '[' ? c + 32 : 0).sum();
   }
 }
