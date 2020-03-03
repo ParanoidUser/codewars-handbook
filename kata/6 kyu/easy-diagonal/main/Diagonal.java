7329886fe2743c09346abc7ccfd2109f51050407
Paranoid User <pilgrim.whynot@gmail.com>
2020-03-02 22:52:17 -0800

    https://sonarcloud.io/project/issues?id=codewars-solutions&issues=AXCYbhL0OUUhaXqThNAo&open=AXCYbhL0OUUhaXqThNAo

--- kata/6 kyu/easy-diagonal/main/Diagonal.java
+++ kata/6 kyu/easy-diagonal/main/Diagonal.java
@@ -5,9 +5,9 @@ import java.math.BigInteger;
 
 class Diagonal {
   static BigInteger diagonal(int n, int p) {
-    BigInteger sum = ONE, P = valueOf(p);
+    BigInteger sum = ONE, bp = valueOf(p);
     for (BigInteger i = ONE, prev = ONE; i.intValue() < n - p + 1; i = i.add(ONE)) {
-      sum = sum.add(prev = prev.multiply(P.add(i)).divide(i));
+      sum = sum.add(prev = prev.multiply(bp.add(i)).divide(i));
     }
     return sum;
   }
