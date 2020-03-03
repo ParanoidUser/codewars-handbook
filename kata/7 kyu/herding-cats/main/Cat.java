070faac96dc075e0f882a2d916a49303044c8d3f
Paranoid User <pilgrim.whynot@gmail.com>
2020-03-02 22:41:37 -0800

    https://sonarcloud.io/project/issues?id=codewars-solutions&issues=AW8aMaoqah6ko95KFxgn&open=AW8aMaoqah6ko95KFxgn

--- kata/7 kyu/herding-cats/main/Cat.java
+++ kata/7 kyu/herding-cats/main/Cat.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Cat implements Comparable<Cat> {
   String name;
   double weight;
@@ -10,5 +12,22 @@ public class Cat implements Comparable<Cat> {
   @Override
   public int compareTo(Cat c) {
     return this.name.compareTo(c.name);
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Cat cat = (Cat) o;
+    return Double.compare(cat.weight, weight) == 0 && Objects.equals(name, cat.name);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(name, weight);
   }
 }
