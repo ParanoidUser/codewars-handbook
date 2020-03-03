81bf95585354bb8113904b81bec4c0c81badcd8a
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2020-03-02 23:17:58 -0800

    https://sonarcloud.io/project/issues?id=codewars-solutions&issues=AW88VUTfd0Pb5o5Bj_Wd&open=AW88VUTfd0Pb5o5Bj_Wd

--- kata/7 kyu/matrix-addition-of-elements/test/SumOfElementsTest.java
+++ kata/7 kyu/matrix-addition-of-elements/test/SumOfElementsTest.java
@@ -6,10 +6,10 @@ public class SumOfElementsTest {
   @Test
   public void test1() {
     int[][] a = {{1, 0}, {0, 1}};
-    assertEquals(Sid.sumOfElements(a), 2);
+    assertEquals(2, Sid.sumOfElements(a));
     int[][] b = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-    assertEquals(Sid.sumOfElements(b), 45);
+    assertEquals(45, Sid.sumOfElements(b));
     int[][] c = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 2}};
-    assertEquals(Sid.sumOfElements(c), 122);
+    assertEquals(122, Sid.sumOfElements(c));
   }
 }
