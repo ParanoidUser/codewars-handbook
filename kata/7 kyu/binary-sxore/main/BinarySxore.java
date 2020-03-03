751c799bfc6194b5a06ad7faaccf72890a4ea4df
Paranoid User <pilgrim.whynot@gmail.com>
2020-03-02 22:34:38 -0800

    https://sonarcloud.io/project/issues?id=codewars-solutions&issues=AW8aMa2aah6ko95KFxpf&open=AW8aMa2aah6ko95KFxpf

--- kata/7 kyu/binary-sxore/main/BinarySxore.java
+++ kata/7 kyu/binary-sxore/main/BinarySxore.java
@@ -5,6 +5,6 @@ import java.math.BigInteger;
 
 class BinarySxore {
   static BigInteger sxore(BigInteger n) {
-    return new BigInteger[] {n, ONE, n.add(ONE), ZERO}[n.mod(new BigInteger("4")).intValue()];
+    return new BigInteger[] {n, ONE, n.add(ONE), ZERO}[n.mod(BigInteger.valueOf(4)).intValue()];
   }
 }
