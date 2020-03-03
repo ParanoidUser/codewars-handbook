479fc08657267da69bf33fba5179c1b1ac8a8949
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2020-03-02 22:11:19 -0800

    https://sonarcloud.io/project/issues?id=codewars-solutions&issues=AW8aMayZah6ko95KFxnB&open=AW8aMayZah6ko95KFxnB

--- kata/7 kyu/ten-green-bottles/main/Dinglemouse.java
+++ kata/7 kyu/ten-green-bottles/main/Dinglemouse.java
@@ -1,13 +1,13 @@
 class Dinglemouse {
-  private static String[] COUNT = {"No", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", " green bottle"};
+  private static final String[] COUNT = {"No", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", " green bottle"};
 
   static String tenGreenBottles(int n) {
     var lyrics = new StringBuilder();
     while (n > 0) {
-      lyrics.append(COUNT[n] + COUNT[11] + (n > 1 ? "s" : "") + " hanging on the wall,\n");
-      lyrics.append(COUNT[n] + COUNT[11] + (n > 1 ? "s" : "") + " hanging on the wall,\n");
-      lyrics.append((n > 1 ? "And if" : "If that") + " one green bottle should accidentally fall,\n");
-      lyrics.append("There'll be " + COUNT[n - 1].toLowerCase() + COUNT[11] + (n == 2 ? "" : "s") + " hanging on the wall.\n");
+      lyrics.append(COUNT[n]).append(COUNT[11]).append(n > 1 ? "s" : "").append(" hanging on the wall,\n");
+      lyrics.append(COUNT[n]).append(COUNT[11]).append(n > 1 ? "s" : "").append(" hanging on the wall,\n");
+      lyrics.append(n > 1 ? "And if" : "If that").append(" one green bottle should accidentally fall,\n");
+      lyrics.append("There'll be ").append(COUNT[n - 1].toLowerCase()).append(COUNT[11]).append(n == 2 ? "" : "s").append(" hanging on the wall.\n");
       lyrics.append(--n > 0 ? "\n" : "");
     }
     return lyrics.toString();
