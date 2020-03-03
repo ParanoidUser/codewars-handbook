6683cfadc926f78b9f690756238b627eb0fc5a44
Paranoid User <pilgrim.whynot@gmail.com>
2020-03-02 23:01:52 -0800

    https://sonarcloud.io/project/issues?id=codewars-solutions&issues=AW8aMappah6ko95KFxhO&open=AW8aMappah6ko95KFxhO

--- kata/7 kyu/basics-generators-number-1/test/SolutionTest.java
+++ kata/7 kyu/basics-generators-number-1/test/SolutionTest.java
@@ -6,11 +6,11 @@ public class SolutionTest {
   @Test
   public void testSomething() {
     Generator g = new Generator();
-    assertEquals(1,g.next());
-    assertEquals(2,g.next());
-    assertEquals(3,g.next());
-    assertEquals(4,g.next());
-    assertEquals(5,g.next());
+    assertEquals(1, g.next());
+    assertEquals(2, g.next());
+    assertEquals(3, g.next());
+    assertEquals(4, g.next());
+    assertEquals(5, g.next());
     assertEquals(10, g.next(10));
   }
 }
