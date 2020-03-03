cb752d15dd5eff742c1c071e0302cf3807d7e887
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2020-03-02 23:17:03 -0800

    https://sonarcloud.io/project/issues?id=codewars-solutions&issues=AW83I7oDE6orbafpZaZx&open=AW83I7oDE6orbafpZaZx

--- kata/7 kyu/linked-lists-get-nth-node/test/NodeTest.java
+++ kata/7 kyu/linked-lists-get-nth-node/test/NodeTest.java
@@ -11,9 +11,9 @@ public class NodeTest {
     n.next.data = 42;
     n.next.next = new Node();
     n.next.next.data = 23;
-    assertEquals(Node.getNth(n, 0), 1337);
-    assertEquals(Node.getNth(n, 1), 42);
-    assertEquals(Node.getNth(n, 2), 23);
+    assertEquals(1337, Node.getNth(n, 0));
+    assertEquals(42, Node.getNth(n, 1));
+    assertEquals(23, Node.getNth(n, 2));
   }
 
   @Test(expected = RuntimeException.class)
