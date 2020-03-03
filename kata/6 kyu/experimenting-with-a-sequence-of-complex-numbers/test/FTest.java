5adc262668f65cb242282129197ccd6d1615d023
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2020-03-02 23:00:04 -0800

    https://sonarcloud.io/project/issues?id=codewars-solutions&issues=AW-gWgZ1O8lUyTUXlFpk&open=AW-gWgZ1O8lUyTUXlFpk

--- kata/6 kyu/experimenting-with-a-sequence-of-complex-numbers/test/FTest.java
+++ kata/6 kyu/experimenting-with-a-sequence-of-complex-numbers/test/FTest.java
@@ -5,8 +5,8 @@ import org.junit.Test;
 public class FTest {
   @Test
   public void test1() {
-    assertEquals(F.f(0.64, 0.75, 1e-12), 1952, 1.0);
-    assertEquals(F.f(0.3, 0.5, 1e-4), 17, 1.0);
-    assertEquals(F.f(30.0, 50.0, 1e-4), -1, 1.0);
+    assertEquals(1952, F.f(0.64, 0.75, 1e-12), 1.0);
+    assertEquals(17, F.f(0.3, 0.5, 1e-4), 1.0);
+    assertEquals(-1, F.f(30.0, 50.0, 1e-4), 1.0);
   }
 }
