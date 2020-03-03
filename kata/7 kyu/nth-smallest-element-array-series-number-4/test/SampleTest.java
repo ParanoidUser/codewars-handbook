220be5e6a9c71d7e3f6284e4282cf59b7a32ed00
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2020-03-02 23:19:35 -0800

    https://sonarcloud.io/project/issues?id=codewars-solutions&issues=AW8aMas7ah6ko95KFxjf&open=AW8aMas7ah6ko95KFxjf

--- kata/7 kyu/nth-smallest-element-array-series-number-4/test/SampleTest.java
+++ kata/7 kyu/nth-smallest-element-array-series-number-4/test/SampleTest.java
@@ -5,19 +5,19 @@ import org.junit.Test;
 public class SampleTest {
   @Test
   public void checkPositiveValues() {
-    assertEquals(Kata.nthSmallest(new int[] {3, 1, 2}, 2), 2);
-    assertEquals(Kata.nthSmallest(new int[] {15, 20, 7, 10, 4, 3}, 3), 7);
+    assertEquals(2, Kata.nthSmallest(new int[] {3, 1, 2}, 2));
+    assertEquals(7, Kata.nthSmallest(new int[] {15, 20, 7, 10, 4, 3}, 3));
   }
 
   @Test
   public void checkNegativeValues() {
-    assertEquals(Kata.nthSmallest(new int[] {-5, -1, -6, -18}, 4), -1);
-    assertEquals(Kata.nthSmallest(new int[] {-102, -16, -1, -2, -367, -9}, 5), -2);
+    assertEquals(-1, Kata.nthSmallest(new int[] {-5, -1, -6, -18}, 4));
+    assertEquals(-2, Kata.nthSmallest(new int[] {-102, -16, -1, -2, -367, -9}, 5));
   }
 
   @Test
   public void checkMixedValues() {
-    assertEquals(Kata.nthSmallest(new int[] {2, 169, 13, -5, 0, -1}, 4), 2);
-    assertEquals(Kata.nthSmallest(new int[] {177, 225, 243, -169, -12, -5, 2, 92}, 5), 92);
+    assertEquals(2, Kata.nthSmallest(new int[] {2, 169, 13, -5, 0, -1}, 4));
+    assertEquals(92, Kata.nthSmallest(new int[] {177, 225, 243, -169, -12, -5, 2, 92}, 5));
   }
 }
