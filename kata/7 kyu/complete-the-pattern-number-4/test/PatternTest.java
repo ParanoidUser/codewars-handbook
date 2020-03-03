d9cbce4c14ae9b71baf4743b539fcae1002fab0f
Paranoid User <pilgrim.whynot@gmail.com>
2020-03-02 23:05:20 -0800

    https://sonarcloud.io/project/issues?id=codewars-solutions&issues=AW8aMawOah6ko95KFxlo&open=AW8aMawOah6ko95KFxlo

--- kata/7 kyu/complete-the-pattern-number-4/test/PatternTest.java
+++ kata/7 kyu/complete-the-pattern-number-4/test/PatternTest.java
@@ -5,11 +5,11 @@ import org.junit.Test;
 public class PatternTest {
   @Test
   public void pattern() {
-    assertEquals(Pattern.pattern(1), "1");
-    assertEquals(Pattern.pattern(2), "12\n2");
-    assertEquals(Pattern.pattern(5), "12345\n2345\n345\n45\n5");
-    assertEquals(Pattern.pattern(0), "");
-    assertEquals(Pattern.pattern(-25), "");
-    assertEquals(Pattern.pattern(-59), "");
+    assertEquals("1", Pattern.pattern(1));
+    assertEquals("12\n2", Pattern.pattern(2));
+    assertEquals("12345\n2345\n345\n45\n5", Pattern.pattern(5));
+    assertEquals("", Pattern.pattern(0));
+    assertEquals("", Pattern.pattern(-25));
+    assertEquals("", Pattern.pattern(-59));
   }
 }
