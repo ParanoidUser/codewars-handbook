eed76abdc617cb7cdc753114908652f84d00f88a
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2020-03-02 22:48:45 -0800

    https://sonarcloud.io/project/issues?id=codewars-solutions&issues=AW8aMatXah6ko95KFxjz&open=AW8aMatXah6ko95KFxjz

--- kata/7 kyu/ad2070-help-lorimar-troubleshoot-his-robots-ultrasonic-distance-analysis/main/Kata.java
+++ kata/7 kyu/ad2070-help-lorimar-troubleshoot-his-robots-ultrasonic-distance-analysis/main/Kata.java
@@ -1,16 +1,16 @@
 class Kata {
-  static double[] sensorAnalysis(Object[][] sensor_data) {
+  static double[] sensorAnalysis(Object[][] sensorData) {
     double mean = 0;
-    for (Object[] data : sensor_data) {
+    for (Object[] data : sensorData) {
       mean += (double) data[1];
     }
-    mean /= sensor_data.length;
+    mean /= sensorData.length;
 
     double deviation = 0;
-    for (Object[] data : sensor_data) {
+    for (Object[] data : sensorData) {
       deviation += Math.pow((double) data[1] - mean, 2);
     }
-    deviation /= sensor_data.length - 1;
+    deviation /= sensorData.length - 1;
 
     return new double[] {Math.round(mean * 10e3) / 10e3, Math.round(Math.sqrt(deviation) * 10e3) / 10e3};
   }
