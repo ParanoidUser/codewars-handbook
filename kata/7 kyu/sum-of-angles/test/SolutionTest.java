54708d81c84cf763bda4897c7ed3190c5d8c021c
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2020-03-02 23:30:29 -0800

    https://sonarcloud.io/project/issues?id=codewars-solutions&issues=AW8aMa4dah6ko95KFxqu&open=AW8aMa4dah6ko95KFxqu

--- kata/7 kyu/sum-of-angles/test/SolutionTest.java
+++ kata/7 kyu/sum-of-angles/test/SolutionTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 public class SolutionTest {
   @Test
   public void sampleTests() {
-    assertEquals(AngleSum.sumOfAngles(3), 180);
-    assertEquals(AngleSum.sumOfAngles(4), 360);
+    assertEquals(180, AngleSum.sumOfAngles(3));
+    assertEquals(360, AngleSum.sumOfAngles(4));
   }
 }
