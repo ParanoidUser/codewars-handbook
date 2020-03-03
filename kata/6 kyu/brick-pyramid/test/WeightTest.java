bade118b238d783b02c5f5196d30562947c298f2
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2020-03-02 22:58:12 -0800

    https://sonarcloud.io/project/issues?id=codewars-solutions&issues=AW_Sj45JBz5_QLvp3JnY&open=AW_Sj45JBz5_QLvp3JnY

--- kata/6 kyu/brick-pyramid/test/WeightTest.java
+++ kata/6 kyu/brick-pyramid/test/WeightTest.java
@@ -5,13 +5,11 @@ import org.junit.Test;
 public class WeightTest {
   @Test
   public void test1() {
-    assertEquals(Pyramid.weight(0, 0), 0, 0);
-
-    assertEquals(Pyramid.weight(1, 0), 0.5, 0);
-    assertEquals(Pyramid.weight(1, 1), 0.5, 0);
-
-    assertEquals(Pyramid.weight(2, 0), 0.75, 0);
-    assertEquals(Pyramid.weight(2, 1), 1.5, 0);
-    assertEquals(Pyramid.weight(2, 2), 0.75, 0);
+    assertEquals(0, Pyramid.weight(0, 0), 0);
+    assertEquals(0.5, Pyramid.weight(1, 0), 0);
+    assertEquals(0.5, Pyramid.weight(1, 1), 0);
+    assertEquals(0.75, Pyramid.weight(2, 0), 0);
+    assertEquals(1.5, Pyramid.weight(2, 1), 0);
+    assertEquals(0.75, Pyramid.weight(2, 2), 0);
   }
 }
