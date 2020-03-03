2818b86355a55ba7750ed86a8edac0a4358960f8
Paranoid User <pilgrim.whynot@gmail.com>
2020-03-02 23:14:41 -0800

    https://sonarcloud.io/project/issues?id=codewars-solutions&issues=AW9PgtmZmWvs_bS_QAkT&open=AW9PgtmZmWvs_bS_QAkT

--- kata/7 kyu/how-many-times-does-it-contain/test/SolutionTest.java
+++ kata/7 kyu/how-many-times-does-it-contain/test/SolutionTest.java
@@ -5,9 +5,9 @@ import org.junit.Test;
 public class SolutionTest {
   @Test
   public void exampleTests() {
-    assertEquals(StringCounter.stringCounter("Hello World", 'o'), 2);
-    assertEquals(StringCounter.stringCounter("Wait isn't it supposed to be cynical?", 'i'), 4);
-    assertEquals(StringCounter.stringCounter("I'm gona be the best code warrior ever dad", 'r'), 4);
-    assertEquals(StringCounter.stringCounter("Do you like Harry Potter?", '?'), 1);
+    assertEquals(2, StringCounter.stringCounter("Hello World", 'o'));
+    assertEquals(4, StringCounter.stringCounter("Wait isn't it supposed to be cynical?", 'i'));
+    assertEquals(4, StringCounter.stringCounter("I'm gona be the best code warrior ever dad", 'r'));
+    assertEquals(1, StringCounter.stringCounter("Do you like Harry Potter?", '?'));
   }
 }
