f6ec2a17a0bfe089890bc944e7409403355d844b
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2020-03-02 22:30:01 -0800

    https://sonarcloud.io/project/issues?id=codewars-solutions&issues=AW8aMa4bah6ko95KFxqt&open=AW8aMa4bah6ko95KFxqt

--- kata/7 kyu/execute-me-ntimes/main/Worker.java
+++ kata/7 kyu/execute-me-ntimes/main/Worker.java
@@ -1,13 +1,6 @@
 class Worker {
-  static void execute(Runnable action, int nTimes) {
-    Thread last = null;
-    for (int i = 0; i < nTimes; i++) {
-      last = new Thread(action);
-      last.start();
-    }
-    try {
-      if (last != null) last.join();
-    } catch (Exception ignore) {
-    }
+  void execute(Runnable action, int nTimes) {
+    for (int i = 0; i < nTimes; i++) new Thread(action).start();
+    while (Thread.activeCount() > 1);
   }
 }
