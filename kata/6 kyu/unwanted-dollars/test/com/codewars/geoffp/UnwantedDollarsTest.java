86a7771e73fac31298d55fad29886a845d531245
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2020-03-05 09:48:06 -0800

    https://www.codewars.com/kata/unwanted-dollars

---
+++ kata/6 kyu/unwanted-dollars/test/com/codewars/geoffp/UnwantedDollarsTest.java
@@ -0,0 +1,22 @@
+package com.codewars.geoffp;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class UnwantedDollarsTest {
+  @Test
+  public void moneyValue() {
+    assertEquals(12.34, UnwantedDollars.moneyValue("12.34"), 1e-9);
+    assertEquals(5.67, UnwantedDollars.moneyValue(" $5.67"), 1e-9);
+    assertEquals(-0.89, UnwantedDollars.moneyValue("-0.89"), 1e-9);
+    assertEquals(-0.10, UnwantedDollars.moneyValue("-$ 0.1"), 1e-9);
+    assertEquals(-2.3456, UnwantedDollars.moneyValue("$-2.3456"), 1e-9);
+    assertEquals(7, UnwantedDollars.moneyValue("007"), 1e-9);
+    assertEquals(89.0, UnwantedDollars.moneyValue(" $ 89"), 1e-9);
+    assertEquals(0.11, UnwantedDollars.moneyValue("   .11"), 1e-9);
+    assertEquals(0.20, UnwantedDollars.moneyValue("$.2"), 1e-9);
+    assertEquals(-0.34, UnwantedDollars.moneyValue("-.34"), 1e-9);
+    assertEquals(0.0, UnwantedDollars.moneyValue("$$$"), 1e-9);
+  }
+}
