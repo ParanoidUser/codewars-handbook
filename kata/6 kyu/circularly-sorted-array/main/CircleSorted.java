5b8a67f516c8f4600e96d6cdda9d5fed0ff618b7
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2020-03-08 00:24:16 -0800

    https://www.codewars.com/kata/circularly-sorted-array

---
+++ kata/6 kyu/circularly-sorted-array/main/CircleSorted.java
@@ -0,0 +1,10 @@
+import static java.util.stream.IntStream.range;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+class CircleSorted {
+  static boolean isCircleSorted(int[] a) {
+    var count = new AtomicInteger();
+    return range(0, a.length).noneMatch(i -> a[i] > a[++i % a.length] && count.getAndIncrement() > 0);
+  }
+}
