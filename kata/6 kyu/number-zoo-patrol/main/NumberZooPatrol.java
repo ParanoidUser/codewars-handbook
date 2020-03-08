0a5f7605e4109eca1f30f3fc6e5b3f6ebe962b90
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2020-03-07 23:49:33 -0800

    https://www.codewars.com/kata/number-zoo-patrol

---
+++ kata/6 kyu/number-zoo-patrol/main/NumberZooPatrol.java
@@ -0,0 +1,7 @@
+import static java.util.stream.IntStream.of;
+
+class NumberZooPatrol {
+  static int findMissingNumber(int[] numbers) {
+    return (int) (numbers.length * (numbers.length + 3L) / 2 - of(numbers).sum()) + 1;
+  }
+}
