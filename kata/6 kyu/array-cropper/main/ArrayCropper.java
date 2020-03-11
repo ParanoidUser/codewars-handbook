483170646556dfd7f257596129f6fb85a138d001
Paranoid User <pilgrim.whynot@gmail.com>
2020-03-10 19:07:44 -0700

    https://sonarcloud.io/project/issues?id=codewars-solutions&issues=AW-30xSI4co6C_95KVIz&open=AW-30xSI4co6C_95KVIz

--- kata/6 kyu/array-cropper/main/ArrayCropper.java
+++ kata/6 kyu/array-cropper/main/ArrayCropper.java
@@ -1,16 +1,17 @@
 class ArrayCropper {
   static int[][] getCroppedFieldAsArray(int[][] src) {
-    int up = -1, down = src.length, left = -1, right = src[0].length;
+    int up = 0, down = src.length, left = 0, right = src[0].length;
     for (int i = 0; i < src.length; i++) {
       for (int j = 0; j < src[i].length; j++) {
         if (src[i][j] == 1) {
-          if (i < down) down = i;
-          if (i > up) up = i;
-          if (j < right) right = j;
-          if (j > left) left = j;
+          up = Math.max(up, i);
+          down = Math.min(down, i);
+          left = Math.max(left, j);
+          right = Math.min(right, j);
         }
       }
     }
+
     int[][] trimmed = new int[up - down + 1][left - right + 1];
     for (int i = down, j = 0; i <= up; i++) {
       System.arraycopy(src[i], right, trimmed[j++], 0, left - right + 1);
