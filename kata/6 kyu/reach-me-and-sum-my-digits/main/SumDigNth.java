3b3bfcfaad5d32cb1cd8bd79dce1246a1b40b108
Paranoid User <pilgrim.whynot@gmail.com>
2020-03-10 21:01:49 -0700

    [fix]: https://www.codewars.com/kata/reach-me-and-sum-my-digits

--- kata/6 kyu/reach-me-and-sum-my-digits/main/SumDigNth.java
+++ kata/6 kyu/reach-me-and-sum-my-digits/main/SumDigNth.java
@@ -4,7 +4,7 @@ import static java.util.stream.LongStream.of;
 class SumDigNth {
   static long sumDigNthTerm(long initval, long[] patternl, int nthterm) {
     long a = stream(patternl, 0, --nthterm % patternl.length).sum();
-    long b = of(patternl).sum() * nthterm / patternl.length;
+    long b = nthterm / patternl.length * of(patternl).sum();
     return (initval + a + b + "").chars().map(c -> c - 48).sum();
   }
 }
