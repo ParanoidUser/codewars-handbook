706189a53cb514268f2e5a3bc6e1d15bfa29065d
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2020-03-10 20:48:59 -0700

    https://sonarcloud.io/project/issues?id=codewars-solutions&issues=AXBrE-ruLydf2EQCWugp&open=AXBrE-ruLydf2EQCWugp

--- kata/6 kyu/fruit-machine/main/Solution.java
+++ kata/6 kyu/fruit-machine/main/Solution.java
@@ -1,13 +1,13 @@
+import static java.util.stream.IntStream.range;
+
 class Solution {
   static int fruit(String[][] reels, int[] spins) {
-    for (int i = 0; i < 3; i++) {
-      spins[i] = "JaQuKiBaChSeShBeStWi".indexOf(reels[i][spins[i]].substring(0, 2)) / 2 + 1;
+    int xxx = 0, score = 0;
+    int value = range(0, 3).map(i -> 16 << "JaQuKiBaChSeShBeStWi".indexOf(reels[i][spins[i]].substring(0, 2))).sum();
+    for (int i = 0; i < 11; i++) {
+      xxx = (value /= 4) % 4;
+      score += xxx > 2 ? 10 * i : xxx > 1 ? i : 0;
     }
-    int multi = spins[0] == spins[1] && spins[1] == spins[2] ? 10 : 0;
-    if (multi == 0 && (spins[0] == spins[1] || spins[1] == spins[2] || spins[0] == spins[2])) {
-      multi = (spins[0] == spins[1] ? spins[2] : spins[1] == spins[2] ? spins[0] : spins[1]) < 10 ? 1 : 2;
-      spins[0] = spins[1] == spins[2] ? spins[1] : spins[0];
-    }
-    return multi * spins[0];
+    return xxx < 2 ? ++xxx * score : score;
   }
 }
