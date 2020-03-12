bfec6d6856326375c9bf1b9f1d9716cc0b6df6a9
Paranoid User <pilgrim.whynot@gmail.com>
2020-03-11 23:19:14 -0700

    fix: methods should not return constants (java:S3400)

--- kata/8 kyu/subtract-the-sum/main/Kata.java
+++ kata/8 kyu/subtract-the-sum/main/Kata.java
@@ -1,5 +1,10 @@
 class Kata {
   static String subtractSum(int n) {
-    return "apple";
+    int sum = 0, num = n;
+    while (num > 0) {
+      sum += num % 10;
+      num /= 10;
+    }
+    return (n - sum) % 9 > 0 ? null : "apple";
   }
 }
