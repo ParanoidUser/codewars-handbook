888801816be973f2ff6979334f96ddecfbcab648
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2020-03-12 16:22:57 -0700

    refactor: printf-style format strings should be used correctly (java:S3457)

--- kata/6 kyu/easy-balance-checking/main/EasyBal.java
+++ kata/6 kyu/easy-balance-checking/main/EasyBal.java
@@ -1,12 +1,12 @@
 class EasyBal {
   static String balance(String book) {
     var lines = book.replaceAll("[^\\w\n. ]", "").split("\n");
-    var report = new StringBuilder("Original Balance: " + lines[0] + "\\r\\n");
+    var report = new StringBuilder("Original Balance: " + lines[0] + (book = "\\r\\n"));
     double balance = Double.parseDouble(lines[0]), sum = 0;
     for (int i = 1; i < lines.length; i++) {
       sum += Double.parseDouble(lines[i].split(" ")[2]);
-      report.append(lines[i].trim().replaceAll("\\s+", " ")).append(String.format(" Balance %.2f", balance - sum)).append("\\r\\n");
+      report.append(lines[i].trim().replaceAll("\\s+", " ")).append(String.format(" Balance %.2f", balance - sum)).append(book);
     }
-    return report + String.format("Total expense  %.2f\\r\\nAverage expense  %.2f", sum, sum / (lines.length - 1));
+    return report + String.format("Total expense  %.2f%sAverage expense  %.2f", sum, book, sum / (lines.length - 1));
   }
 }
