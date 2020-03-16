4745974853efad0921665e0481168a41c3afd651
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2020-03-15 22:56:21 -0700

    feat: https://www.codewars.com/kata/math-dot-pow

---
+++ kata/7 kyu/math-dot-pow/main/KidMath.java
@@ -0,0 +1,7 @@
+import java.math.BigInteger;
+
+class KidMath {
+  static BigInteger pow(BigInteger base, BigInteger exponent) {
+    return exponent.intValue() < 1 ? BigInteger.valueOf(-1) : base.pow(BigInteger.TWO.pow(exponent.intValue() - 1).intValue());
+  }
+}
