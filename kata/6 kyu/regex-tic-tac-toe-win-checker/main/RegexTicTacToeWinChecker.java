754363d0073e52aede9c61c7fa103d2d219ead61
Paranoid User <pilgrim.whynot@gmail.com>
2020-03-18 19:01:15 -0700

    feat: https://www.codewars.com/kata/regex-tic-tac-toe-win-checker

---
+++ kata/6 kyu/regex-tic-tac-toe-win-checker/main/RegexTicTacToeWinChecker.java
@@ -0,0 +1,5 @@
+class RegexTicTacToeWinChecker {
+  static boolean regexTicTacToeWinChecker(String board) {
+    return board.matches(".*(\\w)(..(\\1|.\\1.)..\\1.*|.\\1.\\1..|\\1\\1(...)*)");
+  }
+}
