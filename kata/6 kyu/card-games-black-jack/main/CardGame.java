e6ed9c8ece7737968e8a85977099ace632203d06
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2020-03-24 21:51:59 -0700

    refact(java:S3776): cognitive complexity of methods should not be too high

--- kata/6 kyu/card-games-black-jack/main/CardGame.java
+++ kata/6 kyu/card-games-black-jack/main/CardGame.java
@@ -2,57 +2,34 @@ import static java.util.stream.Stream.of;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
+import java.util.Map;
+import java.util.function.Predicate;
 
 class CardGame {
   static String[] getBlackJackWinners(String[] p1, String[] p2, String[] p3, String[] croupier, String... cards) {
-    int croupierHand = hand(croupier);
-    if (croupierHand == 21 && croupier.length == 2) {
-      return new String[0];
-    }
-
-    List<String> croupierCards = new ArrayList<>(Arrays.asList(croupier));
-    while (croupierHand < 17 && cards.length > 0) {
-      croupierCards.add(cards[0]);
-      croupierHand = hand(croupierCards.toArray(String[]::new));
+    int house;
+    while ((house = count(croupier)) < 17 && cards.length > 0) {
+      croupier = Arrays.copyOf(croupier, croupier.length + 1);
+      croupier[croupier.length - 1] = cards[0];
       cards = Arrays.copyOfRange(cards, 1, cards.length);
     }
 
-    int p1Hand = hand(p1);
-    int p2Hand = hand(p2);
-    int p3Hand = hand(p3);
+    Predicate<String[]> blackjack = hand -> hand.length == 2 && count(hand) == 21;
 
-    List<String> luckies = new ArrayList<>();
-    if (croupierHand > 21) {
-      if (p1Hand < 22) {
-        luckies.add("Player 1");
-      }
-      if (p2Hand < 22) {
-        luckies.add("Player 2");
-      }
-      if (p3Hand < 22) {
-        luckies.add("Player 3");
-      }
-    } else {
-      if (p1Hand == 21 && p1.length == 2 || p1Hand < 22 && p1Hand > croupierHand) {
-        luckies.add("Player 1");
-      }
-      if (p2Hand == 21 && p2.length == 2 || p2Hand < 22 && p2Hand > croupierHand) {
-        luckies.add("Player 2");
-      }
-      if (p3Hand == 21 && p3.length == 2 || p3Hand < 22 && p3Hand > croupierHand) {
-        luckies.add("Player 3");
-      }
+    var win = new ArrayList<String>();
+    if (!blackjack.test(croupier)) {
+      var players = Map.of(1, p1, 2, p2, 3, p3);
+      for (int i = 1; i <= players.size(); i++)
+        if (blackjack.test(players.get(i)) || count(players.get(i)) < 22 && (house > 21 || count(players.get(i)) > house))
+          win.add("Player " + i);
     }
-    return luckies.toArray(String[]::new);
+    return win.toArray(new String[0]);
   }
 
-  static int hand(String[] cards) {
+  static int count(String[] cards) {
     int hand = of(cards).mapToInt(c -> Math.min(" A2345678910JQK".indexOf(c), 10)).sum();
     for (String card : cards) {
-      if ("A".equals(card) && hand < 12) {
-        hand += 10;
-      }
+      if ("A".equals(card) && hand < 12) hand += 10;
     }
     return hand;
   }
