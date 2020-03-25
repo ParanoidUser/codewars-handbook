f1c0c983c9f11ba3dd10198ab2c134a598bb928f
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2020-03-24 18:51:28 -0700

    refact(java:S3776): cognitive complexity of methods should not be too high

--- kata/6 kyu/texting-with-an-old-school-mobile-phone/main/Kata.java
+++ kata/6 kyu/texting-with-an-old-school-mobile-phone/main/Kata.java
@@ -1,18 +1,16 @@
 class Kata {
+  static final String[] keymap = {"0", "1111", "", "#-", "", "", "", "*", "", "", "*-", "***", "11", "**", "1", "", "0-", "1-", "2-", "3-", "4-", "5-", "6-", "7-", "8-", "9-", "", "", "", "****", "", "111", "", "2", "22", "222", "3", "33", "333", "4", "44", "444", "5", "55", "555", "6", "66", "666", "7", "77", "777", "7777", "8", "88", "888", "9", "99", "999", "9999"};
+
   static String sendMessage(String message) {
     boolean low = true;
-    char prev = 0;
     var keys = new StringBuilder();
     for (char c : message.toCharArray()) {
-      if (Character.isLetter(c) && Character.isLowerCase(c) ^ low) {
-        keys.append('#');
+      if (Character.isLowerCase(c) && !low || Character.isUpperCase(c) && low) {
+        keys.append("#");
         low = !low;
-        prev = 0;
       }
-      int i = "1.,?!2abc_3def_4ghi_5jkl_6mno_7pqrs8tuv_9wxyz*'-+=0 ___#".indexOf(Character.toLowerCase(c));
-      char key = i > 54 ? '#' : i > 49 ? '0' : i > 44 ? '*' : Character.forDigit(i / 5 + 1, 10);
-      keys.append(key == prev ? " " : "").append(i % 5 > 0 ? (key + "").repeat(i % 5) : (key + "-"));
-      prev = i % 5 > 0 ? key : 0;
+      var key = keymap[Character.toUpperCase(c) - ' '];
+      keys.append(keys.length() > 0 && keys.charAt(keys.length() - 1) == key.charAt(0) ? " " : "").append(key);
     }
     return keys.toString();
   }
