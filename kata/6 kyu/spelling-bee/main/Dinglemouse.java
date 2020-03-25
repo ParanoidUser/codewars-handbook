72fe8abc246337506a72a2f8ca6d82075bbc62c3
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2020-03-24 19:57:51 -0700

    refact(java:S3776): cognitive complexity of methods should not be too high

--- kata/6 kyu/spelling-bee/main/Dinglemouse.java
+++ kata/6 kyu/spelling-bee/main/Dinglemouse.java
@@ -1,15 +1,25 @@
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.IntStream.range;
+import static java.util.stream.Stream.iterate;
+import static org.apache.commons.lang3.tuple.Pair.of;
+
+import java.util.function.BiPredicate;
+
 class Dinglemouse {
   static int howManyBees(char[][] hive) {
+    var bs = iterate(0, i -> hive != null && i < hive.length, i -> i + 1).flatMap(i -> range(0, hive[i].length).filter(j -> hive[i][j] == 'b').mapToObj(j -> of(i, j))).collect(toList());
+
+    BiPredicate<Integer, Integer> up = (i, j) -> i > 1 && hive[i - 1][j] == 'e' && hive[i - 2][j] == 'e';
+    BiPredicate<Integer, Integer> down = (i, j) -> i < hive.length - 2 && hive[i + 1][j] == 'e' && hive[i + 2][j] == 'e';
+    BiPredicate<Integer, Integer> left = (i, j) -> j > 1 && hive[i][j - 1] == 'e' && hive[i][j - 2] == 'e';
+    BiPredicate<Integer, Integer> right = (i, j) -> j < hive[i].length - 2 && hive[i][j + 1] == 'e' && hive[i][j + 2] == 'e';
+
     int bees = 0;
-    for (int i = 0; hive != null && i < hive.length; i++) {
-      for (int j = 0; j < hive[i].length; j++) {
-        if (hive[i][j] == 'b') {
-          if (i > 1 && hive[i - 1][j] == 'e' && hive[i - 2][j] == 'e') bees++;
-          if (j > 1 && hive[i][j - 1] == 'e' && hive[i][j - 2] == 'e') bees++;
-          if (j < hive[i].length - 2 && hive[i][j + 1] == 'e' && hive[i][j + 2] == 'e') bees++;
-          if (i < hive.length - 2 && hive[i + 1][j] == 'e' && hive[i + 2][j] == 'e') bees++;
-        }
-      }
+    for (var ij : bs) {
+      if (up.test(ij.getLeft(), ij.getRight())) bees++;
+      if (down.test(ij.getLeft(), ij.getRight())) bees++;
+      if (left.test(ij.getLeft(), ij.getRight())) bees++;
+      if (right.test(ij.getLeft(), ij.getRight())) bees++;
     }
     return bees;
   }
