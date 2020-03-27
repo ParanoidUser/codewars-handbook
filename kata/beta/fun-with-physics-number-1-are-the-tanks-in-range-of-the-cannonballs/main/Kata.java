43a79962901b9cef08c244f87e1ab544767372dd
Paranoid User <pilgrim.whynot@gmail.com>
2020-03-27 00:29:30 -0700

    feat(beta): https://www.codewars.com/kata/fun-with-physics-number-1-are-the-tanks-in-range-of-the-cannonballs

---
+++ kata/beta/fun-with-physics-number-1-are-the-tanks-in-range-of-the-cannonballs/main/Kata.java
@@ -0,0 +1,5 @@
+class Kata {
+  static boolean areTanksInRange(double tankDistance, double initialVelocity, double angle) {
+    return Math.pow(initialVelocity, 2) / 9.8 * Math.sin(Math.toRadians(2 * angle)) >= tankDistance;
+  }
+}
