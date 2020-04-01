fb35c06710d9090d4b391319c428c0c2863322b2
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2020-04-01 15:44:25 -0700

    feat(beta): https://www.codewars.com/flip-it

---
+++ kata/beta/flip-it/main/Robot.java
@@ -0,0 +1,5 @@
+class Robot {
+  static String reverse(String str) {
+    return new StringBuilder(str).reverse().toString();
+  }
+}
---
+++ kata/beta/flip-it/test/RobotTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class RobotTest {
+  @Test
+  public void haha() {
+    assertEquals(Robot.reverse("Emma"), "ammE");
+  }
+} 
