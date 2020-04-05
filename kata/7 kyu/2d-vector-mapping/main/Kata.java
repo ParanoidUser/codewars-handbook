694478fb19b16dda2e558b20dc7436316aa226ac
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2020-04-05 01:07:44 -0700

    feat(7 kyu): https://www.codewars.com/kata/2d-vector-mapping

---
+++ kata/7 kyu/2d-vector-mapping/main/Kata.java
@@ -0,0 +1,9 @@
+class Kata {
+  static double[] mapVector(double[] vector, double[] circle1, double[] circle2) {
+    var ratio = circle2[2] / circle1[2];
+    return new double[]{
+        circle2[0] - (circle1[0] - vector[0]) * ratio,
+        circle2[1] - (circle1[1] - vector[1]) * ratio
+    };
+  }
+}
---
+++ kata/7 kyu/2d-vector-mapping/test/SolutionTest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.assertArrayEquals;
+
+import org.junit.Test;
+
+public class SolutionTest {
+  @Test
+  public void basicTests() {
+    assertArrayEquals(new double[]{46, 58}, Kata.mapVector(new double[]{46, 58}, new double[]{0, 0, 100}, new double[]{0, 0, 100}), 1e-2);
+    assertArrayEquals(new double[]{125, 10}, Kata.mapVector(new double[]{50, 88}, new double[]{-25, 128, 100}, new double[]{50, 50, 100}), 1e-2);
+    assertArrayEquals(new double[]{147.78, -87.0}, Kata.mapVector(new double[]{120, 58}, new double[]{100, 76, 36}, new double[]{120, -62, 50}), 1e-2);
+    assertArrayEquals(new double[]{-107.14, -63.43}, Kata.mapVector(new double[]{5, 5}, new double[]{10, 20, 42}, new double[]{-100, -42, 60}), 1e-2);
+  }
+}
