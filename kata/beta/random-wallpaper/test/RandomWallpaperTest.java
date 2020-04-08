964f814c768d0998aa136aefce3220761ae47282
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2020-04-07 20:23:11 -0700

    test: fix random order case

--- kata/beta/random-wallpaper/test/RandomWallpaperTest.java
+++ kata/beta/random-wallpaper/test/RandomWallpaperTest.java
@@ -11,7 +11,10 @@ public class RandomWallpaperTest {
     int[] wallpapers = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
     int[] shuffled = RandomWallpaper.randomOrder(wallpapers);
     assertEquals(wallpapers.length, shuffled.length);
-    assertNotEquals(Arrays.toString(wallpapers), Arrays.toString(shuffled));
+    if (Arrays.equals(wallpapers, shuffled)) {
+      shuffled = RandomWallpaper.randomOrder(wallpapers);
+      assertNotEquals(Arrays.toString(wallpapers), Arrays.toString(shuffled));
+    }
     Arrays.sort(wallpapers);
     Arrays.sort(shuffled);
     assertArrayEquals(wallpapers, shuffled);
