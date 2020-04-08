ce8baf1fd32826ca496f386dff6a2a029a01e870
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2020-04-07 20:22:31 -0700

    test: fix Assert deprecate warning

--- kata/6 kyu/sortable-shapes/test/SortableShapesTests.java
+++ kata/6 kyu/sortable-shapes/test/SortableShapesTests.java
@@ -1,9 +1,8 @@
-import static junit.framework.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-
 import org.junit.Test;
 
 public class SortableShapesTests {
