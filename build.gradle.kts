7e859f7cccbfce1fce55160376d04ec46bb63f59
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2020-04-07 20:20:20 -0700

    build: bump junit to 5.6.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,8 +38,8 @@ subprojects {
         implementation("org.apache.commons", "commons-lang3", "3.9")
         compileOnly("org.projectlombok", "lombok", "1.18.12")
         annotationProcessor("org.projectlombok", "lombok", "1.18.12")
-        testImplementation("org.junit.jupiter", "junit-jupiter", "5.6.0")
-        testRuntimeOnly("org.junit.vintage", "junit-vintage-engine", "5.6.0")
+        testImplementation("org.junit.jupiter", "junit-jupiter", "5.6.1")
+        testRuntimeOnly("org.junit.vintage", "junit-vintage-engine", "5.6.1")
         testImplementation("junit", "junit", "4.13")
     }
 
