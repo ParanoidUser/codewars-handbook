2d1ccc514541c2e768ab07d041e7768f7e373ad1
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2020-04-08 20:25:11 -0700

    refactor(S1197): make counter a local variable
    Array designators "[]" should be on the type, not the variable
    
    https://sonarcloud.io/project/issues?id=codewars-handbook&issues=AXE5Gm_P-4yDQ75Yl8lf&open=AXE5Gm_P-4yDQ75Yl8lf

--- kata/4 kyu/snail/test/SnailTest.java
+++ kata/4 kyu/snail/test/SnailTest.java
@@ -1,10 +1,11 @@
-import org.junit.Assert;
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
-public class SnailTest {
+import org.junit.jupiter.api.Test;
+
+class SnailTest {
   @Test
-  public void snailTest1() {
+  void snailTest1() {
     int[][] array = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-    Assert.assertArrayEquals(new int[] {1, 2, 3, 6, 9, 8, 7, 4, 5}, Snail.snail(array));
+    assertArrayEquals(new int[]{1, 2, 3, 6, 9, 8, 7, 4, 5}, Snail.snail(array));
   }
 }
