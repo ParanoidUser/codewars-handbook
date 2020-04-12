945abede1d9b6b739e2239d25fb045abf6bbbc8d
Paranoid User <pilgrim.whynot@gmail.com>
2020-04-12 00:20:16 -0700

    refactor(S3740): generalize the list
    Raw types should not be used

--- kata/7 kyu/describe-a-list/main/Kata.java
+++ kata/7 kyu/describe-a-list/main/Kata.java
@@ -1,7 +1,7 @@
 import java.util.List;
 
 class Kata {
-  static String describeList(List list) {
+  static String describeList(List<Object> list) {
     return list.isEmpty() ? "empty" : list.size() > 1 ? "longer" : "singleton";
   }
 }
