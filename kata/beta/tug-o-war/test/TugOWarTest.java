03d4b7619839441f545b356e1a4f68db9887c405
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2020-04-17 00:06:31 -0700

    test(S3415): swap assertion arguments
    Assertion arguments should be passed in the correct order
    
    https://sonarcloud.io/project/issues?branch=develop&id=codewars-handbook&issues=AXGBvisbvqN-V9zHt4_g&open=AXGBvisbvqN-V9zHt4_g
    https://sonarcloud.io/project/issues?branch=develop&id=codewars-handbook&issues=AXGBvisbvqN-V9zHt4_h&open=AXGBvisbvqN-V9zHt4_h
    https://sonarcloud.io/project/issues?branch=develop&id=codewars-handbook&issues=AXGBvisbvqN-V9zHt4_i&open=AXGBvisbvqN-V9zHt4_i

--- kata/beta/tug-o-war/test/TugOWarTest.java
+++ kata/beta/tug-o-war/test/TugOWarTest.java
@@ -5,8 +5,8 @@ import org.junit.jupiter.api.Test;
 class TugOWarTest {
   @Test
   void testTugOWar() {
-    assertEquals(TugOWar.tugOWar(new int[][]{{1, 2, 3, 4, 6}, {5, 4, 3, 2, 1}}), "Team 1 wins!");
-    assertEquals(TugOWar.tugOWar(new int[][]{{1, 2, 3, 4, 5}, {1, 2, 3, 4, 5}}), "Team 2 wins!");
-    assertEquals(TugOWar.tugOWar(new int[][]{{1, 2, 3, 4, 5}, {5, 4, 3, 2, 1}}), "It's a tie!");
+    assertEquals("Team 1 wins!", TugOWar.tugOWar(new int[][]{{1, 2, 3, 4, 6}, {5, 4, 3, 2, 1}}));
+    assertEquals("Team 2 wins!", TugOWar.tugOWar(new int[][]{{1, 2, 3, 4, 5}, {1, 2, 3, 4, 5}}));
+    assertEquals("It's a tie!", TugOWar.tugOWar(new int[][]{{1, 2, 3, 4, 5}, {5, 4, 3, 2, 1}}));
   }
 }
