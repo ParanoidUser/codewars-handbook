4774e8353810c9aa50c29f29a0a1a4b4fbe29eb5
Paranoid User <pilgrim.whynot@gmail.com>
2020-04-17 21:26:03 -0700

    feat(beta): https://www.codewars.com/kata/the-number-of-digits-of-a-power-of-2

---
+++ kata/beta/the-number-of-digits-of-a-power-of-2/test/TestCase.java
@@ -0,0 +1,20 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class TestCase {
+  @Test
+  void test() {
+    assertEquals(1L, Power.digit(1L));
+    assertEquals(1L, Power.digit(2L));
+    assertEquals(1L, Power.digit(3L));
+    assertEquals(2L, Power.digit(4L));
+    assertEquals(4L, Power.digit(10L));
+    assertEquals(4L, Power.digit(11L));
+  }
+  @Test
+  void hugeTest() {
+    assertEquals(28915723780146183L, Power.digit(96055955209270208L));
+    assertEquals(15443817617893451L, Power.digit(51303251637196672L));
+  }
+}
