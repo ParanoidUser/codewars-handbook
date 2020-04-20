48f8f02ac9f180df3b03b87f269cb0eae9d2710f
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2020-04-19 23:27:01 -0700

    feat(beta): https://www.codewars.com/kata/fibonacci-number-1

---
+++ kata/beta/fibonacci-number-1/README.md
@@ -0,0 +1,7 @@
+# [Fibonacci Number](https://www.codewars.com/kata/fibonacci-number-1 "https://www.codewars.com/kata/59160107cdee8d34ed000134")
+
+Fibonacci number
+---------------
+Please follow the requirements :-) 
+* Should return n-th member of Fibonacci sequence. 
+* Throw IllegalArgumentException for negative numbers .
---
+++ kata/beta/fibonacci-number-1/test/FibonacciTest.java
@@ -0,0 +1,14 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class FibonacciTest {
+  @Test
+  void testSomething() {
+    assertEquals(1, Fibonacci.fib(1));
+    assertEquals(1, Fibonacci.fib(2));
+    assertEquals(2, Fibonacci.fib(3));
+    assertEquals(3, Fibonacci.fib(4));
+    assertEquals(5, Fibonacci.fib(5));
+  }
+}
