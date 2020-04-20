ec26e8aa66811e52768550032bb9de2ff78fdb42
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2020-04-20 11:14:39 -0700

    refactor(S3776): extract method
    Cognitive Complexity of methods should not be too high
    
    https://sonarcloud.io/project/issues?branch=develop&id=codewars-handbook&issues=AXFtfqH5crUDfEs8ynlY&open=AXFtfqH5crUDfEs8ynlY

--- kata/6-kyu/traffic-lights-one-car/main/Dinglemouse.java
+++ kata/6-kyu/traffic-lights-one-car/main/Dinglemouse.java
@@ -5,15 +5,19 @@ class Dinglemouse {
     String[] moves = new String[n + 1];
     road = (moves[0] = road).replace("C", ".");
 
-    int[] lights = range(0, road.length()).filter(i -> "RG".indexOf(moves[0].charAt(i)) > -1).toArray();
     for (int car = moves[0].indexOf('C'); n > 0; n--) {
-      for (int l : lights) {
-        int state = ((moves[0].charAt(l) == 'G' ? 0 : 6) + moves.length - n) % 11;
-        road = road.substring(0, l) + (state < 5 ? 'G' : state > 5 ? 'R' : 'O') + road.substring(l + 1);
-      }
+      road = switchLights(road, moves.length - n, moves[0]);
       if (car > road.length() - 2 || ".G".indexOf(road.charAt(car + 1)) > -1) car++;
       moves[moves.length - n] = car < road.length() ? road.substring(0, car) + 'C' + road.substring(car + 1) : road;
     }
     return moves;
+  }
+
+  private static String switchLights(String road, int n, String move) {
+    for (int l : range(0, road.length()).filter(i -> "RG".indexOf(move.charAt(i)) > -1).toArray()) {
+      int state = ((move.charAt(l) == 'G' ? 0 : 6) + n) % 11;
+      road = road.substring(0, l) + (state < 5 ? 'G' : state > 5 ? 'R' : 'O') + road.substring(l + 1);
+    }
+    return road;
   }
 }
