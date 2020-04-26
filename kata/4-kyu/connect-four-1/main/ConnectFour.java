d874e7acb130fca71e772ff59ba8510f6a0e9c92
Paranoid User <pilgrim.whynot@gmail.com>
2020-04-25 17:16:06 -0700

    refactor(3-kyu): index out of bound check

--- kata/4-kyu/connect-four-1/main/ConnectFour.java
+++ kata/4-kyu/connect-four-1/main/ConnectFour.java
@@ -1,15 +1,15 @@
 import java.util.List;
 
-class ConnectFour {
+interface ConnectFour {
   static String whoIsWinner(List<String> piecesPositionList) {
     var board = new String[7][6];
     for (String piece : piecesPositionList) {
-      int row, column = piece.charAt(0) - 65;
-      for (row = 0; board[column][row] != null; row++) ;
+      int column = piece.charAt(0) - 65, row = 0;
+      for (; row < board[column].length - 1 && board[column][row] != null; row++);
 
       board[column][row] = piece.substring(2);
       for (var step : new int[][] {{0, 1}, {1, 0}, {1, 1}, {1, -1}}) {
-        var result = move(column, row, step, piece.substring(2), board);
+        var result = move(column, row, step, board[column][row], board);
         if (!result.isEmpty()) {
           return result;
         }
@@ -19,9 +19,9 @@ class ConnectFour {
   }
 
   private static String move(int column, int row, int[] step, String piece, String[][] board) {
-    for (int n = 0, f = 1; f >= -1; f -= 2) {
+    for (int n = 0, f = 1; f > -2; f -= 2) {
       for (int c = column, r = row;
-          c >= 0 && c < 7 && r >= 0 && r < 6 && piece.equals(board[c][r]);
+          c >= 0 && c < board.length && r >= 0 && r < board[c].length && piece.equals(board[c][r]);
           c += step[0] * f, r += step[1] * f) {
         if (++n > 4) {
           return piece;
