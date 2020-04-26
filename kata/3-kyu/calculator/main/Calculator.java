3f5b6e2a0fac9570fde3a6e35f660e992b86878e
Paranoid User <pilgrim.whynot@gmail.com>
2020-04-25 17:10:05 -0700

    refactor(3-kyu): rewrite without using nashorn

--- kata/3-kyu/calculator/main/Calculator.java
+++ kata/3-kyu/calculator/main/Calculator.java
@@ -1,12 +1,18 @@
-import javax.script.ScriptEngineManager;
+import static java.util.Collections.singletonList;
 
-class Calculator {
+import java.util.LinkedList;
+
+interface Calculator {
   static Double evaluate(String expression) {
-    try {
-      var engine = new ScriptEngineManager().getEngineByName("JavaScript");
-      return Double.parseDouble(String.valueOf(engine.eval(expression)));
-    } catch (Exception cause) {
-      return 0.0;
+    var args = expression.split(" ");
+    var stack = new LinkedList<>(singletonList(Double.valueOf(args[0])));
+    for (int i = 1; i < args.length; i += 2) {
+      switch (args[i]) {
+        case "*": stack.push(stack.pop() * Double.parseDouble(args[i + 1])); break;
+        case "/": stack.push(stack.pop() / Double.parseDouble(args[i + 1])); break;
+        default:  stack.push(Double.parseDouble(args[i + 1]) * ("+".equals(args[i]) ? 1 : -1)); break;
+      }
     }
+    return stack.stream().mapToDouble(d -> d).sum();
   }
 }
