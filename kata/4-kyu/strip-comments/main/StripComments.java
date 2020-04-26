4af96253545cf38effbf32cb4ec9e8adfc317edb
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2020-04-25 17:17:00 -0700

    refactor: use interface for util classes

--- kata/4-kyu/strip-comments/main/StripComments.java
+++ kata/4-kyu/strip-comments/main/StripComments.java
@@ -1,4 +1,4 @@
-class StripComments {
+interface StripComments {
   static String stripComments(String text, String[] commentSymbols) {
     return text.replaceAll("\\h*[" + String.join("", commentSymbols) + "].*(\\n?)", "$1")
                .replaceAll("\\h(\\n)", "$1")
