052dd58861f6cfa9d7a338261830f534adac70f5
Paranoid User <pilgrim.whynot@gmail.com>
2020-04-25 17:18:31 -0700

    test: add missing tests with junit5

--- kata/4-kyu/connect-four-1/test/MyTestConnectFour.java
+++ kata/4-kyu/connect-four-1/test/MyTestConnectFour.java
@@ -1,31 +1,43 @@
-import static org.junit.Assert.assertEquals;
+import static java.util.List.of;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.util.List;
+import org.junit.jupiter.api.Test;
 
-import org.junit.Test;
+class MyTestConnectFour {
 
-public class MyTestConnectFour {
   @Test
-  public void firstTest() {
-    var myList = List.of("A_Red", "B_Yellow", "A_Red", "B_Yellow", "A_Red", "B_Yellow", "G_Red", "B_Yellow");
-    assertEquals("it should return Yellow", "Yellow", ConnectFour.whoIsWinner(myList));
+  void firstTest() {
+    var myList = of("A_Red", "B_Yellow", "A_Red", "B_Yellow", "A_Red", "B_Yellow", "G_Red", "B_Yellow");
+    assertEquals("Yellow", ConnectFour.whoIsWinner(myList));
   }
 
   @Test
-  public void secondTest() {
-    var myList = List.of(
-            "C_Yellow", "E_Red", "G_Yellow", "B_Red", "D_Yellow", "B_Red", "B_Yellow",
-            "G_Red", "C_Yellow", "C_Red", "D_Yellow", "F_Red", "E_Yellow", "A_Red",
-            "A_Yellow", "G_Red", "A_Yellow", "F_Red", "F_Yellow", "D_Red", "B_Yellow",
-            "E_Red", "D_Yellow", "A_Red", "G_Yellow", "D_Red", "D_Yellow", "C_Red");
-    assertEquals("it should return Yellow", "Yellow", ConnectFour.whoIsWinner(myList));
+  void secondTest() {
+    var myList = of(
+        "C_Yellow", "E_Red", "G_Yellow", "B_Red", "D_Yellow", "B_Red", "B_Yellow",
+        "G_Red", "C_Yellow", "C_Red", "D_Yellow", "F_Red", "E_Yellow", "A_Red",
+        "A_Yellow", "G_Red", "A_Yellow", "F_Red", "F_Yellow", "D_Red", "B_Yellow",
+        "E_Red", "D_Yellow", "A_Red", "G_Yellow", "D_Red", "D_Yellow", "C_Red");
+    assertEquals("Yellow", ConnectFour.whoIsWinner(myList));
   }
 
   @Test
-  public void thirdTest() {
-    var myList = List.of(
-            "A_Yellow", "B_Red", "B_Yellow", "C_Red", "G_Yellow", "C_Red", "C_Yellow",
-            "D_Red", "G_Yellow", "D_Red", "G_Yellow", "D_Red", "F_Yellow", "E_Red", "D_Yellow");
-    assertEquals("it should return Red", "Red", ConnectFour.whoIsWinner(myList));
+  void thirdTest() {
+    var myList = of(
+        "A_Yellow", "B_Red", "B_Yellow", "C_Red", "G_Yellow", "C_Red", "C_Yellow",
+        "D_Red", "G_Yellow", "D_Red", "G_Yellow", "D_Red", "F_Yellow", "E_Red", "D_Yellow");
+    assertEquals("Red", ConnectFour.whoIsWinner(myList));
+  }
+
+  @Test
+  void drawTest() {
+    var myList = of("C_Yellow", "B_Red");
+    assertEquals("Draw", ConnectFour.whoIsWinner(myList));
+  }
+
+  @Test
+  void overflowTest() {
+    var myList = of("A_Yellow", "A_Red", "A_Yellow", "A_Red", "A_Yellow", "A_Red", "A_Yellow", "A_Red");
+    assertEquals("Draw", ConnectFour.whoIsWinner(myList));
   }
 }
--- kata/4-kyu/strip-comments/test/StripCommentsTest.java
+++ kata/4-kyu/strip-comments/test/StripCommentsTest.java
@@ -1,11 +1,11 @@
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-public class StripCommentsTest {
+class StripCommentsTest {
   @Test
-  public void stripComments() {
-    assertEquals("apples, pears\ngrapes\nbananas", StripComments.stripComments("apples, pears # and bananas\ngrapes\nbananas !apples", new String[] {"#", "!"}));
-    assertEquals("a\nc\nd", StripComments.stripComments("a #b\nc\nd $e f g", new String[] {"#", "$"}));
+  void sample() {
+    assertEquals("apples, pears\ngrapes\nbananas", StripComments.stripComments("apples, pears # and bananas\ngrapes\nbananas !apples", new String[]{"#", "!"}));
+    assertEquals("a\nc\nd", StripComments.stripComments("a #b\nc\nd $e f g", new String[]{"#", "$"}));
   }
 }
