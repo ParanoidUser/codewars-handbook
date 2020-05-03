97cc7ec2c8aaef892a630178087b6dc0869c4d57
Paranoid User <pilgrim.whynot@gmail.com>
2020-05-03 00:56:42 -0700

    feat(beta): https://www.codewars.com/kata/base-factorial-converter

---
+++ kata/beta/base-factorial-converter/main/FactorialBaseConverter.java
@@ -0,0 +1,25 @@
+import static java.util.stream.IntStream.rangeClosed;
+
+interface FactorialBaseConverter {
+  static int intoFactorial(String s) {
+    int i = 0, f, n = Integer.parseInt(s.trim());
+    while (n > (f = rangeClosed(1, ++i).reduce(1, (a, b) -> a * b)));
+    int fact = 0;
+    for (; i >= 0; n %= f, f = rangeClosed(1, --i).reduce(1, (a, b) -> a * b)) {
+      fact = fact * 10 + n / f;
+    }
+    return fact;
+  }
+
+  static int outOfFactorial(String s) {
+    int num = 0;
+    for (int i = 0; i < s.length(); i++) {
+      int d = s.charAt(s.length() - i - 1) - 48;
+      if (d > i) {
+        throw new IllegalArgumentException();
+      }
+      num += d * rangeClosed(1, i).reduce(1, (a, b) -> a * b);
+    }
+    return num;
+  }
+} 
