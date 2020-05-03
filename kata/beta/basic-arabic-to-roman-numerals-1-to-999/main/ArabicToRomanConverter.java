ea87b0ef865018b31c30c0c097a5262dbfcbfc3f
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2020-05-03 01:05:56 -0700

    feat(beta): https://www.codewars.com/kata/basic-arabic-to-roman-numerals-1-to-999

---
+++ kata/beta/basic-arabic-to-roman-numerals-1-to-999/main/ArabicToRomanConverter.java
@@ -0,0 +1,11 @@
+class ArabicToRomanConverter {
+  static String toRoman(int arabic) {
+    return arabic < 1 || arabic > 999 ? "NaR" : "I".repeat(arabic)
+        .replace("IIIII", "V").replace("IIII", "IV")
+        .replace("VV", "X").replace("VIV", "IX")
+        .replace("XXXXX", "L").replace("XXXX", "XL")
+        .replace("LL", "C").replace("LXL", "XC")
+        .replace("CCCCC", "D").replace("CCCC", "CD")
+        .replace("DD", "M").replace("DCD", "CM");
+  }
+}
