f93f47206e33e99f0b4b1fcf0e5d6fac30d92012
Paranoid User <pilgrim.whynot@gmail.com>
2020-05-08 16:24:23 -0700

    test(6-kyu): add missing tests with junit5

--- kata/6-kyu/the-office-v-find-a-chair/test/SolutionTest.java
+++ kata/6-kyu/the-office-v-find-a-chair/test/SolutionTest.java
@@ -1,52 +1,58 @@
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-public class SolutionTest {
+class SolutionTest {
+
   @Test
-  public void basic1() {
-    Room[] rooms = {
-      new Room("XXX", 3),
-      new Room("XXXXX", 6),
-      new Room("XXXXXX", 9)
+  void sample() {
+    Room[] officeA = {
+        new Room("XXX", 3),
+        new Room("XXXXX", 6),
+        new Room("XXXXXX", 9)
     };
-    assertArrayEquals(new int[] {0, 1, 3}, (int[]) TheOffice.meeting(rooms, 4));
+    assertArrayEquals(new int[]{0, 1, 3}, (int[]) TheOffice.meeting(officeA, 4));
+
+    Room[] officeB = {
+        new Room("XXX", 1),
+        new Room("XXXXXX", 6),
+        new Room("X", 2),
+        new Room("XXXXXX", 8),
+        new Room("X", 3),
+        new Room("XXX", 1)
+    };
+    assertArrayEquals(new int[]{0, 0, 1, 2, 2}, (int[]) TheOffice.meeting(officeB, 5));
+
+    Room[] officeC = {
+        new Room("XXXX", 7),
+        new Room("XXXXXXX", 3),
+        new Room("XXX", 1),
+        new Room("XXXXXXXX", 3),
+        new Room("XXXXXX", 3),
+        new Room("XX", 5)
+    };
+    assertArrayEquals(new int[]{3, 0, 0, 0, 0, 1}, (int[]) TheOffice.meeting(officeC, 4));
   }
 
   @Test
-  public void basic2() {
-    Room[] rooms = {
-      new Room("XXX", 1),
-      new Room("XXXXXX", 6),
-      new Room("X", 2),
-      new Room("XXXXXX", 8),
-      new Room("X", 3),
-      new Room("XXX", 1)
+  void no_chairs_are_needed() {
+    Room[] office = {
+        new Room("XX", 2),
+        new Room("XXXX", 6),
+        new Room("XXXXX", 4)
     };
-    assertArrayEquals(new int[] {0, 0, 1, 2, 2}, (int[]) TheOffice.meeting(rooms, 5));
+    assertEquals("Game On", TheOffice.meeting(office, 0));
   }
 
   @Test
-  public void basic3() {
-    Room[] rooms = {
-      new Room("XX", 2),
-      new Room("XXXX", 6),
-      new Room("XXXXX", 4)
+  void not_enough_chairs() {
+    Room[] office = {
+        new Room("XX", 2),
+        new Room("XXXX", 6),
+        new Room("XXXXX", 4)
     };
-    assertEquals("Game On", TheOffice.meeting(rooms, 0));
-  }
-
-  @Test
-  public void basic4() {
-    Room[] rooms = {
-      new Room("XXXX", 7),
-      new Room("XXXXXXX", 3),
-      new Room("XXX", 1),
-      new Room("XXXXXXXX", 3),
-      new Room("XXXXXX", 3),
-      new Room("XX", 5)
-    };
-    assertArrayEquals(new int[] {3, 0, 0, 0, 0, 1}, (int[]) TheOffice.meeting(rooms, 4));
+    assertEquals("Not enough!", TheOffice.meeting(office, 3));
+    assertEquals("Not enough!", TheOffice.meeting(new Room[0], 1));
   }
 }
