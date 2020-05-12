e61005b25709a1b8581215719825bacf5fdfc0d5
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2020-05-11 21:54:27 -0700

    test(8-kyu): add missing tests with junit5

--- kata/8-kyu/sum-without-highest-and-lowest-number/test/SolutionTest.java
+++ kata/8-kyu/sum-without-highest-and-lowest-number/test/SolutionTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 class SolutionTest {
   @Test
   void sample() {
+    assertEquals(0, Kata.sum(null));
+    assertEquals(0, Kata.sum(new int[0]));
     assertEquals(16, Kata.sum(new int[]{6, 2, 1, 8, 10}));
   }
 }
