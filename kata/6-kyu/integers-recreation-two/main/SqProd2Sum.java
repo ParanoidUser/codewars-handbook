9da8c77af751865f8e7060fc274e843467e4ac1c
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2020-05-16 16:49:39 -0700

    feat(6-kyu): https://www.codewars.com/kata/integers-recreation-two

---
+++ kata/6-kyu/integers-recreation-two/main/SqProd2Sum.java
@@ -0,0 +1,11 @@
+import static java.util.stream.LongStream.of;
+
+import java.util.List;
+
+interface SqProd2Sum {
+  static List<long[]> prod2Sum(long a, long b, long c, long d) {
+    var ef1 = of(a * c + b * d, Math.abs(a * d - b * c)).sorted().toArray();
+    var ef2 = of(a * d + b * c, Math.abs(a * c - b * d)).sorted().toArray();
+    return ef1[0] == ef2[0] && ef1[1] == ef2[1] ? List.of(ef1) : List.of(ef1, ef2);
+  }
+}
