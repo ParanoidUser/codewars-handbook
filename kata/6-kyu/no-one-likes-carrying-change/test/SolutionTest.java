ea59c91254d085782367f672469dafe3589d9aea
Paranoid User <pilgrim.whynot@gmail.com>
2020-05-22 23:38:07 -0700

    feat(6-kyu): https://www.codewars.com/kata/no-one-likes-carrying-change

---
+++ kata/6-kyu/no-one-likes-carrying-change/test/SolutionTest.java
@@ -0,0 +1,13 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+  @Test
+  void sample() {
+    assertEquals(3, CoinFree.solve(75, new int[]{1, 5, 10, 25}));
+    assertEquals(9, CoinFree.solve(123, new int[]{1, 5, 10, 25}));
+    assertEquals(3, CoinFree.solve(75, new int[]{1, 2, 5, 10, 20, 50}));
+    assertEquals(5, CoinFree.solve(123, new int[]{1, 2, 5, 10, 20, 50}));
+  }
+}
