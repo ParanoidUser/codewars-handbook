42409dc1f11594fc8d5c58a40206049cff0b9cd4
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2020-05-23 01:19:34 -0700

    feat(6-kyu): https://www.codewars.com/kata/simple-fun-number-165-withdraw

---
+++ kata/6-kyu/simple-fun-number-165-withdraw/test/SolutionTest.java
@@ -0,0 +1,14 @@
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+  @Test
+  void sample() {
+    assertArrayEquals(new int[]{0, 0, 2}, Solution.withdraw(40));
+    assertArrayEquals(new int[]{0, 0, 3}, Solution.withdraw(60));
+    assertArrayEquals(new int[]{1, 1, 4}, Solution.withdraw(230));
+    assertArrayEquals(new int[]{2, 1, 0}, Solution.withdraw(250));
+    assertArrayEquals(new int[]{2, 0, 3}, Solution.withdraw(260));
+  }
+}
