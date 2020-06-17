9d80bba620a4bce7fc6bb4c8ef57e21dafdf960e
Paranoid User <pilgrim.whynot@gmail.com>
2020-06-17 01:34:32 -0700

    test(S5785): use dedicated assertion methods
    JUnit assertTrue/assertFalse should be simplified to the corresponding dedicated assertion
    
    https://sonarcloud.io/organizations/paranoiduser/rules?open=java%3AS5785&rule_key=java%3AS5785

--- kata/7-kyu/null/test/ExampleTests.java
+++ kata/7-kyu/null/test/ExampleTests.java
@@ -6,11 +6,11 @@ import org.junit.jupiter.api.Test;
 class ExampleTests {
   @Test
   void sample() {
+    assertEquals("null", new Null().toString());
     assertEquals(0, new Null().hashCode());
     assertEquals(new Null().hashCode(), new Null().hashCode());
-    assertEquals(null, new Null());
+    assertEquals(new Null(), null);
     assertEquals(new Null(), new Null());
-    assertEquals("null", new Null().toString());
     Null n = new Null();
     assertEquals(n, n);
     assertNotEquals(new Object(), new Null());
