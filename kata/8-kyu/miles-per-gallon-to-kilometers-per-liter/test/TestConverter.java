b63deb89f866511ab1f5095b3c307d9e9ceaf659
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2020-06-17 01:02:03 -0700

    test(S5786): remove 'public' modifier from test method
    JUnit5 test classes and methods should have default package visibility
    
    https://sonarcloud.io/organizations/paranoiduser/rules?open=java%3AS5786&rule_key=java%3AS5786

--- kata/8-kyu/miles-per-gallon-to-kilometers-per-liter/test/TestConverter.java
+++ kata/8-kyu/miles-per-gallon-to-kilometers-per-liter/test/TestConverter.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 class TestConverter {
   @Test
-  public void sample() {
+  void sample() {
     assertEquals(3.54f, Converter.mpgToKPM(10), 0.001f);
     assertEquals(7.08f, Converter.mpgToKPM(20), 0.001f);
     assertEquals(10.62f, Converter.mpgToKPM(30), 0.001f);
