a21143dc4393029f10b1bccc13ce335898f51978
Paranoid User <pilgrim.whynot@gmail.com>
2020-06-18 00:31:41 -0700

    refactor: remove unreachable condition statements

--- kata/6-kyu/when-greatest-is-less-than-smallest/main/GreatestSmallest.java
+++ kata/6-kyu/when-greatest-is-less-than-smallest/main/GreatestSmallest.java
@@ -1,16 +1,15 @@
 import static java.math.BigInteger.ONE;
-import static java.math.BigInteger.ZERO;
 
 import java.math.BigInteger;
 
 interface GreatestSmallest {
   static BigInteger greatest(BigInteger x, BigInteger y, BigInteger n) {
     var lcm = x.multiply(y).divide(x.gcd(y));
-    return lcm.equals(ONE) ? ZERO : n.subtract(ONE).divide(lcm).multiply(lcm);
+    return n.subtract(ONE).divide(lcm).multiply(lcm);
   }
 
   static BigInteger smallest(BigInteger x, BigInteger y, BigInteger n) {
     var lcm = x.multiply(y).divide(x.gcd(y));
-    return lcm.equals(ONE) ? ZERO : n.divide(lcm).add(ONE).multiply(lcm);
+    return n.divide(lcm).add(ONE).multiply(lcm);
   }
 }
