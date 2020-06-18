26615653347bde7742d6969b24ffce06e38ff2ae
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2020-06-17 22:18:28 -0700

    feat(6-kyu): https://www.codewars.com/kata/sieve-of-eratosthenes

---
+++ kata/6-kyu/sieve-of-eratosthenes/main/Primes.java
@@ -0,0 +1,11 @@
+import static java.math.BigInteger.valueOf;
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.IntStream.iterate;
+
+import java.util.List;
+
+interface Primes {
+  static List<Integer> Primes(Integer x) {
+    return iterate(2, i -> i <= x, i -> i + 1).filter(i -> valueOf(i).isProbablePrime(10)).boxed().collect(toList());
+  }
+}
