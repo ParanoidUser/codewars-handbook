0a1162bf184403a9b2c274a7ed8f6fda11aafff1
Paranoid User <pilgrim.whynot@gmail.com>
2020-07-23 15:18:37 -0700

    refactor: replace unreachable conditions

--- kata/6-kyu/integers-recreation-two/main/SqProd2Sum.java
+++ kata/6-kyu/integers-recreation-two/main/SqProd2Sum.java
@@ -6,6 +6,6 @@ interface SqProd2Sum {
   static List<long[]> prod2Sum(long a, long b, long c, long d) {
     var ef1 = of(a * c + b * d, Math.abs(a * d - b * c)).sorted().toArray();
     var ef2 = of(a * d + b * c, Math.abs(a * c - b * d)).sorted().toArray();
-    return ef1[0] == ef2[0] && ef1[1] == ef2[1] ? List.of(ef1) : List.of(ef1, ef2);
+    return ef1[0] == ef2[0] ? List.of(ef1) : List.of(ef1, ef2);
   }
 }
