609d2d95028768bd0775e04ae4ffc8ffe5d666ce
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2020-07-23 15:18:37 -0700

    refactor: replace unreachable conditions

--- kata/6-kyu/finding-neo-pg-13/main/Morpheus.java
+++ kata/6-kyu/finding-neo-pg-13/main/Morpheus.java
@@ -1,6 +1,6 @@
 class Morpheus {
   int[] find(Matrix matrix, int neo) {
-    for (int x = 0, y = matrix.size() - 1, e; x < matrix.size() && y >= 0; x += e < neo ? 1 : 0, y -= e > neo ? 1 : 0) {
+    for (int x = 0, y = matrix.size() - 1, e; x < matrix.size() && y >= 0; e = e < neo ? x++ : y--) {
       if ((e = matrix.get(x, y)) == neo) {
         return new int[]{x, y};
       }
