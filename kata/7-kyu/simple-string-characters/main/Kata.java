f29ad21ff62a1e8010a29ce91c7059b50c9cf9a7
Paranoid User <pilgrim.whynot@gmail.com>
2020-07-23 11:20:32 -0700

    feat(7-kyu): https://www.codewars.com/kata/simple-string-characters

---
+++ kata/7-kyu/simple-string-characters/main/Kata.java
@@ -0,0 +1,10 @@
+interface Kata {
+  static int[] Solve(String word) {
+    return new int[] {
+        word.replaceAll("[^A-Z]", "").length(),
+        word.replaceAll("[^a-z]", "").length(),
+        word.replaceAll("[^0-9]", "").length(),
+        word.replaceAll("[\\w]", "").length()
+    };
+  }
+}
---
+++ kata/7-kyu/simple-string-characters/test/SolutionTest.java
@@ -0,0 +1,15 @@
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+  @Test
+  void sample() {
+    assertArrayEquals(new int[]{1, 18, 3, 2}, Kata.Solve("Codewars@codewars123.com"));
+    assertArrayEquals(new int[]{8, 6, 3, 2}, Kata.Solve("CbgA5<1d-tOwUZTS8yQ"));
+    assertArrayEquals(new int[]{9, 9, 6, 9}, Kata.Solve("P*K4%>mQUDaG$h=cx2?.Czt7!Zn16p@5H"));
+    assertArrayEquals(new int[]{15, 8, 6, 9}, Kata.Solve("RYT'>s&gO-.CM9AKeH?,5317tWGpS<*x2ukXZD"));
+    assertArrayEquals(new int[]{10, 7, 3, 6}, Kata.Solve("$Cnl)Sr<7bBW-&qLHI!mY41ODe"));
+    assertArrayEquals(new int[]{7, 13, 4, 10}, Kata.Solve("@mw>0=QD-iAx!rp9TaG?o&M%l$34L.nbft"));
+  }
+}
