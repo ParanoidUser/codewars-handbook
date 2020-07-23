4b38718fe2e3812f7efb79e462b26206adb9ddbc
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2020-07-23 11:45:07 -0700

    refactor(S5857): use character classes in regex if appropriately
    https://sonarcloud.io/organizations/paranoiduser/rules?open=java%3AS5857&rule_key=java%3AS5857

--- kata/6-kyu/alex-and-snooker-scores/main/Frame.java
+++ kata/6-kyu/alex-and-snooker-scores/main/Frame.java
@@ -1,7 +1,7 @@
 interface Frame {
   static int[] frame(String score) {
     int[] result = new int[2];
-    for (var match : score.replaceAll("\\(.*?\\)", "").split(";")) {
+    for (var match : score.replaceAll("\\([^)]+\\)", "").split(";")) {
       var one = Integer.parseInt(match.substring(0, match.indexOf('-')).trim());
       var two = Integer.parseInt(match.substring(match.indexOf('-') + 1).trim());
       result[one > two ? 0 : 1]++;
