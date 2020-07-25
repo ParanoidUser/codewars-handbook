8390059c3e2e2fdd8335f1aeefafb55fdca57775
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2020-07-25 13:35:52 -0700

    test: equals and hashcode

--- kata/6-kyu/hex-class/test/SampleTests.java
+++ kata/6-kyu/hex-class/test/SampleTests.java
@@ -1,5 +1,4 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import org.junit.jupiter.api.Test;
@@ -11,18 +10,18 @@ class SampleTests {
     assertEquals("0xFF", ff.toString());
     assertEquals(256, ff.valueOf() + 1);
     assertEquals("0xFF", ff.toJSON());
-    assertEquals(ff, new Hex(255));
     assertEquals("0xFE", ff.minus(1).toString());
     assertEquals("0x0", ff.minus(ff).toString());
     assertEquals("0xF", new Hex(10).plus(5).toString());
     assertEquals("0x1FE", ff.plus(ff).toString());
     assertEquals(32, Hex.parse("0x20"));
     assertEquals(512, Hex.parse("200"));
-    assertNotEquals(new Hex(1), new Hex(2));
-    assertNotEquals(new Hex(1), new Object());
-    assertEquals(255, new Hex(255).hashCode());
 
     assertEquals(ff, ff);
-    assertFalse(ff.equals(null));
+    assertEquals(ff, new Hex(255));
+    assertNotEquals(ff, new Hex(2));
+    assertNotEquals(ff, new Object());
+    assertNotEquals(null, ff);
+    assertEquals(255, ff.hashCode());
   }
 }
