98dfc89b7e2135a130e166cafff1adec8f78699b
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2020-07-25 01:07:12 -0700

    test: add missing tests + migrate to JUnit 5

--- kata/6-kyu/vasya-clerk/test/LineTests.java
+++ kata/6-kyu/vasya-clerk/test/LineTests.java
@@ -6,6 +6,7 @@ class LineTests {
   @Test
   void sample() {
     assertEquals("YES", Line.Tickets(new int[]{25, 25, 50}));
+    assertEquals("YES", Line.Tickets(new int[]{25, 25, 25, 100}));
     assertEquals("YES", Line.Tickets(new int[]{25, 25, 50, 100}));
     assertEquals("NO", Line.Tickets(new int[]{25, 100}));
     assertEquals("NO", Line.Tickets(new int[]{25, 25, 50, 50, 100}));
