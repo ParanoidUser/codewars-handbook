d04c2d4e534f63f54ae415be04369d9b813cd16c
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2020-08-13 14:08:51 -0700

    refactor: replace utility classes with interfaces

--- kata/8-kyu/thinkful-logic-drills-traffic-light/main/TrafficLights.java
+++ kata/8-kyu/thinkful-logic-drills-traffic-light/main/TrafficLights.java
@@ -1,4 +1,4 @@
-class TrafficLights {
+interface TrafficLights {
   static String updateLight(String current) {
     return current.equals("green") ?  "yellow" : current.equals("yellow") ? "red" : "green";
   }
