20aa3260b3a40f75c8c8a41e334bbec4fb00717b
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2020-08-13 14:09:25 -0700

    test: migrate to JUnit 5

--- kata/6-kyu/traffic-lights-one-car/test/ExampleTests.java
+++ kata/6-kyu/traffic-lights-one-car/test/ExampleTests.java
@@ -1,34 +1,31 @@
-import static org.junit.Assert.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-public class ExampleTests {
+class ExampleTests {
   @Test
-  public void example1() {
-    assertArrayEquals(new String[] {
-          "C...R............G......",
-          ".C..R............G......",
-          "..C.R............G......",
-          "...CR............G......",
-          "...CR............G......",
-          "....C............O......",
-          "....GC...........R......",
-          "....G.C..........R......",
-          "....G..C.........R......",
-          "....G...C........R......",
-          "....O....C.......R......"
-        }, Dinglemouse.trafficLights("C...R............G......", 10));
-  }
+  void sample() {
+    assertArrayEquals(new String[]{
+        "C...R............G......",
+        ".C..R............G......",
+        "..C.R............G......",
+        "...CR............G......",
+        "...CR............G......",
+        "....C............O......",
+        "....GC...........R......",
+        "....G.C..........R......",
+        "....G..C.........R......",
+        "....G...C........R......",
+        "....O....C.......R......"
+    }, Dinglemouse.trafficLights("C...R............G......", 10));
 
-  @Test
-  public void example2() {
-    assertArrayEquals(new String[] {
-          "CG...",
-          ".C...",
-          ".GC..",
-          ".G.C.",
-          ".G..C",
-          ".O...",
-        }, Dinglemouse.trafficLights("CG...", 5));
+    assertArrayEquals(new String[]{
+        "CG...",
+        ".C...",
+        ".GC..",
+        ".G.C.",
+        ".G..C",
+        ".O...",
+    }, Dinglemouse.trafficLights("CG...", 5));
   }
 }
