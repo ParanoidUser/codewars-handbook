67af136ac82b9873700386a12499bd8c6d4caff3
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2020-08-14 10:08:31 -0700

    test: add missing tests

--- kata/7-kyu/averages-of-numbers/test/SolutionTest.java
+++ kata/7-kyu/averages-of-numbers/test/SolutionTest.java
@@ -8,6 +8,6 @@ class SolutionTest {
     assertArrayEquals(new double[]{2, 2, 2, 2}, Kata.averages(new int[]{2, 2, 2, 2, 2}));
     assertArrayEquals(new double[]{0, 0, 0, 0}, Kata.averages(new int[]{2, -2, 2, -2, 2}));
     assertArrayEquals(new double[]{2, 4, 3, -4.5}, Kata.averages(new int[]{1, 3, 5, 1, -10}));
-    assertArrayEquals(new double[0], Kata.averages(new int[0]));
+    assertArrayEquals(new double[0], Kata.averages(null));
   }
 }
