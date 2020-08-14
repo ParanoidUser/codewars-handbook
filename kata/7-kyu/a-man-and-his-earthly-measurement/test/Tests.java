2949d5aeace0df2f228c1c49b93c6e0ef0888747
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2020-08-13 19:56:06 -0700

    test: round expected values to epsilon

--- kata/7-kyu/a-man-and-his-earthly-measurement/test/Tests.java
+++ kata/7-kyu/a-man-and-his-earthly-measurement/test/Tests.java
@@ -5,8 +5,8 @@ import org.junit.jupiter.api.Test;
 class Tests {
   @Test
   void sample() {
-    assertEquals(40075.00, Eratosthenes.circumference(8.1, 1.0, 800), 0.05);
-    assertEquals(91.0, Eratosthenes.circumference(1, 5, 20), 0.05);
-    assertEquals(264.0, Eratosthenes.circumference(2, 5, 50), 0.05);
+    assertEquals(40900.3, Eratosthenes.circumference(8.1, 1.0, 800), 0.1);
+    assertEquals(91.4, Eratosthenes.circumference(1, 5, 20), 0.1);
+    assertEquals(263.8, Eratosthenes.circumference(2, 5, 50), 0.1);
   }
 }
