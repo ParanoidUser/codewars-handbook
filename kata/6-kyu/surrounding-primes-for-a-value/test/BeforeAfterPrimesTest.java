8d1b362dbb56a2bb70524ff199b09c1776ad72ec
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2020-09-03 17:54:25 -0700

    fix: increase prime certainty

--- kata/6-kyu/surrounding-primes-for-a-value/test/BeforeAfterPrimesTest.java
+++ kata/6-kyu/surrounding-primes-for-a-value/test/BeforeAfterPrimesTest.java
@@ -1,19 +1,14 @@
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 import org.junit.jupiter.api.Test;
 
 class BeforeAfterPrimesTest {
   @Test
   void sample() {
-    assertEquals(89, BeforeAfterPrimes.primeBefAft(97)[0]);
-    assertEquals(101, BeforeAfterPrimes.primeBefAft(97)[1]);
-    assertEquals(97, BeforeAfterPrimes.primeBefAft(100)[0]);
-    assertEquals(101, BeforeAfterPrimes.primeBefAft(100)[1]);
-    assertEquals(97, BeforeAfterPrimes.primeBefAft(101)[0]);
-    assertEquals(103, BeforeAfterPrimes.primeBefAft(101)[1]);
-    assertEquals(113, BeforeAfterPrimes.primeBefAft(120)[0]);
-    assertEquals(127, BeforeAfterPrimes.primeBefAft(120)[1]);
-    assertEquals(127, BeforeAfterPrimes.primeBefAft(130)[0]);
-    assertEquals(131, BeforeAfterPrimes.primeBefAft(130)[1]);
+    assertArrayEquals(new long[]{89, 101}, BeforeAfterPrimes.primeBefAft(97));
+    assertArrayEquals(new long[]{97, 101}, BeforeAfterPrimes.primeBefAft(100));
+    assertArrayEquals(new long[]{97, 103}, BeforeAfterPrimes.primeBefAft(101));
+    assertArrayEquals(new long[]{113, 127}, BeforeAfterPrimes.primeBefAft(120));
+    assertArrayEquals(new long[]{127, 131}, BeforeAfterPrimes.primeBefAft(130));
   }
 }
