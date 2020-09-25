c4a4f81f4f9a20940930e9ada783b0e7a141b4a0
Paranoid User <pilgrim.whynot@gmail.com>
2020-09-25 14:09:53 -0700

    feat(6-kyu): https://www.codewars.com/kata/can-you-win-the-codewar

---
+++ kata/6-kyu/can-you-win-the-codewar/main/Kata.java
@@ -0,0 +1,29 @@
+import static java.util.Arrays.stream;
+
+import java.util.LinkedList;
+
+interface Kata {
+  static String codewarResult(int[] codewarrior, int[] opponent) {
+    LinkedList<Integer> army = stream(codewarrior).sorted().collect(LinkedList::new, LinkedList::addLast, LinkedList::addAll);
+    LinkedList<Integer> enemy = stream(opponent).sorted().collect(LinkedList::new, LinkedList::addLast, LinkedList::addAll);
+    LinkedList<Integer> draw = new LinkedList<>();
+
+    int score = 0;
+    while (!army.isEmpty()) {
+      if (army.getLast() > enemy.getLast()) {
+        army.removeLast();
+        score++;
+      } else if (!draw.isEmpty() && draw.getLast() > enemy.getLast()) {
+        army.removeFirst();
+        draw.remove();
+      } else if (army.getLast().equals(enemy.getLast())) {
+        draw.add(army.removeLast());
+      } else {
+        army.removeFirst();
+        score--;
+      }
+      enemy.removeLast();
+    }
+    return score > 0 ? "Victory" : score < 0 ? "Defeat" : "Stalemate";
+  }
+}
