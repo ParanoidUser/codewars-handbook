3e64ebdf7060fbe6a6a0617eb2d68c2134466650
Paranoid User <pilgrim.whynot@gmail.com>
2020-10-05 22:44:30 -0700

    feat(6-kyu): https://www.codewars.com/kata/print-number-with-character

---
+++ kata/6-kyu/print-number-with-character/main/Kata.java
@@ -0,0 +1,22 @@
+import static java.util.stream.IntStream.of;
+
+interface Kata {
+  String[][] N = {
+      {" @@@@ ", "  @@  ", " @@@@ ", " @@@@ ", "@@  @@", "@@@@@@", "   @@ ", "@@@@@@", " @@@@ ", " @@@@ "},
+      {"@@  @@", " @@@  ", "@@  @@", "@@  @@", "@@  @@", "@@    ", "  @@  ", "@@  @@", "@@  @@", "@@  @@"},
+      {"@@  @@", "@ @@  ", "   @@ ", "   @@ ", "@@  @@", "@@@@@ ", " @@@@ ", "   @@ ", " @@@@ ", "@@  @@"},
+      {"@@  @@", "  @@  ", "  @@  ", "   @@ ", " @@@@@", "    @@", "@@  @@", "  @@  ", " @@@@ ", " @@@@ "},
+      {"@@  @@", "  @@  ", " @@   ", "@@  @@", "    @@", "    @@", "@@  @@", " @@   ", "@@  @@", "  @@  "},
+      {" @@@@ ", "@@@@@@", "@@@@@@", " @@@@ ", "    @@", "@@@@@ ", " @@@@ ", " @@   ", " @@@@ ", " @@   "}};
+
+  static String printNumber(int num, char ch) {
+    var s = new StringBuilder((ch + "").repeat(40)).append("\n").append(ch).append(" ".repeat(38)).append(ch).append("\n");
+    of(0, 1, 2, 3, 4, 5).forEach(i -> {
+          s.append(ch).append("  ");
+          of(10000, 1000, 100, 10, 1).forEach(j -> s.append(N[i][num / j % 10].replace('@', ch)).append(" "));
+          s.append(" ").append(ch).append("\n");
+        }
+    );
+    return s.append(ch).append(" ".repeat(38)).append(ch).append("\n").append((ch + "").repeat(40)).toString();
+  }
+}
