4547fc2fff5c00321b9983031e37d1a0da2cc45f
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2020-10-17 18:52:15 -0700

    refactor: sonar code smells

--- kata/6-kyu/read-the-time/main/Solution.java
+++ kata/6-kyu/read-the-time/main/Solution.java
@@ -1,11 +1,10 @@
 interface Solution {
-  String[] H = "midnight,one,two,three,four,five,six,seven,eight,nine,ten,eleven,twelve,one,two,three,four,five,six,seven,eight,nine,ten,eleven,midnight".split(",");
-  String[] M = "o'clock,one,two,three,four,five,six,seven,eight,nine,ten,eleven,twelve,thirteen,fourteen,quarter,sixteen,seventeen,eighteen,nineteen,twenty,twenty one,twenty two,twenty three,twenty four,twenty five,twenty six,twenty seven,twenty eight,twenty nine,half".split(",");
-
   static String solve(String time) {
+    String[] hh = "midnight,one,two,three,four,five,six,seven,eight,nine,ten,eleven,twelve,one,two,three,four,five,six,seven,eight,nine,ten,eleven,midnight".split(",");
+    String[] mm = "o'clock,one,two,three,four,five,six,seven,eight,nine,ten,eleven,twelve,thirteen,fourteen,quarter,sixteen,seventeen,eighteen,nineteen,twenty,twenty one,twenty two,twenty three,twenty four,twenty five,twenty six,twenty seven,twenty eight,twenty nine,half".split(",");
     int h = Integer.parseInt(time.substring(0, 2)), m = Integer.parseInt(time.substring(3));
     return m == 0 ?
-        String.format("%s%s", H[h], h > 0 ? " " + M[m] : "") :
-        String.format("%s%s %s %s", M[m > 30 ? 60 - m : m], m % 15 == 0 ? "" : m == 1 || m == 59 ? " minute" : " minutes", m > 30 ? "to" : "past", H[h + m / 31]);
+        String.format("%s%s", hh[h], h > 0 ? " " + mm[m] : "") :
+        String.format("%s%s %s %s", mm[m > 30 ? 60 - m : m], m % 15 == 0 ? "" : m == 1 || m == 59 ? " minute" : " minutes", m > 30 ? "to" : "past", hh[h + m / 31]);
   }
 }
--- kata/6-kyu/real-password-cracker/main/Solution.java
+++ kata/6-kyu/real-password-cracker/main/Solution.java
@@ -4,41 +4,44 @@ import java.math.BigInteger;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.UnaryOperator;
 
 interface Solution extends Iterator<String> {
-
-  UnaryOperator<String> SHA1 = s -> {
-    try {
-      return new BigInteger(1, getInstance("SHA-1").digest(s.getBytes())).toString(16);
-    } catch (Exception fatal) {
-      throw new IllegalStateException(fatal);
-    }
-  };
-
-  Map<String, String> CACHE = new HashMap<>(Map.of("da39a3ee5e6b4b0d3255bfef95601890afd80709", ""));
-  Solution PASGEN = () -> true;
-  int[] STATE = new int[2];
-
   default String next() {
     var guess = new StringBuilder();
-    for (int i = STATE[0]++, l = STATE[1], m; l >= 0; l--, i %= m) {
+    for (int i = Util.CHAR_CODE.getAndIncrement(), l = Util.CHAR_LEVEL.get(), m; l >= 0; l--, i %= m) {
       guess.append((char) (i / (m = (int) Math.pow(26, l)) + 'a'));
     }
 
-    if (STATE[0] > Math.pow(26, STATE[1] + 1.)) {
-      STATE[0] = 0;
-      STATE[1]++;
+    if (Util.CHAR_CODE.get() > Math.pow(26, Util.CHAR_LEVEL.get() + 1.)) {
+      Util.CHAR_CODE.set(0);
+      Util.CHAR_LEVEL.incrementAndGet();
     }
     return guess.toString();
   }
 
   static String passwordCracker(String hash) {
-    String guess = CACHE.get(hash);
-    for (String sha1 = ""; guess == null || !(sha1 = SHA1.apply(guess)).equals(hash);
-        guess = PASGEN.next()) {
-      CACHE.put(sha1, guess);
+    String guess = Util.CACHE.get(hash);
+    for (String sha1 = ""; guess == null || !(sha1 = Util.SHA1.apply(guess)).equals(hash); guess = Util.INSTANCE.next()) {
+      Util.CACHE.put(sha1, guess);
     }
     return guess;
+  }
+
+  class Util {
+    private Util() {}
+
+    private static final Solution INSTANCE = () -> true;
+    private static final AtomicInteger CHAR_CODE = new AtomicInteger();
+    private static final AtomicInteger CHAR_LEVEL = new AtomicInteger();
+    private static final Map<String, String> CACHE = new HashMap<>(Map.of("da39a3ee5e6b4b0d3255bfef95601890afd80709", ""));
+    private static final UnaryOperator<String> SHA1 = s -> {
+      try {
+        return new BigInteger(1, getInstance("SHA-1").digest(s.getBytes())).toString(16);
+      } catch (Exception fatal) {
+        throw new IllegalStateException(fatal);
+      }
+    };
   }
 }
