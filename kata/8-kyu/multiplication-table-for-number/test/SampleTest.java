d793b9e62e243186d72ea4bbe1f08c93f9373823
Paranoid User <pilgrim.whynot@gmail.com>
2020-12-22 23:29:35 -0800

    feat(8-kyu): https://www.codewars.com/kata/multiplication-table-for-number

---
+++ kata/8-kyu/multiplication-table-for-number/test/SampleTest.java
@@ -0,0 +1,11 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class SampleTest {
+  @Test
+  void sample() {
+    assertEquals("1 * 1 = 1\n2 * 1 = 2\n3 * 1 = 3\n4 * 1 = 4\n5 * 1 = 5\n6 * 1 = 6\n7 * 1 = 7\n8 * 1 = 8\n9 * 1 = 9\n10 * 1 = 10", Kata.multiTable(1));
+    assertEquals("1 * 5 = 5\n2 * 5 = 10\n3 * 5 = 15\n4 * 5 = 20\n5 * 5 = 25\n6 * 5 = 30\n7 * 5 = 35\n8 * 5 = 40\n9 * 5 = 45\n10 * 5 = 50", Kata.multiTable(5));
+  }
+}
