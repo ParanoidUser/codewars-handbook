ce8bd2fcef2705ad55c04b9da612e04c04d1ce05
Paranoid User <pilgrim.whynot@gmail.com>
2020-12-23 11:34:35 -0800

    refactor: code-smell 'replace this alternation with a character class'

--- kata/7-kyu/acronym-generator/main/AcronymGenerator.java
+++ kata/7-kyu/acronym-generator/main/AcronymGenerator.java
@@ -1,7 +1,7 @@
 interface AcronymGenerator {
   static String createAcronym(String lastName, String firstName) {
     var acronym = new StringBuilder();
-    for (String name : (firstName + " " + lastName).split("\\s|-")) {
+    for (String name : (firstName + " " + lastName).split("[\\s-]")) {
       acronym.append(name.equalsIgnoreCase("von") ? 'v' : Character.toUpperCase(name.charAt(0)));
     }
     return acronym.toString();
