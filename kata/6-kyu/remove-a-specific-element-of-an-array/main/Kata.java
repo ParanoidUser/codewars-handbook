87f5f68a860e7e9fa75bd2ef802e6976065fc64b
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2020-12-23 23:07:11 -0800

    feat(6-kyu): https://www.codewars.com/kata/remove-a-specific-element-of-an-array

---
+++ kata/6-kyu/remove-a-specific-element-of-an-array/main/Kata.java
@@ -0,0 +1,24 @@
+import static java.util.stream.IntStream.of;
+
+import java.util.HashSet;
+
+interface Kata {
+  static int[][] selectSubarray(int[] arr) {
+    var sum = of(arr).sum();
+    var prod = of(arr).mapToDouble(Math::abs).reduce(1, (a, b) -> a * b);
+
+    double min = prod;
+    var idx = new HashSet<Integer>();
+    for (int i = 0; i < arr.length; i++) {
+      var q = Math.abs(prod / arr[i] / (sum == arr[i] ? 1 : sum - arr[i]));
+      if (q < min) {
+        min = q;
+        idx.clear();
+      }
+      if (q == min) {
+        idx.add(i);
+      }
+    }
+    return idx.stream().map(i -> new int[]{i, arr[i]}).toArray(int[][]::new);
+  }
+}
