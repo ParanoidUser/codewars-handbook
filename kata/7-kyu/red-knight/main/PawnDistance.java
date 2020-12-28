be498e2595d9640374338d2da88f82937f96c59e
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2020-12-27 18:27:07 -0800

    test: replace auto-generated @EqualsAndHashCode with implicit

--- kata/7-kyu/red-knight/main/PawnDistance.java
+++ kata/7-kyu/red-knight/main/PawnDistance.java
@@ -1,9 +1,25 @@
+import java.util.Objects;
 import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
 
 @AllArgsConstructor
-@EqualsAndHashCode
 class PawnDistance {
   final String color;
   final long distance;
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof PawnDistance)) {
+      return false;
+    }
+    PawnDistance pawn = (PawnDistance) o;
+    return pawn.distance == distance && Objects.equals(pawn.color, color);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(color, distance);
+  }
 }
