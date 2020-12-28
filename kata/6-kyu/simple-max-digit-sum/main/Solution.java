de6ea2fe62d36961a32c0d231c146162b6bcc4eb
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2020-12-27 22:12:30 -0800

    feat(6-kyu): https://www.codewars.com/kata/simple-max-digit-sum

---
+++ kata/6-kyu/simple-max-digit-sum/main/Solution.java
@@ -0,0 +1,7 @@
+interface Solution {
+  static long solve(long n) {
+    long power = (long) Math.pow(10, (n + "").length() - 1);
+    long last = power * (n / power + 1) - 1;
+    return last > n + 1 ? last - (long) Math.pow(10, (last - n + "").length()) : Math.min(last, n);
+  }
+}
