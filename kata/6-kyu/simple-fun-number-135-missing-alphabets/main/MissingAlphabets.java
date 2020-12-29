5dbf42b4185e1d9abf33a6a6cbc8b07b7ae6e647
Paranoid User <pilgrim.whynot@gmail.com>
2020-12-28 20:57:34 -0800

    refactor: switch from spring-core to for specific commons-lang3

--- kata/6-kyu/simple-fun-number-135-missing-alphabets/main/MissingAlphabets.java
+++ kata/6-kyu/simple-fun-number-135-missing-alphabets/main/MissingAlphabets.java
@@ -1,10 +1,11 @@
 import static java.util.stream.Collectors.joining;
-import static java.util.stream.IntStream.*;
-import static org.springframework.util.StringUtils.countOccurrencesOf;
+import static java.util.stream.IntStream.of;
+import static java.util.stream.IntStream.rangeClosed;
+import static org.apache.commons.lang3.StringUtils.countMatches;
 
 interface MissingAlphabets {
   static String find(String text) {
-    int[] occur = rangeClosed('a', 'z').map(c -> countOccurrencesOf(text, (char) c + "")).toArray();
+    int[] occur = rangeClosed('a', 'z').map(c -> countMatches(text, (char) c + "")).toArray();
     int max = of(occur).max().orElse(0);
     return rangeClosed('a', 'z').mapToObj(c -> ((char) c + "").repeat(max - occur[c - 97])).collect(joining());
   }
