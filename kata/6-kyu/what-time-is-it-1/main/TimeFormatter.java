95e55b682265ac37c73911382056ff4932567327
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2020-12-30 20:19:07 -0800

    feat(6-kyu): https://www.codewars.com/kata/what-time-is-it-1

---
+++ kata/6-kyu/what-time-is-it-1/main/TimeFormatter.java
@@ -0,0 +1,17 @@
+import static java.util.Arrays.stream;
+
+import java.util.function.IntFunction;
+import java.util.function.IntUnaryOperator;
+
+interface TimeFormatter {
+  static String toHumanTime(final String time) {
+    var clock = new String[]{"o'clock", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "quarter", "sixteen", "seventeen", "eighteen", "nineteen", "twenty", "twenty-one", "twenty-two", "twenty-three", "twenty-four", "twenty-five", "twenty-six", "twenty-seven", "twenty-eight", "twenty-nine", "half"};
+    var units = stream(time.replaceAll("[^\\d:]", "").split(":")).mapToInt(Integer::parseInt).toArray();
+
+    IntUnaryOperator hh = h -> h % 12 > 0 ? h % 12 : 12;
+    IntFunction<String> mm = m -> clock[m] + (m == 15 || m == 30 ? "" : m == 1 ? " minute" : " minutes");
+
+    return units[1] > 30 ? mm.apply(60 - units[1]) + " to " + clock[hh.applyAsInt(units[0] + 1)] :
+           units[1] > 0 ? mm.apply(units[1]) + " past " + clock[hh.applyAsInt(units[0])] : clock[hh.applyAsInt(units[0])] + " " + clock[0];
+  }
+}
