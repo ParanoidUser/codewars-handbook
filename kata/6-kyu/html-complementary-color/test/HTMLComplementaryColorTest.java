8dcf9fc6f5cded9bff57886cf504186a1450f3fd
Paranoid User <pilgrim.whynot@gmail.com>
2021-03-18 15:05:36 -0700

    test: cover missing condition

--- kata/6-kyu/html-complementary-color/test/HTMLComplementaryColorTest.java
+++ kata/6-kyu/html-complementary-color/test/HTMLComplementaryColorTest.java
@@ -9,6 +9,7 @@ class HTMLComplementaryColorTest {
     assertEquals("#FFFFFF", HTMLComplementaryColor.getReversedColor(""));
     assertEquals("#FFFFFF", HTMLComplementaryColor.getReversedColor("0"));
     assertEquals("#FF00FF", HTMLComplementaryColor.getReversedColor("ff00"));
+    assertThrows(IllegalArgumentException.class, () -> HTMLComplementaryColor.getReversedColor(null));
     assertThrows(IllegalArgumentException.class, () -> HTMLComplementaryColor.getReversedColor("1234567"));
     assertThrows(IllegalArgumentException.class, () -> HTMLComplementaryColor.getReversedColor("AA00ZZ"));
     assertThrows(IllegalArgumentException.class, () -> HTMLComplementaryColor.getReversedColor("#AAFFAA"));
