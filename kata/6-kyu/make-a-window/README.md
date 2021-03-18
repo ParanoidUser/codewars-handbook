61eac88077a4e3f723969f542346dbe1ed09953c
Paranoid User <pilgrim.whynot@gmail.com>
2021-03-17 20:10:35 -0700

    feat(6-kyu): https://www.codewars.com/kata/make-a-window

---
+++ kata/6-kyu/make-a-window/README.md
@@ -0,0 +1,27 @@
+# [Make A Window](https://www.codewars.com/kata/make-a-window "https://www.codewars.com/kata/59c03f175fb13337df00002e")
+
+Make me a window.  I'll give you a number (N).  You return a window.
+
+Rules:
+
+The window will always be 2 x 2.
+
+The window needs to have N number of periods across and N number of periods vertically in each pane.
+
+Example:
+
+```
+N = 3
+
+---------
+|...|...|
+|...|...|
+|...|...|
+|---+---|
+|...|...|
+|...|...|
+|...|...|
+---------
+```
+
+Note: there should be no trailing new line characters at the end.
---
+++ kata/6-kyu/make-a-window/main/Solution.java
@@ -0,0 +1,16 @@
+import java.util.LinkedList;
+
+interface Solution {
+  static String makeAWindow(int num) {
+    var row = String.format("|%1$s|%1$s|", ".".repeat(num));
+    var windows = new LinkedList<String>();
+    for (int i = 0; i < num; i++) {
+      windows.add(row);
+      windows.add(row);
+    }
+    windows.add(num, String.format("|%1$s+%1$s|", "-".repeat(num)));
+    windows.addFirst(String.format("-%1$s-%1$s-", "-".repeat(num)));
+    windows.addLast(String.format("-%1$s-%1$s-", "-".repeat(num)));
+    return String.join("\n", windows);
+  }
+}
