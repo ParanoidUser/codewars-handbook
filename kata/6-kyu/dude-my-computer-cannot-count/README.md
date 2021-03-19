899708f52e124e6f4f147c048d4d64800de12d82
Paranoid User <pilgrim.whynot@gmail.com>
2021-03-19 14:16:56 -0700

    feat(6-kyu): https://www.codewars.com/kata/dude-my-computer-cannot-count

---
+++ kata/6-kyu/dude-my-computer-cannot-count/README.md
@@ -0,0 +1,11 @@
+# [Dude, my computer cannot count!](https://www.codewars.com/kata/dude-my-computer-cannot-count "https://www.codewars.com/kata/58fa9898dfec0ef150000014")
+
+Remember the times when every website had a counter? A friend of yours runs one of those sites.
+
+But, as his site gains popularity, he observes some strange behavior:
+Sometimes, when there are many users accessing his page, the counter does not represent what he can get from the access logs.
+
+Knowing your programming skills, he asks you to investigate.
+You look at his code and immediately see what's wrong: his VisitorStatistics implementation is not thread-safe.
+
+"No big deal," you tell him. "I'll fix it in no time!"
---
+++ kata/6-kyu/dude-my-computer-cannot-count/test/VisitorStatisticsTest.java
@@ -0,0 +1,49 @@
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.concurrent.CountDownLatch;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class VisitorStatisticsTest {
+  private VisitorStatistics visitorStatistics;
+
+  @BeforeEach
+  public void setUp() {
+    visitorStatistics = new VisitorStatistics();
+  }
+
+  @Test
+  void shouldIncrementCounterAndTrackUsers() {
+    visitorStatistics.visit("A");
+    visitorStatistics.visit("B");
+    visitorStatistics.visit("A");
+    assertEquals(3, visitorStatistics.totalVisits());
+    assertEquals(2, visitorStatistics.visitsBy("A"));
+    assertEquals(1, visitorStatistics.visitsBy("B"));
+  }
+
+  @Test
+  void shouldHandleConcurrentAccess() throws Exception {
+    CountDownLatch latch = new CountDownLatch(1);
+    Thread[] threads = new Thread[100];
+    String name = "Visitor";
+    for (int i = 0; i < threads.length; i++) {
+      threads[i] = new Thread(() -> {
+        try {
+          latch.await();
+        } catch (InterruptedException e) {
+          throw new RuntimeException(e);
+        }
+        visitorStatistics.visit(name);
+      });
+      threads[i].start();
+    }
+    latch.countDown();
+    for (Thread thread : threads) {
+      thread.join();
+    }
+    assertEquals(threads.length, visitorStatistics.totalVisits());
+    assertEquals(threads.length, visitorStatistics.visitsBy(name));
+  }
+}
