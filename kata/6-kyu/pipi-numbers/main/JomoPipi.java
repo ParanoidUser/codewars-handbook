d4d75bf1459194fcd83446b8c7b7315caae147e7
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2021-03-22 13:28:05 -0700

    style(code smell): mutable fields should not be "public static"
    https://sonarcloud.io/project/issues?branch=develop&id=codewars-handbook&issues=AXhbbQLTVrchjitaUrlJ&open=AXhbbQLTVrchjitaUrlJ

--- kata/6-kyu/pipi-numbers/main/JomoPipi.java
+++ kata/6-kyu/pipi-numbers/main/JomoPipi.java
@@ -3,17 +3,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 interface JomoPipi {
-  List<BigInteger> cache = new ArrayList<>();
-
   static BigInteger pipi(int n) {
-    if (n < cache.size()) {
-      return cache.get(n);
+    if (n < Cache.NUMBERS.size()) {
+      return Cache.NUMBERS.get(n);
     }
     var pn = BigInteger.valueOf(n);
     for (int i = 0; i < n; i++) {
       pn = (pn = pn.subtract(pipi(i))).multiply(pn);
     }
-    cache.add(pn);
+    Cache.NUMBERS.add(pn);
     return pn;
+  }
+
+  class Cache {
+    private Cache() { }
+    private static final List<BigInteger> NUMBERS = new ArrayList<>();
   }
 }
