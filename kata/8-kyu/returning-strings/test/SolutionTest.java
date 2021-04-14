5b2ee1ef624fdc311cba2f67ce676c1f11abb01d
Paranoid User <pilgrim.whynot@gmail.com>
2021-04-14 10:09:21 -0700

    feat(8-kyu): https://www.codewars.com/kata/returning-strings

---
+++ kata/8-kyu/returning-strings/test/SolutionTest.java
@@ -0,0 +1,10 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+  @Test
+  void sample() {
+    assertEquals("Hello, Ryan how are you doing today?", Kata.greet("Ryan"));
+  }
+}
