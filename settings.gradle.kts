1946c9286952e0df60cb149176263b1a9febeaf1
Paranoid User <pilgrim.whynot@gmail.com>
2021-07-27 18:00:07 -0400

    build: bump gradle enterprise plugin to 3.6.3

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,7 +9,7 @@ File(rootDir, "kata").walk().maxDepth(2)
         }
 
 plugins {
-    id("com.gradle.enterprise") version "3.6.2"
+    id("com.gradle.enterprise") version "3.6.3"
 }
 
 gradleEnterprise {
