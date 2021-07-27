59c018293feff108c9ee7e6baf6c0eaca817f957
Paranoid User <pilgrim.whynot@gmail.com>
2021-07-27 19:39:56 -0400

    feat(7-kyu): https://www.codewars.com/kata/harvest-festival

---
+++ kata/7-kyu/harvest-festival/test/SolutionTest.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+  @Test
+  void sample() {
+    assertEquals("-+", Kata.plant('+', 1, 3, 15));
+    assertEquals("---,,,,,,,---,,,,,,,---,,,,,,,", Kata.plant(',', 3, 7, 25));
+    assertEquals("---------;;;;;;;;;;---------;;;;;;;;;;---------;;;;;;;;;;---------;;;;;;;;;;---------;;;;;;;;;;---------;;;;;;;;;;---------;;;;;;;;;;---------;;;;;;;;;;---------;;;;;;;;;;", Kata.plant(';', 9, 10, 30));
+  }
+}
