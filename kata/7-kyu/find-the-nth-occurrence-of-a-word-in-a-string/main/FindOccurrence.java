d6606a741ccf098fcd2140bec05aac31b3a11474
Paranoid User <pilgrim.whynot@gmail.com>
2021-07-27 22:36:46 -0400

    fix: obsolete solution

--- kata/7-kyu/find-the-nth-occurrence-of-a-word-in-a-string/main/FindOccurrence.java
+++ kata/7-kyu/find-the-nth-occurrence-of-a-word-in-a-string/main/FindOccurrence.java
@@ -1,5 +1,10 @@
 interface FindOccurrence {
   static int findNthOccurrence(String subStr, String str, int occurrence) {
-    return occurrence > 0 ? str.indexOf(subStr, findNthOccurrence(subStr, str, --occurrence) + occurrence) : 0;
+    for (int i = 0; i < str.length(); i++) {
+      if (str.startsWith(subStr, i) && occurrence-- == 1) {
+        return i;
+      }
+    }
+    return -1;
   }
 }
