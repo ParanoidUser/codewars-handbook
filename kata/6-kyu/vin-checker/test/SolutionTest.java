d017447d90c289abf6f326829a088413afcbd7df
Paranoid User <pilgrim.whynot@gmail.com>
2021-07-27 23:22:46 -0400

    feat(6-kyu): https://www.codewars.com/kata/vin-checker

---
+++ kata/6-kyu/vin-checker/test/SolutionTest.java
@@ -0,0 +1,15 @@
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+  @Test
+  void sample() {
+    assertTrue(CodeWars.checkVin("5YJ3E1EA7HF000337"));
+    assertTrue(CodeWars.checkVin("5YJ3E1EAXHF000347"));
+    assertTrue(CodeWars.checkVin("5VGYMVUX7JV764512"));
+    assertFalse(CodeWars.checkVin("7WDMMTDV9TG739741"));
+    assertFalse(CodeWars.checkVin("7JTRH08L5EJ234829"));
+  }
+}
