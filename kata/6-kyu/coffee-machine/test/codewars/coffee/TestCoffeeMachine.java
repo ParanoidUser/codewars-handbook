68836059b49efb5e7b33c6ac2b22cd26f51ab136
Paranoid User <pilgrim.whynot@gmail.com>
2022-01-02 15:02:34 -0500

    refactor(S2119): random objects should be reused

--- kata/6-kyu/coffee-machine/test/codewars/coffee/TestCoffeeMachine.java
+++ kata/6-kyu/coffee-machine/test/codewars/coffee/TestCoffeeMachine.java
@@ -1,20 +1,20 @@
 package codewars.coffee;
 
+import static java.util.concurrent.ThreadLocalRandom.current;
+
 import codewars.coffee.PreloadedCoffee.Coffee;
 import codewars.coffee.PreloadedCoffee.CoffeeMachine;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Random;
 import java.util.concurrent.atomic.AtomicInteger;
 
 class TestCoffeeMachine implements CoffeeMachine {
   private static final String[] coffeeTypes = {
       "Espresso", "Percolated", "Turkish coffee", "Caffe Americano", "Cafe Cubano"
   };
-  private static final Random random = new Random();
 
   static Coffee generate() {
-    return new Coffee(coffeeTypes[random.nextInt(coffeeTypes.length)]);
+    return new Coffee(coffeeTypes[current().nextInt(coffeeTypes.length)]);
   }
 
   static final Map<String, Integer> data = new HashMap<>();
--- kata/8-kyu/stringy-strings/test/StringyExampleTests.java
+++ kata/8-kyu/stringy-strings/test/StringyExampleTests.java
@@ -1,14 +1,13 @@
+import static java.util.concurrent.ThreadLocalRandom.current;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.util.Random;
 import org.junit.jupiter.api.Test;
 
 class StringyExampleTests {
   @Test
   void sample() {
-    Random randGen = new Random();
     for (int i = 0; i < 10; i++) {
-      int size = randGen.nextInt(50);
+      int size = current().nextInt(50);
       assertEquals(size, Kata.stringy(size).length());
     }
   }
