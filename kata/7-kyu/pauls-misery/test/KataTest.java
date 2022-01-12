c736c4bc4a81cdf26a6a1b328961b7a4f503f2cd
Paranoid User <pilgrim.whynot@gmail.com>
2022-01-11 19:35:52 -0500

    feat(7-kyu): https://www.codewars.com/kata/pauls-misery

---
+++ kata/7-kyu/pauls-misery/test/KataTest.java
@@ -0,0 +1,13 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class KataTest {
+  @Test
+  void sample() {
+    assertEquals("Super happy!", Kata.paul(new String[]{"life", "eating", "life"}));
+    assertEquals("Super happy!", Kata.paul(new String[]{"life", "Petes kata", "Petes kata", "Petes kata", "eating"}));
+    assertEquals("Happy!", Kata.paul(new String[]{"Petes kata", "Petes kata", "eating", "Petes kata", "Petes kata", "eating"}));
+    assertEquals("Sad!", Kata.paul(new String[]{"Petes kata", "Petes kata", "Petes kata", "Petes kata", "Petes kata", "Petes kata", "Petes kata", "Petes kata", "Petes kata", "kata", "eating", "eating", "eating", "eating"}));
+  }
+}
