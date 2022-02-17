ab9a1dfd7c854a89c2cc4e20c790de8f4ac3b40b
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2022-02-16 21:03:27 -0500

    fix: pacific/fiji time zone resolution

--- kata/beta/my-friend-time/test/TimeWhereverIgoTest.java
+++ kata/beta/my-friend-time/test/TimeWhereverIgoTest.java
@@ -10,6 +10,6 @@ class TimeWhereverIgoTest {
     assertEquals("28-03-2021 10:41", TimeWhereverIgo.getFriendDateAndTime("28-03-2021 19:41", "Australia/Sydney", "Europe/Warsaw"));
     assertEquals("19-03-2021 12:01", TimeWhereverIgo.getFriendDateAndTime("19-03-2021 01:01", "America/Halifax", "Asia/Makassar"));
     assertEquals("01-12-2021 00:00", TimeWhereverIgo.getFriendDateAndTime("01-12-2021 00:00", "Europe/Dublin", "Europe/Belfast"));
-    assertEquals("07-12-2021 11:59", TimeWhereverIgo.getFriendDateAndTime("07-12-2021 23:59", "Pacific/Fiji", "Europe/Berlin"));
+    assertEquals("07-12-2021 11:59", TimeWhereverIgo.getFriendDateAndTime("07-12-2021 23:59", "UTC+13", "Europe/Berlin"));
   }
 }
