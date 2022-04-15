4c7a8d02642364cde401e91c6f20b63bcaa2dcb7
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2022-04-14 21:51:53 -0400

    test: assign fixed time zone

--- kata/6-kyu/count-the-days/test/DayTest.java
+++ kata/6-kyu/count-the-days/test/DayTest.java
@@ -1,19 +1,23 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.time.ZoneId;
+import java.time.ZoneOffset;
 import java.util.Calendar;
+import java.util.TimeZone;
 import org.junit.jupiter.api.Test;
 
 class DayTest {
   @Test
   void sample() {
-    var today = Calendar.getInstance();
+    var utc = TimeZone.getTimeZone("UTC");
+    var today = Calendar.getInstance(utc);
     assertEquals("Today is the day!", new Day().countDays(today.getTime()));
 
-    var yesterday = Calendar.getInstance();
+    var yesterday = Calendar.getInstance(utc);
     yesterday.add(Calendar.DATE, -1);
     assertEquals("The day is in the past!", new Day().countDays(yesterday.getTime()));
 
-    var tomorrow = Calendar.getInstance();
+    var tomorrow = Calendar.getInstance(utc);
     tomorrow.add(Calendar.DATE, 2);
     assertEquals("2 days", new Day().countDays(tomorrow.getTime()));
   }
