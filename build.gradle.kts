22ea9f64a496568961d9b22d3c045c7f3e0549a0
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2022-04-20 01:47:15 +0000

    build(deps): bump mockito-inline from 4.4.0 to 4.5.0
    Bumps [mockito-inline](https://github.com/mockito/mockito) from 4.4.0 to 4.5.0.
    - [Release notes](https://github.com/mockito/mockito/releases)
    - [Commits](https://github.com/mockito/mockito/compare/v4.4.0...v4.5.0)
    
    ---
    updated-dependencies:
    - dependency-name: org.mockito:mockito-inline
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,7 +41,7 @@ subprojects {
         implementation("org.springframework.boot:spring-boot-starter-web:2.6.6")
         compileOnly("org.projectlombok:lombok:1.18.24")
         annotationProcessor("org.projectlombok:lombok:1.18.24")
-        testImplementation("org.mockito:mockito-inline:4.4.0")
+        testImplementation("org.mockito:mockito-inline:4.5.0")
         testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
         testImplementation("org.springframework.boot:spring-boot-starter-test:2.6.6")
     }
