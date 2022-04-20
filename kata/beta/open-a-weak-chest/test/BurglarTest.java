26b8bb714738a6427ff042f3aa1ca8e20942312b
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2022-04-19 23:55:18 -0400

    refactor: code coverage

--- kata/beta/open-a-weak-chest/test/BurglarTest.java
+++ kata/beta/open-a-weak-chest/test/BurglarTest.java
@@ -1,3 +1,4 @@
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
@@ -5,7 +6,7 @@ import org.junit.jupiter.api.Test;
 class BurglarTest {
   @Test
   void sample() {
-    WeakChest chest = new WeakChest(42);
-    assertTrue(new Burglar().lockpick(chest).isOpen());
+    assertTrue(new Burglar().lockpick(new WeakChest(42)).isOpen());
+    assertFalse(new Burglar().lockpick(new WeakChest(-42)).isOpen());
   }
 }
