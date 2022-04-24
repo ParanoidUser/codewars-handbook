1d6eb843ce04aa7756f1d741485fe25f2b6fa3ae
Paranoid User <pilgrim.whynot@gmail.com>
2022-04-24 11:46:36 -0400

    refactor(java:S114): code smell - interface names should comply with a naming convention

--- kata/7-kyu/santas-naughty-list/test/TestFixure.java
+++ kata/7-kyu/santas-naughty-list/test/TestFixure.java
@@ -6,8 +6,8 @@ import org.junit.jupiter.api.Test;
 class TestFixure {
   @Test
   void sample() {
-    assertEquals(List.of("Jason", "Jordan"), findList.findChildren(List.of("Jason", "Jackson", "Jordan", "Johnny"), List.of("Jason", "Jordan", "Jennifer")));
-    assertEquals(List.of("JJ", "Jason"), findList.findChildren(List.of("Jason", "Jackson", "Johnson", "JJ"), List.of("Jason", "James", "JJ")));
-    assertEquals(List.of("JAsoN", "jASon"), findList.findChildren(List.of("jASon", "JAsoN", "JaSON", "jasON"), List.of("JasoN", "jASOn", "JAsoN", "jASon", "JASON")));
+    assertEquals(List.of("Jason", "Jordan"), FindList.findChildren(List.of("Jason", "Jackson", "Jordan", "Johnny"), List.of("Jason", "Jordan", "Jennifer")));
+    assertEquals(List.of("JJ", "Jason"), FindList.findChildren(List.of("Jason", "Jackson", "Johnson", "JJ"), List.of("Jason", "James", "JJ")));
+    assertEquals(List.of("JAsoN", "jASon"), FindList.findChildren(List.of("jASon", "JAsoN", "JaSON", "jasON"), List.of("JasoN", "jASOn", "JAsoN", "jASon", "JASON")));
   }
 }
