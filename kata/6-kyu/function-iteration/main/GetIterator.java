92633d9b4390b42cb06838486c84e741b8087b98
Paranoid User <pilgrim.whynot@gmail.com>
2022-04-24 11:24:08 -0400

    refactor(java:S4276): code smell - functional interfaces should be as specialised as possible

--- kata/6-kyu/function-iteration/main/GetIterator.java
+++ kata/6-kyu/function-iteration/main/GetIterator.java
@@ -1,7 +1,8 @@
 import java.util.function.Function;
+import java.util.function.UnaryOperator;
 
 interface GetIterator {
-  static Function<Integer, Integer> getIterator(Function<Integer, Integer> func, int times) {
+  static Function<Integer, Integer> getIterator(UnaryOperator<Integer> func, int times) {
     return times > 1 ? func.andThen(getIterator(func, --times)) : func;
   }
 }
