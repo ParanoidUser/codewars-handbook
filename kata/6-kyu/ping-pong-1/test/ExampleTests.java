bc2bed8c9fe02930654c20a379f9267229102a09
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2022-04-25 12:03:50 -0400

    test: cover score combinations

--- kata/6-kyu/ping-pong-1/test/ExampleTests.java
+++ kata/6-kyu/ping-pong-1/test/ExampleTests.java
@@ -7,6 +7,7 @@ class ExampleTests {
   void sample() {
     assertEquals("ping", Dinglemouse.pingPong("ping-pong-aaaa-ping-pong-ping-aaaa"));
     assertEquals("ping", Dinglemouse.pingPong("pong-ping-bink-ping-pong-donk"));
+    assertEquals("pong", Dinglemouse.pingPong("ping-pong-bink-pong-ping-donk"));
     assertEquals("ping", Dinglemouse.pingPong("ping-pong-ping-pong-bonk-bing-doof"));
     assertEquals("ping", Dinglemouse.pingPong("pong-ding-pong-ping-donk-ping-pong-thud"));
     assertEquals("pong", Dinglemouse.pingPong("pong-ping-dong-ping-pong-tink-bonk-pong-ping-doof"));
