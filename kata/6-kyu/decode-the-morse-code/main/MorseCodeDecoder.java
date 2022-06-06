c9b02a03c2f156ba6a668324c04d1d1567157379
Paranoid User <pilgrim.whynot@gmail.com>
2022-06-05 21:09:17 -0400

    refactor(java:S6326): code smell
    Regular expressions should not contain multiple spaces

--- kata/6-kyu/decode-the-morse-code/main/MorseCodeDecoder.java
+++ kata/6-kyu/decode-the-morse-code/main/MorseCodeDecoder.java
@@ -3,7 +3,7 @@ import static java.util.stream.Collectors.joining;
 
 interface MorseCodeDecoder {
   static String decode(String morseCode) {
-    return stream(morseCode.trim().split("   "))
+    return stream(morseCode.trim().split(" {3}"))
         .map(w -> stream(w.split(" ")).map(MorseCode::get).collect(joining()))
         .collect(joining(" "));
   }
