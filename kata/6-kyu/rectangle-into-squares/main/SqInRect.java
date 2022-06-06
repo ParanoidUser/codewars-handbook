55fe25ca9a355ea4738fbc3e40025c4b17795bdc
Paranoid User <pilgrim.whynot@gmail.com>
2022-06-06 10:19:22 -0400

    refactor(java:S1168): code smell - empty arrays and collections should be returned instead of null

--- kata/6-kyu/rectangle-into-squares/main/SqInRect.java
+++ kata/6-kyu/rectangle-into-squares/main/SqInRect.java
@@ -3,8 +3,8 @@ import java.util.List;
 
 interface SqInRect {
   static List<Integer> sqInRect(int lng, int wdth) {
-    if (lng == wdth) return null;
     var result = new ArrayList<Integer>();
+    if (lng == wdth) return result;
     while (lng * wdth > 0) {
       result.add(lng > wdth ? lng - (lng -= wdth) : wdth - (wdth -= lng));
     }
