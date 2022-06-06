77a7a556f8862d5327b8e9a0a42737530271bd32
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2022-06-06 10:20:52 -0400

    test: fix outdated null assertion

--- kata/6-kyu/rectangle-into-squares/test/SqInRectTest.java
+++ kata/6-kyu/rectangle-into-squares/test/SqInRectTest.java
@@ -1,5 +1,4 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
 
 import java.util.List;
 import org.junit.jupiter.api.Test;
@@ -8,6 +7,6 @@ class SqInRectTest {
   @Test
   void sample() {
     assertEquals(List.of(3, 2, 1, 1), SqInRect.sqInRect(5, 3));
-    assertNull(SqInRect.sqInRect(5, 5));
+    assertEquals(List.of(), SqInRect.sqInRect(5, 5));
   }
 }
