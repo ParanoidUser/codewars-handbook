6ea3165284124286e75785e2b79671635991cd63
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2022-06-05 21:13:12 -0400

    refactor(java:S6397): code smell
    Character classes in regular expressions should not contain only one character

--- kata/7-kyu/simple-string-characters/main/Kata.java
+++ kata/7-kyu/simple-string-characters/main/Kata.java
@@ -4,7 +4,7 @@ interface Kata {
         word.replaceAll("[^A-Z]", "").length(),
         word.replaceAll("[^a-z]", "").length(),
         word.replaceAll("[^0-9]", "").length(),
-        word.replaceAll("[\\w]", "").length()
+        word.replaceAll("\\w", "").length()
     };
   }
 }
--- kata/7-kyu/wheres-wally/main/WallyFinder.java
+++ kata/7-kyu/wheres-wally/main/WallyFinder.java
@@ -2,7 +2,7 @@ import static java.util.regex.Pattern.compile;
 
 interface WallyFinder {
   static int wheresWally(String str) {
-    var matcher = compile("(?<=^|[ ])Wally(?!\\w)").matcher(str);
+    var matcher = compile("(?<=^| )Wally(?!\\w)").matcher(str);
     return matcher.find() ? matcher.start() : -1;
   }
 }
