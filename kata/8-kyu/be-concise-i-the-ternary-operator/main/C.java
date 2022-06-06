4c4725a59ebf0a1518d3ae499e62dd6f08f56cf9
Paranoid User <pilgrim.whynot@gmail.com>
2022-06-06 11:09:14 -0400

    refactor(java:S1118): code smell - utility classes should not have public constructors

--- kata/8-kyu/be-concise-i-the-ternary-operator/main/C.java
+++ kata/8-kyu/be-concise-i-the-ternary-operator/main/C.java
@@ -1,1 +1,1 @@
-class C{static String describeAge(int a){return"You're a(n) "+(a<13?"kid":a<18?"teenager":a<65?"adult":"elderly");}}
+interface C{static String describeAge(int a){return"You're a(n) "+(a<13?"kid":a<18?"teenager":a<65?"adult":"elderly");}}
