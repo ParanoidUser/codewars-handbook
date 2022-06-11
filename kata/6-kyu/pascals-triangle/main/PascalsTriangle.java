02e76c7b9785168f11220f4c655e576989e957b1
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2022-06-11 18:55:52 -0400

    refactor(java:S1121): code smell - assignments should not be made from within sub-expressions
    Partially remediated across kyu 6 problems.

--- kata/6-kyu/pascals-triangle/main/PascalsTriangle.java
+++ kata/6-kyu/pascals-triangle/main/PascalsTriangle.java
@@ -1,7 +1,8 @@
 interface PascalsTriangle {
   static long[] generate(int level) {
     long[] pascal = new long[level * (level + 1) / 2];
-    for (int i = 0, l = 0; l < level; l++) {
+    int i = 0;
+    for (int l = 0; l < level; l++) {
       for (long f = 1, k = 0; k <= l; f = f * (l - k) / (k + 1), k++) {
         pascal[i++] = f;
       }
--- kata/6-kyu/password-maker-1/main/Dinglemouse.java
+++ kata/6-kyu/password-maker-1/main/Dinglemouse.java
@@ -1,7 +1,10 @@
 interface Dinglemouse {
   static String makePassword(int len, boolean flagUpper, boolean flagLower, boolean flagDigit) {
     var password = new StringBuilder();
-    for (int u = 0, l = 0, d = 0; password.length() < len;) {
+    int u = 0;
+    int l = 0;
+    int d = 0;
+    while (password.length() < len) {
       if (flagUpper) password.append("ABCDEFGHIJKLMNOPQRSTUVWXYZ".charAt(u++));
       if (flagLower) password.append("abcdefghijklmnopqrstuvwxyz".charAt(l++));
       if (flagDigit && d < 10) password.append("0123456789".charAt(d++));
