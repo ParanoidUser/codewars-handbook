2595f16c4d22e76fcc52e69dbacd309a5d15ffc4
Paranoid User <pilgrim.whynot@gmail.com>
2022-06-26 22:15:27 -0400

    refactor: replace ternary operators by switch expressions

--- kata/8-kyu/basic-mathematical-operations/main/BasicOperations.java
+++ kata/8-kyu/basic-mathematical-operations/main/BasicOperations.java
@@ -1,5 +1,10 @@
 interface BasicOperations {
   static Integer basicMath(String op, int v1, int v2) {
-    return op.equals("*") ? v1 * v2 : op.equals("/") ? v1 / v2 : v1 + (op.equals("+") ? v2 : -v2);
+    return switch (op) {
+      case "*" -> v1 * v2;
+      case "/" -> v1 / v2;
+      case "+" -> v1 + v2;
+      default -> v1 - v2;
+    };
   }
 }
