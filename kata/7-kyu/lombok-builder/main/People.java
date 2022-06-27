d510b73c189753e412b59a1e4e3b55000768812f
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2022-06-26 22:11:38 -0400

    refactor: drop unused generated methods

--- kata/7-kyu/lombok-builder/main/People.java
+++ kata/7-kyu/lombok-builder/main/People.java
@@ -1,15 +1,15 @@
 import lombok.Builder;
-import lombok.Value;
+import lombok.Getter;
 
 @Builder
-@Value
+@Getter
 class People {
   private static final String GREET = "hello my name is ";
-  String name;
-  String lastName;
-  String city;
-  String job;
-  int age;
+  private final String name;
+  private final String lastName;
+  private final String city;
+  private final String job;
+  private final int age;
 
   String greet() {
     return GREET + name;
