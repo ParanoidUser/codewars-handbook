9bbc0ab863e2ae964058ff78a9a2fe22fa579e83
Paranoid User <pilgrim.whynot@gmail.com>
2022-08-06 17:38:24 -0400

    refactor: replace non-digit regex

--- kata/7-kyu/batman-quotes/main/BatmanQuotes.java
+++ kata/7-kyu/batman-quotes/main/BatmanQuotes.java
@@ -1,5 +1,5 @@
 interface BatmanQuotes {
   static String getQuote(String[] quotes, String hero) {
-    return (hero.startsWith("B") ? "Batman" : hero.startsWith("R") ? "Robin" : "Joker") + ": " + quotes[hero.replaceAll("[^\\d]", "").charAt(0) - 48];
+    return (hero.startsWith("B") ? "Batman" : hero.startsWith("R") ? "Robin" : "Joker") + ": " + quotes[hero.replaceAll("\\D", "").charAt(0) - 48];
   }
 }
