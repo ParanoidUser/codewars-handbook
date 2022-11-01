76965e59b82245c5ec7f5674aaecdf5fdb7ed04d
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2022-11-01 01:19:54 -0400

    test: fix unexpected csv comma delimiter

--- kata/8-kyu/return-the-day/test/SolutionTest.java
+++ kata/8-kyu/return-the-day/test/SolutionTest.java
@@ -5,17 +5,17 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 class SolutionTest {
   @ParameterizedTest
-  @CsvSource(textBlock = """
-       0, Wrong, please enter a number between 1 and 7
-       1, Sunday
-       2, Monday
-       3, Tuesday
-       4, Wednesday
-       5, Thursday
-       6, Friday
-       7, Saturday
-       8, Wrong, please enter a number between 1 and 7
-      99, Wrong, please enter a number between 1 and 7
+  @CsvSource(delimiter = '|', textBlock = """
+       0 | Wrong, please enter a number between 1 and 7
+       1 | Sunday
+       2 | Monday
+       3 | Tuesday
+       4 | Wednesday
+       5 | Thursday
+       6 | Friday
+       7 | Saturday
+       8 | Wrong, please enter a number between 1 and 7
+      99 | Wrong, please enter a number between 1 and 7
        """)
   void sample(int n, String expected) {
     assertEquals(expected, DayOfWeek.getDay(n));
