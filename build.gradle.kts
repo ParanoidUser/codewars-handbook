a2ad68f2b43a79f09782b22af79cd2ff63bab7a4
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2022-11-25 11:05:42 +0000

    build(deps): bump spring-boot-starter-web from 2.7.5 to 3.0.0
    Bumps [spring-boot-starter-web](https://github.com/spring-projects/spring-boot) from 2.7.5 to 3.0.0.
    - [Release notes](https://github.com/spring-projects/spring-boot/releases)
    - [Commits](https://github.com/spring-projects/spring-boot/compare/v2.7.5...v3.0.0)
    
    ---
    updated-dependencies:
    - dependency-name: org.springframework.boot:spring-boot-starter-web
      dependency-type: direct:production
      update-type: version-update:semver-major
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,7 +28,7 @@ subprojects {
 
     dependencies {
         implementation("org.apache.commons:commons-lang3:3.12.0")
-        implementation("org.springframework.boot:spring-boot-starter-web:2.7.5")
+        implementation("org.springframework.boot:spring-boot-starter-web:3.0.0")
         compileOnly("org.projectlombok:lombok:1.18.24")
         annotationProcessor("org.projectlombok:lombok:1.18.24")
         testImplementation("org.mockito:mockito-inline:4.9.0")
