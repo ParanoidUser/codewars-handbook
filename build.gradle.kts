7814c32c61abddd498211ee8144c991d4e3a6d28
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2022-12-23 11:06:44 +0000

    build(deps): bump spring-boot-starter-test from 3.0.0 to 3.0.1
    Bumps [spring-boot-starter-test](https://github.com/spring-projects/spring-boot) from 3.0.0 to 3.0.1.
    - [Release notes](https://github.com/spring-projects/spring-boot/releases)
    - [Commits](https://github.com/spring-projects/spring-boot/compare/v3.0.0...v3.0.1)
    
    ---
    updated-dependencies:
    - dependency-name: org.springframework.boot:spring-boot-starter-test
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,7 +34,7 @@ subprojects {
         testImplementation("org.mockito:mockito-inline:4.10.0")
         testImplementation("org.junit.jupiter:junit-jupiter:5.9.1")
         testImplementation("org.awaitility:awaitility:4.2.0")
-        testImplementation("org.springframework.boot:spring-boot-starter-test:3.0.0")
+        testImplementation("org.springframework.boot:spring-boot-starter-test:3.0.1")
     }
 
     tasks {
