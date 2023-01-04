43f31cb1ca41356c712f607ecb9233aca4eef732
Paranoid User <5120290+ParanoidUser@users.noreply.github.com>
2023-01-03 21:32:17 -0500

    refactor: replace iterator with supplier

--- kata/6-kyu/real-password-cracker/main/Solution.java
+++ kata/6-kyu/real-password-cracker/main/Solution.java
@@ -2,45 +2,25 @@ import static java.security.MessageDigest.getInstance;
 
 import java.math.BigInteger;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
-import java.util.NoSuchElementException;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Supplier;
 import java.util.function.UnaryOperator;
 
-interface Solution extends Iterator<String> {
-  default String next() {
-    if (!hasNext()) {
-      throw new NoSuchElementException();
-    }
-    var guess = new StringBuilder();
-    int m;
-    for (int i = Util.CHAR_CODE.getAndIncrement(), l = Util.CHAR_LEVEL.get(); l >= 0; l--, i %= m) {
-      m = (int) Math.pow(26, l);
-      guess.append((char) (i / m + 'a'));
-    }
-
-    if (Util.CHAR_CODE.get() > Math.pow(26, Util.CHAR_LEVEL.get() + 1.)) {
-      Util.CHAR_CODE.set(0);
-      Util.CHAR_LEVEL.incrementAndGet();
-    }
-    return guess.toString();
-  }
-
+interface Solution {
   static String passwordCracker(String hash) {
     var guess = Util.CACHE.get(hash);
     var sha1 = "";
     while (guess == null || !(sha1 = Util.SHA1.apply(guess)).equals(hash)) {
       Util.CACHE.put(sha1, guess);
-      guess = Util.INSTANCE.next();
+      guess = Util.GUESS.get();
     }
     return guess;
   }
 
-  class Util {
+  final class Util {
     private Util() {}
 
-    private static final Solution INSTANCE = () -> true;
     private static final AtomicInteger CHAR_CODE = new AtomicInteger();
     private static final AtomicInteger CHAR_LEVEL = new AtomicInteger();
     private static final Map<String, String> CACHE = new HashMap<>(Map.of("da39a3ee5e6b4b0d3255bfef95601890afd80709", ""));
@@ -50,6 +30,21 @@ interface Solution extends Iterator<String> {
       } catch (Exception fatal) {
         throw new IllegalStateException(fatal);
       }
+    };
+
+    private static final Supplier<String> GUESS = () -> {
+      var guess = new StringBuilder();
+      int m;
+      for (int i = Util.CHAR_CODE.getAndIncrement(), l = Util.CHAR_LEVEL.get(); l >= 0; l--, i %= m) {
+        m = (int) Math.pow(26, l);
+        guess.append((char) (i / m + 'a'));
+      }
+
+      if (Util.CHAR_CODE.get() > Math.pow(26, Util.CHAR_LEVEL.get() + 1.)) {
+        Util.CHAR_CODE.set(0);
+        Util.CHAR_LEVEL.incrementAndGet();
+      }
+      return guess.toString();
     };
   }
 }
