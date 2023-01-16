c7ee3c0994848af3fc38fe13372a86e4e63cd7af
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2023-01-16 17:59:56 -0500

    refactor: replace switch statement with enhanced switch

--- kata/6-kyu/reverse-polish-notation-calculator/main/Calc.java
+++ kata/6-kyu/reverse-polish-notation-calculator/main/Calc.java
@@ -5,11 +5,11 @@ class Calc {
     var stack = new LinkedList<Double>();
     for (var token : expr.split(" ")) {
       switch (token) {
-        case "+": stack.push(stack.pop() + stack.pop()); break;
-        case "-": stack.push(-stack.pop() + stack.pop()); break;
-        case "*": stack.push(stack.pop() * stack.pop()); break;
-        case "/": stack.push(1 / stack.pop() * stack.pop()); break;
-        default: stack.push(token.isEmpty() ? 0 : Double.parseDouble(token));
+        case "+" -> stack.push(stack.pop() + stack.pop());
+        case "-" -> stack.push(-stack.pop() + stack.pop());
+        case "*" -> stack.push(stack.pop() * stack.pop());
+        case "/" -> stack.push(1 / stack.pop() * stack.pop());
+        default -> stack.push(token.isEmpty() ? 0 : Double.parseDouble(token));
       }
     }
     return stack.pop();
