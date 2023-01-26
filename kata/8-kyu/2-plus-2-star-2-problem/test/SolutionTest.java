d0a70107ae4d37f0014b060a5800b8d6b2c86783
Paranoid User <5120290+ParanoidUser@users.noreply.github.com>
2023-01-26 10:12:29 -0500

    feat(8-kyu): https://www.codewars.com/kata/2-plus-2-star-2-problem
    Closes #217

---
+++ kata/8-kyu/2-plus-2-star-2-problem/test/SolutionTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SolutionTest {
+  @Test
+  void sample() {
+    assertEquals(8, TwoPlusTwoTimesTwoProblem.calculate());
+  }
+}
