f444568ff14d7bd56065ec65f538eb2324063fd0
Paranoid User <5120290+ParanoidUser@users.noreply.github.com>
2023-02-06 17:21:13 -0500

    refactor: cover missing cases

--- kata/beta/only-unchecked-exceptions/test/com/codewars/javatlacati/exceptions/ExceptionsPuzzleTest.java
+++ kata/beta/only-unchecked-exceptions/test/com/codewars/javatlacati/exceptions/ExceptionsPuzzleTest.java
@@ -1,13 +1,24 @@
 package com.codewars.javatlacati.exceptions;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mockStatic;
 
+import java.io.IOException;
 import java.sql.SQLException;
 import org.junit.jupiter.api.Test;
 
 class ExceptionsPuzzleTest {
+
   @Test
   void sample() {
     assertThrows(SQLException.class, ExceptionsPuzzle::callMe);
+    assertThrows(IOException.class, () -> ExceptionsPuzzle.doThrow(new IOException()));
+
+    try (var mock = mockStatic(ExceptionsPuzzle.class)) {
+      mock.when(() -> ExceptionsPuzzle.doThrow(any(SQLException.class))).thenAnswer((nothing -> null));
+      assertDoesNotThrow(ExceptionsPuzzle::callMe);
+    }
   }
 }
