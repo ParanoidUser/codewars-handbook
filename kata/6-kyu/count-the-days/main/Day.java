86b3ea8095f2791d4ebbafff749b88906dfc1344
Paranoid User <5120290+ParanoidUser@users.noreply.github.com>
2023-02-06 15:57:40 -0500

    fix: reduce time accuracy to ~100ms

--- kata/6-kyu/count-the-days/main/Day.java
+++ kata/6-kyu/count-the-days/main/Day.java
@@ -2,7 +2,7 @@ import java.util.Date;
 
 class Day {
   String countDays(Date d) {
-    long days = (d.getTime() - System.currentTimeMillis()) / 86400000;
+    long days = (d.getTime() - System.currentTimeMillis()) / 86399900;
     return days < 0 ? "The day is in the past!" : days > 0 ? days + " days" : "Today is the day!";
   }
 }
