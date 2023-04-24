9a55a7e98b43b72605b4ccd3f8fa7a0e1a275dea
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2023-04-24 10:42:23 -0400

    feat(8-kyu): kata/did-she-say-hallo

---
+++ kata/8-kyu/did-she-say-hallo/test/SolutionTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+  @Test
+  void sample() {
+    assertTrue(Kata.validateHello("hello"));
+    assertTrue(Kata.validateHello("ciao bella!"));
+    assertTrue(Kata.validateHello("salut"));
+    assertTrue(Kata.validateHello("hallo, salut"));
+    assertTrue(Kata.validateHello("hombre! Hola!"));
+    assertTrue(Kata.validateHello("Hallo, wie geht\'s dir?"));
+    assertTrue(Kata.validateHello("AHOJ!"));
+    assertTrue(Kata.validateHello("czesc"));
+    assertTrue(Kata.validateHello("Ahoj"));
+    assertFalse(Kata.validateHello("meh"));
+  }
+}
