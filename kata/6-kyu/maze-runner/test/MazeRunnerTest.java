80c94df25b9ec0c6dc1436360990c171425097e3
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2023-05-09 14:37:16 -0400

    feat(6-kyu): kata/maze-runner (#332)

---
+++ kata/6-kyu/maze-runner/test/MazeRunnerTest.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MazeRunnerTest {
+  private final int[][] maze = {
+          {1, 1, 1, 1, 1, 1, 1},
+          {1, 0, 0, 0, 0, 0, 3},
+          {1, 0, 1, 0, 1, 0, 1},
+          {0, 0, 1, 0, 0, 0, 1},
+          {1, 0, 1, 0, 1, 0, 1},
+          {1, 0, 0, 0, 0, 0, 1},
+          {1, 2, 1, 0, 1, 0, 1}};
+
+  @Test
+  void sample() {
+    assertEquals("Finish", MazeRunner.walk(maze, new String[]{"N", "N", "N", "N", "N", "E", "E", "E", "E", "E"}));
+    assertEquals("Finish", MazeRunner.walk(maze, new String[]{"N", "N", "N", "N", "N", "E", "E", "S", "S", "E", "E", "N", "N", "E"}));
+    assertEquals("Finish", MazeRunner.walk(maze, new String[]{"N", "N", "N", "N", "N", "E", "E", "E", "E", "E", "W", "W"}));
+    assertEquals("Dead", MazeRunner.walk(maze, new String[]{"N", "N", "N", "W", "W"}));
+    assertEquals("Dead", MazeRunner.walk(maze, new String[]{"N", "N", "N", "N", "N", "E", "E", "S", "S", "S", "S", "S", "S"}));
+    assertEquals("Lost", MazeRunner.walk(maze, new String[]{"N", "E", "E", "E", "E"}));
+  }
+}
