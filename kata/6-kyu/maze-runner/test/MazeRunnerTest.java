c073e24d4a011bcb5674683c930d99f4d578ed41
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2023-05-12 12:39:06 -0400

    test: verify maze out of bound cases (#340)

--- kata/6-kyu/maze-runner/test/MazeRunnerTest.java
+++ kata/6-kyu/maze-runner/test/MazeRunnerTest.java
@@ -4,12 +4,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MazeRunnerTest {
   private final int[][] maze = {
-          {1, 1, 1, 1, 1, 1, 1},
+          {1, 0, 1, 1, 1, 1, 1},
           {1, 0, 0, 0, 0, 0, 3},
           {1, 0, 1, 0, 1, 0, 1},
           {0, 0, 1, 0, 0, 0, 1},
           {1, 0, 1, 0, 1, 0, 1},
-          {1, 0, 0, 0, 0, 0, 1},
+          {1, 0, 0, 0, 0, 0, 0},
           {1, 2, 1, 0, 1, 0, 1}};
 
   @Test
@@ -19,6 +19,8 @@ class MazeRunnerTest {
     assertEquals("Finish", MazeRunner.walk(maze, new String[]{"N", "N", "N", "N", "N", "E", "E", "E", "E", "E", "W", "W"}));
     assertEquals("Dead", MazeRunner.walk(maze, new String[]{"N", "N", "N", "W", "W"}));
     assertEquals("Dead", MazeRunner.walk(maze, new String[]{"N", "N", "N", "N", "N", "E", "E", "S", "S", "S", "S", "S", "S"}));
+    assertEquals("Dead", MazeRunner.walk(maze, new String[]{"N", "N", "N", "N", "N", "N", "N"}));
+    assertEquals("Dead", MazeRunner.walk(maze, new String[]{"N", "E", "E", "E", "E", "E", "E"}));
     assertEquals("Lost", MazeRunner.walk(maze, new String[]{"N", "E", "E", "E", "E"}));
   }
 }
