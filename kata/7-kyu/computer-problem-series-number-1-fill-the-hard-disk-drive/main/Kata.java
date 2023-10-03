7f0cd4d7e988539ce92eadc3e57c3997ab80fd15
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2023-10-03 15:07:56 -0400

    fix: missing equal condition case

--- kata/7-kyu/computer-problem-series-number-1-fill-the-hard-disk-drive/main/Kata.java
+++ kata/7-kyu/computer-problem-series-number-1-fill-the-hard-disk-drive/main/Kata.java
@@ -2,6 +2,6 @@ import static java.util.stream.IntStream.range;
 
 interface Kata {
   static long save(int[] sizes, int hd) {
-    return range(0, sizes.length).takeWhile(i -> (sizes[0] += i > 0 ? sizes[i] : 0) < hd).count();
+    return range(0, sizes.length).takeWhile(i -> (sizes[0] += i > 0 ? sizes[i] : 0) <= hd).count();
   }
 }
