3bd986424a14a06b10df3620f4f92897ef2395f0
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2023-10-24 11:33:14 -0400

    test: cover already sorted array case

--- kata/7-kyu/bubblesort-once/test/SolutionTest.java
+++ kata/7-kyu/bubblesort-once/test/SolutionTest.java
@@ -6,5 +6,6 @@ class SolutionTest {
   @Test
   void sample() {
     assertArrayEquals(new int[]{7, 5, 3, 1, 2, 4, 6, 8, 9}, BubblesortOnce.bubbleSortOnce(new int[]{9, 7, 5, 3, 1, 2, 4, 6, 8}));
+    assertArrayEquals(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9}, BubblesortOnce.bubbleSortOnce(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9}));
   }
 }
