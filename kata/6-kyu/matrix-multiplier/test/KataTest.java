a20ecf9d2c02cebbba251f0b4e748bdf83e0887c
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2023-11-17 00:23:54 -0500

    test: group sample cases

--- kata/6-kyu/matrix-multiplier/test/KataTest.java
+++ kata/6-kyu/matrix-multiplier/test/KataTest.java
@@ -1,45 +1,31 @@
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 import org.junit.jupiter.api.Test;
 
 class KataTest {
-
   @Test
-  public void test1x1() {
+  void sample() {
     assertArrayEquals(new double[][]{{6}},
         Kata.getMatrixProduct(
             new double[][]{{2}},
             new double[][]{{3}}));
-  }
 
-  @Test
-  public void test2x2() {
     assertArrayEquals(new double[][]{{19, 22}, {43, 50}},
         Kata.getMatrixProduct(
             new double[][]{{1, 2}, {3, 4}},
             new double[][]{{5, 6}, {7, 8}}));
-  }
 
-  @Test
-  public void testDecimals() {
     assertArrayEquals(new double[][]{{0.7, 1.0}, {1.5, 2.2}},
         Kata.getMatrixProduct(
             new double[][]{{0.5, 1}, {1.5, 2}},
             new double[][]{{0.2, 0.4}, {0.6, 0.8}}));
-  }
 
-  @Test
-  public void testAsymmetric() {
-    assertArrayEquals(
-        new double[][]{{73, 31, 78}, {54, 13, 43}, {106, 32, 94}, {63, 36, 81}},
+    assertArrayEquals(new double[][]{{73, 31, 78}, {54, 13, 43}, {106, 32, 94}, {63, 36, 81}},
         Kata.getMatrixProduct(
             new double[][]{{7, 3}, {2, 5}, {6, 8}, {9, 0}},
             new double[][]{{7, 4, 9}, {8, 1, 5}}));
-  }
 
-  @Test
-  public void testInvalid() {
-    assertArrayEquals(null,
-        Kata.getMatrixProduct(new double[][]{{1, 2}, {3, 4}}, new double[][]{{2, 4}}));
+    assertNull(Kata.getMatrixProduct(new double[][]{{1, 2}, {3, 4}}, new double[][]{{2, 4}}));
   }
 }
