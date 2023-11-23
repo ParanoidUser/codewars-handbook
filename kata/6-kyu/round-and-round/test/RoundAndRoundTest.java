a1fc13ef13c2a0a98ebd2a8b04b5385a666577ab
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2023-11-23 12:55:27 -0500

    test: parametrized

--- kata/6-kyu/round-and-round/test/RoundAndRoundTest.java
+++ kata/6-kyu/round-and-round/test/RoundAndRoundTest.java
@@ -1,33 +1,36 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class RoundAndRoundTest {
-  @Test
-  void sample() {
-    double delta = 0.01;
-    assertEquals(18.12D, RoundAndRound.roundTo2DecimalPlaces(18.123D), delta);
-    assertEquals(16.77D, RoundAndRound.roundTo2DecimalPlaces(16.765D), delta);
-    assertEquals(16.36D, RoundAndRound.roundTo2DecimalPlaces(16.355D), delta);
-    assertEquals(7.48D, RoundAndRound.roundTo2DecimalPlaces(7.477D), delta);
-    assertEquals(2D, RoundAndRound.roundTo2DecimalPlaces(2D), delta);
-    assertEquals(1.46D, RoundAndRound.roundTo2DecimalPlaces(1.455D), delta);
-    assertEquals(1.20D, RoundAndRound.roundTo2DecimalPlaces(1.1950000000000001D), delta);
-    assertEquals(1.19D, RoundAndRound.roundTo2DecimalPlaces(1.1949999999999999D), delta);
-    assertEquals(1.19D, RoundAndRound.roundTo2DecimalPlaces(1.1850000000000001D), delta);
-    assertEquals(1.18D, RoundAndRound.roundTo2DecimalPlaces(1.1849999999999999D), delta);
-    assertEquals(1.06D, RoundAndRound.roundTo2DecimalPlaces(1.055D), delta);
-    assertEquals(1.03D, RoundAndRound.roundTo2DecimalPlaces(1.025D), delta);
-    assertEquals(0D, RoundAndRound.roundTo2DecimalPlaces(0D), delta);
-    assertEquals(-5D, RoundAndRound.roundTo2DecimalPlaces(-4.999D), delta);
-    assertEquals(-1.03D, RoundAndRound.roundTo2DecimalPlaces(-1.025D), delta);
-    assertEquals(-1.06D, RoundAndRound.roundTo2DecimalPlaces(-1.055D), delta);
-    assertEquals(-1.18D, RoundAndRound.roundTo2DecimalPlaces(-1.1849999999999999D), delta);
-    assertEquals(-1.19D, RoundAndRound.roundTo2DecimalPlaces(-1.1850000000000001D), delta);
-    assertEquals(-1.19D, RoundAndRound.roundTo2DecimalPlaces(-1.1949999999999999D), delta);
-    assertEquals(-1.20D, RoundAndRound.roundTo2DecimalPlaces(-1.1950000000000001D), delta);
-    assertEquals(-1.46D, RoundAndRound.roundTo2DecimalPlaces(-1.455D), delta);
-    assertEquals(-16.35D, RoundAndRound.roundTo2DecimalPlaces(-16.345D), delta);
-    assertEquals(-16.77D, RoundAndRound.roundTo2DecimalPlaces(-16.765D), delta);
+  @ParameterizedTest
+  @CsvSource(textBlock = """
+      18.123, 18.12
+      16.765, 16.77
+      16.355, 16.36
+      7.477, 7.48
+      2, 2
+      1.455, 1.46
+      1.1950000000000001, 1.20
+      1.1949999999999999, 1.19
+      1.1850000000000001, 1.19
+      1.1849999999999999, 1.18
+      1.055, 1.06
+      1.025, 1.03
+      0, 0
+      -4.999, -5
+      -1.025, -1.03
+      -1.055, -1.06
+      -1.1849999999999999, -1.18
+      -1.1850000000000001, -1.19
+      -1.1949999999999999, -1.19
+      -1.1950000000000001, -1.20
+      -1.455, -1.46
+      -16.345, -16.35
+      -16.765, -16.77
+      """)
+  void sample(double number, double expected) {
+    assertEquals(expected, RoundAndRound.roundTo2DecimalPlaces(number), 0.01);
   }
 }
