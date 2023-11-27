88b85d2e3dfd5a35584a74128fbc1740b106f861
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2023-11-27 14:56:03 -0500

    refactor: optimize

--- kata/4-kyu/codewars-style-ranking-system/main/User.java
+++ kata/4-kyu/codewars-style-ranking-system/main/User.java
@@ -1,66 +1,14 @@
 class User {
-
   int rank = -8;
   int progress;
 
-  User incProgress(int rank) {
-
-    if (rank == 0 || rank < -8 || rank > 8) {
-      throw new IllegalArgumentException("Invalid rank supplied");
+  public void incProgress(int activityRank) {
+    if (activityRank < -8 || activityRank == 0 || activityRank > 8) {
+      throw new IllegalArgumentException();
     }
-
-    if (rank == 8) {
-      progress = 0;
-    } else {
-      // negative diff means that user completed a higher rank
-      // positive diff means that user completed a lower rank
-       int diff = this.rank - rank;
-
-      // same level - 3 progress points
-      if (diff == 0) {
-        this.progress += 3;
-      }
-
-      // completed lower rank
-      // 2 levels or more lower are ignored
-      if (diff == 1) {
-        this.progress += 1;
-      }
-
-      // completed a lower rank while at rank 1
-      if (diff == 2 && this.rank == 1 && rank == -1) {
-        this.progress += 1;
-      }
-
-      // completed a higher rank
-      if (diff < 0) {
-        if (this.rank < 0 && rank > 0) {
-          // there is no zero level
-          this.progress += (10 * (diff + 1) * (diff + 1));
-        } else {
-          this.progress += (10 * diff * diff);
-        }
-      }
-
-      checkProgress();
-    }
-    return this;
-  }
-
-
-  void checkProgress() {
-    while (progress >= 100) {
-      progress -= 100;
-      rank++;
-
-      if (rank == 0) {
-        rank = 1;
-      }
-
-      if (rank >= 8) {
-        rank = 8;
-        progress = 0;
-      }
-    }
+    int diff = activityRank - rank + (activityRank * rank < 0 ? 1 : 0) * (rank > 0 ? 1 : -1);
+    progress += diff > 0 ? 10 * diff * diff : diff == 0 ? 3 : diff == -1 ? 1 : 0;
+    rank += progress / 100 + (progress / 100 == -rank ? 1 : 0);
+    progress = rank > 7 ? 0 : progress % 100;
   }
 }
