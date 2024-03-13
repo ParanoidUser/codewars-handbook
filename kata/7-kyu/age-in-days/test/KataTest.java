ae145de475ee810176515bfda8c4ba2fa0ce3274
Capt. Cutlass <5120290+ParanoidUser@users.noreply.github.com>
2024-03-13 11:32:39 -0400

    test: fix leap year assertion

--- kata/7-kyu/age-in-days/test/KataTest.java
+++ kata/7-kyu/age-in-days/test/KataTest.java
@@ -2,6 +2,7 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.time.LocalDate;
+import java.time.Year;
 import org.junit.jupiter.api.Test;
 
 class KataTest {
@@ -9,6 +10,8 @@ class KataTest {
   void sample() {
     var birthday = LocalDate.now();
     assertEquals("You are 2 days old", Kata.ageInDays(birthday.minusDays(2).getYear(), birthday.minusDays(2).getMonthValue(), birthday.minusDays(2).getDayOfMonth()));
-    assertEquals("You are 365 days old", Kata.ageInDays(birthday.minusYears(1).getYear(), birthday.minusYears(1).getMonthValue(), birthday.minusYears(1).getDayOfMonth()));
+
+    String expected = String.format("You are %d days old", Year.of(birthday.getYear()).length());
+    assertEquals(expected, Kata.ageInDays(birthday.minusYears(1).getYear(), birthday.minusYears(1).getMonthValue(), birthday.minusYears(1).getDayOfMonth()));
   }
 }
