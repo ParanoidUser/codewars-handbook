860a8190259934b033287ddfd7c4ecacc9dfc22d
Capt. Cutlass <5120290+ParanoidUser@users.noreply.github.com>
2024-07-31 01:44:03 -0400

    test: cover negative case

--- kata/7-kyu/make-me-slow/test/SolutionTest.java
+++ kata/7-kyu/make-me-slow/test/SolutionTest.java
@@ -9,4 +9,13 @@ class SolutionTest {
     Kata.makeMeSlow();
     assertTrue(System.currentTimeMillis() > elapsed);
   }
+
+  @Test
+  void negative() {
+    long elapsed = System.currentTimeMillis() + 100;
+    var thread = new Thread(Kata::makeMeSlow);
+    thread.start();
+    thread.interrupt();
+    assertTrue(System.currentTimeMillis() < elapsed);
+  }
 }
