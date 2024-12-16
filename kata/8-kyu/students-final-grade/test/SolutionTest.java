28f9872aba4342e4882f4f80bbd5f4ac8a1a45c9
Capt. Cutlass <5120290+ParanoidUser@users.noreply.github.com>
2024-12-16 18:36:22 -0500

    test: partially covered conditions

--- kata/8-kyu/students-final-grade/test/SolutionTest.java
+++ kata/8-kyu/students-final-grade/test/SolutionTest.java
@@ -7,9 +7,11 @@ class SolutionTest {
   @ParameterizedTest
   @CsvSource(textBlock = """
        99, 1, 100
+       80, 11, 100
        76, 5, 90
        76, 4, 75
        52, 1, 0
+       33, 4, 0
       """)
   void sample(int exam, int projects, int grade) {
     assertEquals(grade, StudentFinalGrade.finalGrade(exam, projects));
