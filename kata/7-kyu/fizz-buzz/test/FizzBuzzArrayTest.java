5f45da13ce232cd2f0153fb066bab2e0b98c7c97
Capt. Cutlass <5120290+ParanoidUser@users.noreply.github.com>
2025-01-02 23:48:28 -0500

    test: cover fizzbuzz case

--- kata/7-kyu/fizz-buzz/test/FizzBuzzArrayTest.java
+++ kata/7-kyu/fizz-buzz/test/FizzBuzzArrayTest.java
@@ -6,6 +6,7 @@ class FizzBuzzArrayTest {
   @Test
   void sample() {
     assertArrayEquals(new String[]{"1"}, FizzBuzzArray.fizzBuzz(1));
-    assertArrayEquals(new String[]{"1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz"}, FizzBuzzArray.fizzBuzz(10));
+    assertArrayEquals(new String[]{"1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz"},
+        FizzBuzzArray.fizzBuzz(15));
   }
 }
