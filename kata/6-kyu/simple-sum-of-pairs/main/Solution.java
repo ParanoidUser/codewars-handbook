cfbf20209321da75917c480fc931850ab7e5d7c1
Capt. Cutlass <5120290+ParanoidUser@users.noreply.github.com>
2025-01-07 23:57:30 -0500

    feat: kata/simple-sum-of-pairs

--- kata/6-kyu/simple-sum-of-pairs/main/Solution.java
+++ kata/6-kyu/simple-sum-of-pairs/main/Solution.java
@@ -3,7 +3,7 @@ import java.util.function.LongToIntFunction;
 interface Solution {
   static int solve(long n) {
     LongToIntFunction sum = l -> (l + "").chars().map(c -> c - 48).sum();
-    long a = (long) Math.pow(10, (n + "").length() - 1.) - 1;
+    long a = (long) Math.pow(10, Math.floor(Math.log10(n))) - 1;
     return (int) (n > 1 ? sum.applyAsInt(a) + sum.applyAsInt(n - a) : n);
   }
 }
