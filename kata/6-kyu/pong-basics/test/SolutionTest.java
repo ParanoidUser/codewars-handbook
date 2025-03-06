b69ed3e15140df17efcd1cdd8aacc081e9c987aa
Capt. Cutlass <5120290+ParanoidUser@users.noreply.github.com>
2025-03-05 22:42:51 -0500

    test: cover player 1 win case

--- kata/6-kyu/pong-basics/test/SolutionTest.java
+++ kata/6-kyu/pong-basics/test/SolutionTest.java
@@ -4,7 +4,15 @@ import org.junit.jupiter.api.Test;
 
 class SolutionTest {
   @Test
-  void sample() {
+  void player1() {
+    Pong game = new Pong(1);
+    assertEquals("Player 1 has hit the ball!", game.play(360, 363));
+    assertEquals("Player 1 has won the game!", game.play(575, 582));
+    assertEquals("Game Over!", game.play(-1, -1));
+  }
+
+  @Test
+  void player2() {
     Pong game = new Pong(2);
     assertEquals("Player 1 has hit the ball!", game.play(50, 53));
     assertEquals("Player 2 has hit the ball!", game.play(100, 97));
