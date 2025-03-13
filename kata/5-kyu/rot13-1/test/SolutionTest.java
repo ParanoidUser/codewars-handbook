8f0ee13212a3d110797ea4df198f00299de8230d
Capt. Cutlass <5120290+ParanoidUser@users.noreply.github.com>
2025-03-13 18:52:13 -0400

    feat: kata/rot13-1

---
+++ kata/5-kyu/rot13-1/test/SolutionTest.java
@@ -0,0 +1,15 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class SolutionTest {
+  @ParameterizedTest
+  @CsvSource(textBlock = """
+      test, grfg
+      Test, Grfg
+      """)
+  void sample(String message, String expected) {
+    assertEquals(expected, Kata.rot13(message));
+  }
+}
