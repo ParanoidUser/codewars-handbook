22dfb682cab2d19a4d20910d82e1c959fe7b3d0e
Capt. Cutlass <5120290+ParanoidUser@users.noreply.github.com>
2025-04-16 02:01:48 -0400

    refactor: use boolean orientation

--- kata/3-kyu/battleship-field-validator/main/BattleField.java
+++ kata/3-kyu/battleship-field-validator/main/BattleField.java
@@ -6,21 +6,20 @@ interface BattleField {
   }
 
   private static int size(int[][] field, int row, int col) {
-    int axis = col < 9 && field[row][col + 1] == 1 ? 1 : row < 9 && field[row + 1][col] == 1 ? -1 : 0;
     int size = 1;
-    while (axis > 0 && col + size < 10 && field[row][col + size] == 1 || // horizontal
-           axis < 0 && row + size < 10 && field[row + size][col] == 1) { // vertical
+    while (col < 9 && field[row][col + 1] == 1 && col + size < 10 && field[row][col + size] == 1 ||
+           row < 9 && field[row + 1][col] == 1 && row + size < 10 && field[row + size][col] == 1) {
       size++;
     }
-    return isValid(field, row, col, axis, size) ? (int) Math.pow(10, size - 1.) : 0;
+    return isValid(field, row, col, row < 9 && field[row + 1][col] == 1, size) ? (int) Math.pow(10, size - 1.) : 0;
   }
 
-  private static boolean isValid(int[][] field, int row, int col, int axis, int size) {
-    for (int r = Math.max(row - 1, 0); r <= Math.min(axis < 0 ? row + size : row + 1, 9); r++) {
-      for (int c = Math.max(col - 1, 0); c <= Math.min(axis > 0 ? col + size : col + 1, 9); c++) {
+  private static boolean isValid(int[][] field, int row, int col, boolean vertical, int size) {
+    for (int r = Math.max(row - 1, 0); r <= Math.min(vertical ? row + size : row + 1, 9); r++) {
+      for (int c = Math.max(col - 1, 0); c <= Math.min(vertical ? col + 1 : col + size, 9); c++) {
         if (field[r][c] == 1 && !(r == row && c == col ||
-            axis > 0 && col + size > c && r == row && c >= col ||
-            axis < 0 && row + size > r && c == col && r >= row)) {
+            col + size > c && r == row && c >= col ||
+            row + size > r && c == col && r >= row)) {
           return false;
         }
       }
