7900f3fbea146d204099bdf407ba589ee1970297
Capt. Cutlass <5120290+ParanoidUser@users.noreply.github.com>
2025-06-12 23:50:24 -0400

    feat: kata/financing-a-purchase

---
+++ kata/6-kyu/financing-a-purchase/main/Finance.java
@@ -0,0 +1,13 @@
+interface Finance {
+  static String amort(double rate, double bal, int term, int numPayment) {
+    rate /= 1200;
+    double c = rate * bal / (1 - Math.pow(1 + rate, -term));
+    double principal = (c - bal * rate) * Math.pow(1 + rate, numPayment - 1);
+    return String.format("num_payment %d c %.0f princ %.0f int %.0f balance %.0f",
+        numPayment,
+        c,
+        principal,
+        c - principal,
+        bal * (1 - (Math.pow(1 + rate, numPayment) - 1) / (Math.pow(1 + rate, term) - 1)));
+  }
+}
