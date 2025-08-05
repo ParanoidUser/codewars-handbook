757ed10d2bdd80e618c07e6836b8b3beeda68cb7
Capt. Cutlass <5120290+ParanoidUser@users.noreply.github.com>
2025-08-04 21:45:19 -0400

    refactor: cast one of the operands of this addition operation to a "long"

--- kata/7-kyu/loop-detector/main/Kata.java
+++ kata/7-kyu/loop-detector/main/Kata.java
@@ -2,6 +2,6 @@ import static java.util.stream.IntStream.iterate;
 
 interface Kata {
   static boolean hasLoop(int[] arr) {
-    return iterate(0, i -> arr[i]).limit(arr.length + 1).allMatch(i -> i < arr.length);
+    return iterate(0, i -> arr[i]).limit(arr.length + 1L).allMatch(i -> i < arr.length);
   }
 }
