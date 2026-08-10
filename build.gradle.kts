f44806909d9f03bf4d10263a16eebbc8ab8d9c6e
Capt. Cutlass <5120290+ParanoidUser@users.noreply.github.com>
2026-08-10 14:28:43 -0400

    build: bump java version to 21

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,8 +13,8 @@ subprojects {
     apply(plugin = "jacoco")
 
     java {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
 
     sourceSets {
